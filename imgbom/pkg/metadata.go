17787f856fce7a09d8ca1ad4461cbfc2a8958b35
Alex Goodman <alex.goodman@anchore.com>
2020-06-01 15:25:47 -0400

    rm unused dkpg metadata, add dpkg source field, add analyzer source name

--- imgbom/pkg/metadata.go
+++ imgbom/pkg/metadata.go
@@ -4,17 +4,7 @@ package pkg
 // Available fields are described at http://manpages.ubuntu.com/manpages/xenial/man1/dpkg-query.1.html
 // in the --showformat section
 type DpkgMetadata struct {
-	Package        string `mapstructure:"Package"`
-	Architecture   string `mapstructure:"Architecture"`
-	DependsPkgs    string `mapstructure:"Depends"`
-	InstalledSize  string `mapstructure:"Installed-Size"`
-	Maintainer     string `mapstructure:"Maintainer"`
-	Priority       string `mapstructure:"Priority"`
-	ProvidesPkgs   string `mapstructure:"Provides"`
-	RecommendsPkgs string `mapstructure:"Recommends"`
-	ReplacesPkgs   string `mapstructure:"Replaces"`
-	Status         string `mapstructure:"Status"`
-	SuggestsPkgs   string `mapstructure:"Suggests"`
-	Version        string `mapstructure:"Version"`
-	ConfigFiles    string `mapstructure:"Conffiles"`
+	Package string `mapstructure:"Package"`
+	Source  string `mapstructure:"Source"`
+	Version string `mapstructure:"Version"`
 }
