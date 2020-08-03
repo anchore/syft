e22ddd386111368b256426e2f01c5a1b392c13d2
Dan Luhring <dan.luhring@anchore.com>
2020-08-03 11:41:11 -0400

    Update pkg Type definition to string
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>

--- syft/pkg/type.go
+++ syft/pkg/type.go
@@ -1,40 +1,23 @@
 package pkg
 
+type Type string
+
 const (
-	UnknownPkg Type = iota
-	ApkPkg
-	BundlerPkg
-	DebPkg
-	EggPkg
-	//PacmanPkg
-	RpmPkg
-	WheelPkg
-	NpmPkg
-	YarnPkg
-	PythonRequirementsPkg
-	JavaPkg
-	JenkinsPluginPkg
-	GoModulePkg
+	UnknownPkg Type = "UnknownPackage"
+	ApkPkg Type = "apk"
+	BundlerPkg Type = "bundle"
+	DebPkg Type = "deb"
+	EggPkg Type = "egg"
+	// PacmanPkg Type = "pacman"
+	RpmPkg Type = "rpm"
+	WheelPkg Type = "wheel"
+	NpmPkg Type = "npm"
+	YarnPkg Type = "yarn"
+	PythonRequirementsPkg Type = "python-requirements"
+	JavaPkg Type = "java-archive"
+	JenkinsPluginPkg Type = "jenkins-plugin"
+	GoModulePkg Type = "go-module"
 )
-
-type Type uint
-
-var typeStr = []string{
-	"UnknownPackage",
-	"apk",
-	"bundle",
-	"deb",
-	"egg",
-	//"pacman",
-	"rpm",
-	"wheel",
-	"npm",
-	"yarn",
-	"python-requirements",
-	"java-archive",
-	"jenkins-plugin",
-	"go-module",
-}
 
 var AllPkgs = []Type{
 	ApkPkg,
@@ -52,9 +35,3 @@ var AllPkgs = []Type{
 	GoModulePkg,
 }
 
-func (t Type) String() string {
-	if int(t) >= len(typeStr) {
-		return typeStr[0]
-	}
-	return typeStr[t]
-}
