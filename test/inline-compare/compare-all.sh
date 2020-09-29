c2e6039f7d68e776b0d749e66b3d674f2b755012
Dan Luhring <dan.luhring@anchore.com>
2020-09-29 11:49:14 -0400

    Update TODO with link
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>

--- test/inline-compare/compare-all.sh
+++ test/inline-compare/compare-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-# TODO: Add "alpine:3.12.0" back in when we've figured out how to handle the apk version field w/ and w/o release information
+# TODO: Add "alpine:3.12.0" back in when we've figured out how to handle the apk version field w/ and w/o release information (see issue: https://github.com/anchore/syft/pull/195)
 images=("debian:10.5" "centos:8.2.2004")
 
 # gather all image analyses
