8dedc03292cfe4b2b100364ba3950d985269e919
Alex Goodman <alex.goodman@anchore.com>
2020-10-13 14:42:42 -0400

    allow for gemspec metadata fields to be optional
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- syft/pkg/gem_metadata.go
+++ syft/pkg/gem_metadata.go
@@ -3,8 +3,8 @@ package pkg
 type GemMetadata struct {
 	Name     string   `mapstructure:"name" json:"name"`
 	Version  string   `mapstructure:"version" json:"version"`
-	Files    []string `mapstructure:"files" json:"files"`
-	Authors  []string `mapstructure:"authors" json:"authors"`
-	Licenses []string `mapstructure:"licenses" json:"licenses"`
-	Homepage string   `mapstructure:"homepage" json:"homepage"`
+	Files    []string `mapstructure:"files" json:"files,omitempty"`
+	Authors  []string `mapstructure:"authors" json:"authors,omitempty"`
+	Licenses []string `mapstructure:"licenses" json:"licenses,omitempty"`
+	Homepage string   `mapstructure:"homepage" json:"homepage,omitempty"`
 }
