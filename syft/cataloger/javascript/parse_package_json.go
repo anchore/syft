9ec3ad58c81ea5a1d0c3cbe7614ddde06f5ea7ab
Dan Luhring <dan.luhring@anchore.com>
2021-01-19 22:20:12 -0500

    Update regression test expected value and pin deps
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>

--- syft/cataloger/javascript/parse_package_json.go
+++ syft/cataloger/javascript/parse_package_json.go
@@ -4,9 +4,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/anchore/syft/internal/log"
 	"io"
 	"regexp"
+
+	"github.com/anchore/syft/internal/log"
 
 	"github.com/anchore/syft/internal"
 
