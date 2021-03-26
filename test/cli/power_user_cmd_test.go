85511687026de9010ed2e63d541e93a1e12b4c5b
Alex Goodman <alex.goodman@anchore.com>
2021-03-26 17:32:17 -0400

    capitalize file type strings
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- test/cli/power_user_cmd_test.go
+++ test/cli/power_user_cmd_test.go
@@ -26,7 +26,7 @@ func TestPowerUserCmdFlags(t *testing.T) {
 			args: []string{"power-user", request},
 			assertions: []traitAssertion{
 				assertNotInOutput(" command is deprecated"),     // only the root command should be deprecated
-				assertInOutput(`"type": "regularFile"`),         // proof of file-metadata data
+				assertInOutput(`"type": "RegularFile"`),         // proof of file-metadata data
 				assertInOutput(`"algorithm": "sha256"`),         // proof of file-metadata default digest algorithm of sha256
 				assertInOutput(`"metadataType": "ApkMetadata"`), // proof of package artifacts data
 				assertSuccessfulReturnCode,
