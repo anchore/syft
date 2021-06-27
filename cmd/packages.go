ab1401718bc1f7b899f9e3197a61654a318c043f
Alex Goodman <wagoodman@gmail.com>
2021-06-27 13:32:45 -0400

    add event loop tests
    Signed-off-by: Alex Goodman <wagoodman@gmail.com>

--- cmd/packages.go
+++ cmd/packages.go
@@ -189,6 +189,7 @@ func packagesExec(_ *cobra.Command, args []string) error {
 		setupSignals(),
 		eventSubscription,
 		ui.Select(appConfig.CliOptions.Verbosity > 0, appConfig.Quiet),
+		syft.Cleanup,
 	)
 }
 
