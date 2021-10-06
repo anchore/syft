5fb24c882d8e34434c878c74cba8892b5af53c88
Christopher Angelo Phillips <christopher.phillips@anchore.com>
2021-10-06 13:08:55 -0400

    unit test for buildGoPkgInfo
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

--- syft/pkg/cataloger/golang/parse_go_bin_test.go
+++ syft/pkg/cataloger/golang/parse_go_bin_test.go
@@ -7,16 +7,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestbuildGoPkgInfo(t *testing.T) {
+func TestBuildGoPkgInfo(t *testing.T) {
 	tests := []struct {
 		name     string
 		mod      string
 		expected []pkg.Package
 	}{
 		{
-			name:     "buildGoPkgInfo can parse a mod string and return packages",
+			name:     "buildGoPkgInfo parses a blank mod string and returns no packages",
 			mod:      "",
 			expected: make([]pkg.Package, 0),
+		},
+		{
+			name: "buildGoPkgInfo parses a populated mod string and returns packages but no source info",
+			mod: `path    github.com/anchore/syft mod     github.com/anchore/syft (devel)
+				  dep     github.com/adrg/xdg     v0.2.1  h1:VSVdnH7cQ7V+B33qSJHTCRlNgra1607Q8PzEmnvb2Ic=
+				  dep     github.com/anchore/client-go    v0.0.0-20210222170800-9c70f9b80bcf      h1:DYssiUV1pBmKqzKsm4mqXx8artqC0Q8HgZsVI3lMsAg=`,
+			expected: []pkg.Package{
+				{
+					Name:     "github.com/adrg/xdg",
+					Version:  "v0.2.1",
+					Language: pkg.Go,
+					Type:     pkg.GoModulePkg,
+				},
+				{
+					Name:     "github.com/anchore/client-go",
+					Version:  "v0.0.0-20210222170800-9c70f9b80bcf",
+					Language: pkg.Go,
+					Type:     pkg.GoModulePkg,
+				},
+			},
 		},
 	}
 
