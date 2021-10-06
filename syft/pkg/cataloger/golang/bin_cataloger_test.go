e910db68e2ec58cd4403402c1c2a41361b4443d6
Christopher Angelo Phillips <christopher.phillips@anchore.com>
2021-10-06 10:59:21 -0400

    typo and test updates
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

---
+++ syft/pkg/cataloger/golang/bin_cataloger_test.go
@@ -0,0 +1,19 @@
+package golang
+
+import "testing"
+
+func TestGoBinCataloger(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "test name",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+		})
+	}
+}
