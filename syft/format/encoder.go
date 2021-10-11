055542694f9e823bb72b6c67f9e4676a538542c2
Alex Goodman <alex.goodman@anchore.com>
2021-10-10 19:02:00 -0700

    remove encoder notes (have already been incorporated into lib interface)
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- syft/format/encoder.go
+++ syft/format/encoder.go
@@ -8,15 +8,4 @@ import (
 	"github.com/anchore/syft/syft/source"
 )
 
-// TODO: this should probably be a helper function to return the bytes
-//func using() {
-//	var buff bytes.Buffer
-//	var encoder Encoder // = ...
-//
-//	_:=encoder(&buff, nil, nil, nil)
-//
-//	bytes.NewReader(buff.Bytes())
-//
-//}
-
 type Encoder func(io.Writer, *pkg.Catalog, *source.Metadata, *distro.Distro) error
