2d387dfe9aefc3da3f221495ad82bf284be578ea
spiffcs <christopher.phillips@anchore.com>
2021-10-12 20:38:19 -0400

    add build tags to files
    Signed-off-by: spiffcs <christopher.phillips@anchore.com>
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

--- internal/ui/event_handlers_linux.go
+++ internal/ui/event_handlers_linux.go
@@ -14,21 +14,6 @@ import (
 	"github.com/wagoodman/go-partybus"
 )
 
-// handleCatalogerPresenterReady is a UI function for processing the CatalogerFinished bus event, displaying the catalog
-// via the given presenter to stdout.
-func handleCatalogerPresenterReady(event partybus.Event, reportOutput io.Writer) error {
-	// show the report to stdout
-	pres, err := syftEventParsers.ParsePresenterReady(event)
-	if err != nil {
-		return fmt.Errorf("bad CatalogerFinished event: %w", err)
-	}
-
-	if err := pres.Present(reportOutput); err != nil {
-		return fmt.Errorf("unable to show package catalog report: %w", err)
-	}
-	return nil
-}
-
 // handleAppUpdateAvailable is a UI handler function to display a new application version to the top of the screen.
 func handleAppUpdateAvailable(_ context.Context, fr *frame.Frame, event partybus.Event, _ *sync.WaitGroup) error {
 	newVersion, err := syftEventParsers.ParseAppUpdateAvailable(event)
