9e72efa45f8ce0bc451dff033cfaa3d73c1e077d
Alex Goodman <alex.goodman@anchore.com>
2021-10-13 16:54:40 -0700

    remove presenter format enum type + add formats shim in presenter helper
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- syft/presenter/packages/presenter.go
+++ syft/presenter/packages/presenter.go
@@ -5,26 +5,31 @@ a specific Presenter implementation given user configuration.
 package packages
 
 import (
+	"github.com/anchore/syft/internal/formats"
 	"github.com/anchore/syft/internal/presenter/packages"
+	"github.com/anchore/syft/syft/format"
 	"github.com/anchore/syft/syft/presenter"
 )
 
 // Presenter returns a presenter for images or directories
-func Presenter(option PresenterOption, config PresenterConfig) presenter.Presenter {
+func Presenter(option format.Option, config PresenterConfig) presenter.Presenter {
 	switch option {
-	case JSONPresenterOption:
-		return packages.NewJSONPresenter(config.Catalog, config.SourceMetadata, config.Distro, config.Scope)
-	case TextPresenterOption:
+	case format.TextOption:
 		return packages.NewTextPresenter(config.Catalog, config.SourceMetadata)
-	case TablePresenterOption:
+	case format.TableOption:
 		return packages.NewTablePresenter(config.Catalog)
-	case CycloneDxPresenterOption:
+	case format.CycloneDxOption:
 		return packages.NewCycloneDxPresenter(config.Catalog, config.SourceMetadata)
-	case SPDXTagValuePresenterOption:
+	case format.SPDXTagValueOption:
 		return packages.NewSPDXTagValuePresenter(config.Catalog, config.SourceMetadata)
-	case SPDXJSONPresenterOption:
+	case format.SPDXJSONOption:
 		return packages.NewSPDXJSONPresenter(config.Catalog, config.SourceMetadata)
 	default:
-		return nil
+		// TODO: the final state is that all other cases would be replaced by formats.ByOption (wed remove this function entirely)
+		f := formats.ByOption(option)
+		if f == nil {
+			return nil
+		}
+		return f.Presenter(config.Catalog, &config.SourceMetadata, config.Distro, config.Scope)
 	}
 }
