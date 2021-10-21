cd10485b68492ca31fdc2978f6f6a40addcd5e99
Christopher Angelo Phillips <christopher.phillips@anchore.com>
2021-10-21 12:14:06 -0400

    update file name changes from pr
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -1,10 +1,6 @@
 package ui
 
 import (
-	"fmt"
-	"io"
-
-	syftEventParsers "github.com/anchore/syft/syft/event/parsers"
 	"github.com/wagoodman/go-partybus"
 )
 
@@ -12,19 +8,4 @@ type UI interface {
 	Setup(unsubscribe func() error) error
 	partybus.Handler
 	Teardown(force bool) error
-}
-
-// handleCatalogerPresenterReady is a UI function for processing the CatalogerFinished bus event, displaying the catalog
-// via the given presenter to stdout.
-func handleCatalogerPresenterReady(event partybus.Event, reportOutput io.Writer) error {
-	// show the report to stdout
-	pres, err := syftEventParsers.ParsePresenterReady(event)
-	if err != nil {
-		return fmt.Errorf("bad CatalogerFinished event: %w", err)
-	}
-
-	if err := pres.Present(reportOutput); err != nil {
-		return fmt.Errorf("unable to show package catalog report: %w", err)
-	}
-	return nil
 }
