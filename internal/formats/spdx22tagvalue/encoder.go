95bd1fdeb592215f43e37a18496320e669099e56
Alex Goodman <alex.goodman@anchore.com>
2021-11-02 14:24:11 -0400

    swap SPDX tag-value formatter to single sbom document
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- internal/formats/spdx22tagvalue/encoder.go
+++ internal/formats/spdx22tagvalue/encoder.go
@@ -3,15 +3,11 @@ package spdx22tagvalue
 import (
 	"io"
 
+	"github.com/anchore/syft/syft/sbom"
 	"github.com/spdx/tools-golang/tvsaver"
-
-	"github.com/anchore/syft/syft/distro"
-
-	"github.com/anchore/syft/syft/pkg"
-	"github.com/anchore/syft/syft/source"
 )
 
-func encoder(output io.Writer, catalog *pkg.Catalog, srcMetadata *source.Metadata, d *distro.Distro, scope source.Scope) error {
-	model := toFormatModel(catalog, srcMetadata, d, scope)
+func encoder(output io.Writer, s sbom.SBOM) error {
+	model := toFormatModel(s)
 	return tvsaver.Save2_2(&model, output)
 }
