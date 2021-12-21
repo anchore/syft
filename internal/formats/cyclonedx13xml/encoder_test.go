6b7f8baefe66ef95cb93a1f2bbc6d0dca18cde1d
Peter Balogh <p.balogh.sa@gmail.com>
2021-12-21 12:47:01 +0100

    add sha256Pattern for layerID
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- internal/formats/cyclonedx13xml/encoder_test.go
+++ internal/formats/cyclonedx13xml/encoder_test.go
@@ -31,8 +31,9 @@ func TestCycloneDxImagePresenter(t *testing.T) {
 func cycloneDxRedactor(s []byte) []byte {
 	serialPattern := regexp.MustCompile(`serialNumber="[a-zA-Z0-9\-:]+"`)
 	rfc3339Pattern := regexp.MustCompile(`([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([\+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))`)
+	sha256Pattern := regexp.MustCompile(`sha256:[A-Fa-f0-9]{64}`)
 
-	for _, pattern := range []*regexp.Regexp{serialPattern, rfc3339Pattern} {
+	for _, pattern := range []*regexp.Regexp{serialPattern, rfc3339Pattern, sha256Pattern} {
 		s = pattern.ReplaceAll(s, []byte("redacted"))
 	}
 	return s
