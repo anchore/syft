e0a8efd2beffe6a3a7b5b95bd216a61d11839c46
spiffcs <christopher.phillips@anchore.com>
2022-01-05 20:35:34 -0500

    add more cases to test
    Signed-off-by: spiffcs <christopher.phillips@anchore.com>
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

--- syft/source/directory_resolver_windows_test.go
+++ syft/source/directory_resolver_windows_test.go
@@ -18,7 +18,43 @@ func Test_windowsToPosix(t *testing.T) {
 			},
 			wantPosixPath: "/c/some/windows/place",
 		},
+		{
+			name: "escaped case",
+			args: args{
+				windowsPath: `C:\\some\\windows\\place`,
+			},
+			wantPosixPath: "/c/some/windows/place",
+		},
+		{
+			name: "forward slash",
+			args: args{
+				windowsPath: `C:/foo/bar`,
+			},
+			wantPosixPath: "/c/foo/bar",
+		},
+		{
+			name: "mix slash",
+			args: args{
+				windowsPath: `C:\foo/bar\`,
+			},
+			wantPosixPath: "/c/foo/bar",
+		},
+		{
+			name: "random case",
+			args: args{
+				windowsPath: `C:\Foo/bAr\`,
+			},
+			wantPosixPath: "/c/Foo/bAr",
+		},
+		{
+			name: "random case",
+			args: args{
+				windowsPath: `C:\ふー\バー`,
+			},
+			wantPosixPath: "/c/ふー/バー",
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotPosixPath := windowsToPosix(tt.args.windowsPath); gotPosixPath != tt.wantPosixPath {
