900d3d3e69a85234a5f517228defb91d39412db8
Christopher Angelo Phillips <christopher.phillips@anchore.com>
2022-01-06 11:24:44 -0500

    add mirror cases to posix function
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>
    Signed-off-by: Christopher Angelo Phillips <christopher.phillips@anchore.com>

--- syft/source/directory_resolver_windows_test.go
+++ syft/source/directory_resolver_windows_test.go
@@ -40,14 +40,14 @@ func Test_windowsToPosix(t *testing.T) {
 			wantPosixPath: "/c/foo/bar",
 		},
 		{
-			name: "random case",
+			name: "case sensitive case",
 			args: args{
 				windowsPath: `C:\Foo/bAr\`,
 			},
 			wantPosixPath: "/c/Foo/bAr",
 		},
 		{
-			name: "random case",
+			name: "special char case",
 			args: args{
 				windowsPath: `C:\ふー\バー`,
 			},
@@ -79,6 +79,41 @@ func Test_posixToWindows(t *testing.T) {
 				posixPath: "/c/some/windows/place",
 			},
 			wantWindowsPath: `C:\some\windows\place`,
+		},
+		{
+			name: "escaped case",
+			args: args{
+				posixPath: "/c/some/windows/place",
+			},
+			wantWindowsPath: `C:\\some\\windows\\place`,
+		},
+		{
+			name: "forward slash",
+			args: args{
+				posixPath: "/c/foo/bar",
+			},
+			wantWindowsPath: `C:/foo/bar`,
+		},
+		{
+			name: "mix slash",
+			args: args{
+				posixPath: "/c/foo/bar",
+			},
+			wantWindowsPath: `C:\foo/bar\`,
+		},
+		{
+			name: "case sensitive case",
+			args: args{
+				posixPath: "/c/Foo/bAr",
+			},
+			wantWindowsPath: `C:\Foo/bAr\`,
+		},
+		{
+			name: "special char case",
+			args: args{
+				posixPath: "/c/ふー/バー",
+			},
+			wantWindowsPath: `C:\ふー\バー`,
 		},
 	}
 	for _, tt := range tests {
