f59af255e315fa870ffedb99a8f3181b4c036553
Keith Zantow <kzantow@gmail.com>
2022-01-13 15:27:06 -0500

    Align SPDX export more with SPDX 2.2 specification (#743)

--- internal/formats/spdx22json/model/element.go
+++ internal/formats/spdx22json/model/element.go
@@ -3,7 +3,7 @@ package model
 type Element struct {
 	SPDXID string `json:"SPDXID"`
 	// Identify name of this SpdxElement.
-	Name string `json:"name"`
+	Name string `json:"name,omitempty"`
 	// Relationships referenced in the SPDX document
 	Relationships []Relationship `json:"relationships,omitempty"`
 	// Provide additional information about an SpdxElement.
