3c6842f41a05af87e53aa053df4ea2f0bc8d6b25
Alex Goodman <alex.goodman@anchore.com>
2022-01-25 10:18:20 -0500

    fix linter warning
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- internal/formats/common/cyclonedxhelpers/group.go
+++ internal/formats/common/cyclonedxhelpers/group.go
@@ -4,11 +4,8 @@ import "github.com/anchore/syft/syft/pkg"
 
 func Group(p pkg.Package) string {
 	if hasMetadata(p) {
-		switch metadata := p.Metadata.(type) {
-		case pkg.JavaMetadata:
-			if metadata.PomProperties != nil {
-				return metadata.PomProperties.GroupID
-			}
+		if metadata, ok := p.Metadata.(pkg.JavaMetadata); ok && metadata.PomProperties != nil {
+			return metadata.PomProperties.GroupID
 		}
 	}
 	return ""
