fafa2707d569ce7b2d975b18130eebfca2dcf768
Christopher Phillips <christopher.phillips@anchore.com>
2022-01-31 13:17:19 -0500

    revert to initial keysign bare bones execution
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- cmd/attest.go
+++ cmd/attest.go
@@ -1,28 +1,30 @@
 package cmd
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/anchore/stereoscope"
 	"github.com/anchore/syft/internal"
-	"github.com/anchore/syft/internal/bus"
 	"github.com/anchore/syft/internal/log"
 	"github.com/anchore/syft/internal/ui"
 	"github.com/anchore/syft/syft"
-	"github.com/anchore/syft/syft/event"
 	"github.com/anchore/syft/syft/format"
-	"github.com/anchore/syft/syft/sbom"
 	"github.com/anchore/syft/syft/source"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/in-toto/in-toto-golang/in_toto"
+	"github.com/pkg/errors"
 	"github.com/pkg/profile"
 	"github.com/sigstore/cosign/cmd/cosign/cli/sign"
 	"github.com/sigstore/cosign/pkg/cosign/attestation"
+	"github.com/sigstore/sigstore/pkg/signature/dsse"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"github.com/wagoodman/go-partybus"
+
+	signatureoptions "github.com/sigstore/sigstore/pkg/signature/options"
 )
 
 var (
@@ -69,8 +71,12 @@ func attestExec(_ *cobra.Command, args []string) error {
 	// can only be an image for attestation
 	userInput := args[0]
 
+	ko := sign.KeyOpts{
+		KeyRef: appConfig.File,
+	}
+
 	return eventLoop(
-		attestationExecWorker(userInput, writer),
+		attestationExecWorker(userInput, ko),
 		setupSignals(),
 		eventSubscription,
 		stereoscope.Cleanup,
@@ -78,7 +84,7 @@ func attestExec(_ *cobra.Command, args []string) error {
 	)
 }
 
-func attestationExecWorker(userInput string, writer sbom.Writer) <-chan error {
+func attestationExecWorker(userInput string, ko sign.KeyOpts) <-chan error {
 	errs := make(chan error)
 	go func() {
 		defer close(errs)
@@ -89,42 +95,41 @@ func attestationExecWorker(userInput string, writer sbom.Writer) <-chan error {
 			return
 		}
 
-		// TODO: how to select on user desired format option
+		// TODO: currently forced into only SPDX; allow user to specify
 		bytes, err := syft.Encode(*s, format.SPDXJSONOption)
 		if err != nil {
 			errs <- err
 			return
 		}
 
-		err = generateAttestation(bytes, src)
+		err = generateAttestation(bytes, src, ko)
 		if err != nil {
 			errs <- err
 			return
 		}
-
-		bus.Publish(partybus.Event{
-			Type:  event.Exit,
-			Value: func() error { return writer.Write(*s) },
-		})
 	}()
 	return errs
 }
 
-func generateAttestation(predicate []byte, src *source.Source) error {
+// TODO: context object injection
+func generateAttestation(predicate []byte, src *source.Source, ko sign.KeyOpts) error {
 	predicateType := in_toto.PredicateSPDX
 
 	h, _ := v1.NewHash(src.Image.Metadata.ManifestDigest)
 
-	sv, err := sign.SignerFromKeyOpts()
+	// TODO: inject command context and cert path
+	sv, err := sign.SignerFromKeyOpts(context.Background(), "", ko)
 	if err != nil {
 		return err
 	}
 	defer sv.Close()
+	// TODO: can we include our own types here?
+	wrapped := dsse.WrapSigner(sv, "application/syft.in-toto+json")
 
 	fmt.Fprintln(os.Stderr, "Using generated sbom as payload")
 
 	sh, err := attestation.GenerateStatement(attestation.GenerateOpts{
-		Predicate: predicate,
+		Predicate: bytes.NewBuffer(predicate),
 		Type:      predicateType,
 		Digest:    h.Hex,
 	})
@@ -132,11 +137,17 @@ func generateAttestation(predicate []byte, src *source.Source) error {
 		return err
 	}
 
-	_, err = json.Marshal(sh)
+	payload, err := json.Marshal(sh)
 	if err != nil {
 		return err
 	}
 
+	signedPayload, err := wrapped.SignMessage(bytes.NewReader(payload), signatureoptions.WithContext(context.Background()))
+	if err != nil {
+		return errors.Wrap(err, "signing")
+	}
+
+	fmt.Println(string(signedPayload))
 	return nil
 }
 
