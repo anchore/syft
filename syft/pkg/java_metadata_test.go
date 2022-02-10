8904daed6725e1a9c5eb7092baa2997dc4a7f1ba
Alex Goodman <alex.goodman@anchore.com>
2022-02-10 13:19:16 -0500

    optionally split out npm namespaces for pURL generation
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- syft/pkg/java_metadata_test.go
+++ syft/pkg/java_metadata_test.go
@@ -3,7 +3,6 @@ package pkg
 import (
 	"testing"
 
-	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -107,41 +106,6 @@ func TestPomProperties_PkgTypeIndicated(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			actual := tc.pomProperties.PkgTypeIndicated()
 			assert.Equal(t, tc.expectedType, actual)
-		})
-	}
-}
-
-func TestJavaMetadata_pURL(t *testing.T) {
-	tests := []struct {
-		metadata JavaMetadata
-		expected string
-	}{
-		{
-			metadata: JavaMetadata{
-				PomProperties: &PomProperties{
-					Path:       "p",
-					Name:       "n",
-					GroupID:    "g.id",
-					ArtifactID: "a",
-					Version:    "v",
-				},
-			},
-			expected: "pkg:maven/g.id/a@v",
-		},
-		{
-			metadata: JavaMetadata{},
-			expected: "",
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.expected, func(t *testing.T) {
-			actual := test.metadata.PackageURL(nil)
-			if actual != test.expected {
-				dmp := diffmatchpatch.New()
-				diffs := dmp.DiffMain(test.expected, actual, true)
-				t.Errorf("diff: %s", dmp.DiffPrettyText(diffs))
-			}
 		})
 	}
 }
