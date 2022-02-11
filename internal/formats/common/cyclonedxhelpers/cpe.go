ccebb67b2b1b947e925f397c276105d4178e86a3
Keith Zantow <kzantow@gmail.com>
2022-02-10 21:09:01 -0500

    search/replace typo
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- internal/formats/common/cyclonedxhelpers/cpe.go
+++ internal/formats/common/cyclonedxhelpers/cpe.go
@@ -9,7 +9,7 @@ import (
 
 func encodeCPE(p pkg.Package) string {
 	// Since the CPEs in a package are sorted by specificity
-	// we can extract the first encodeCPE as the one to output in cyclonedx
+	// we can extract the first CPE as the one to output in cyclonedx
 	if len(p.CPEs) > 0 {
 		return pkg.CPEString(p.CPEs[0])
 	}
