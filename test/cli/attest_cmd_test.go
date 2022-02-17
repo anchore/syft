763a161000b154c534cb685241b1488e6b9d8270
Christopher Phillips <christopher.phillips@anchore.com>
2022-02-17 17:24:03 -0500

    configure failing tests
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- test/cli/attest_cmd_test.go
+++ test/cli/attest_cmd_test.go
@@ -1,20 +1,19 @@
 package cli
 
 import (
+	"os"
 	"strings"
 	"testing"
 )
 
 func TestAttestCmdFlags(t *testing.T) {
-	coverageImage := "caphill4/log4j-vulnerability"
-	cleanup := setupPKI(t)
-	defer cleanup()
-
+	coverageImage := "docker-archive:" + getFixtureImage(t, "image-pkg-coverage")
 	tests := []struct {
 		name       string
 		args       []string
 		env        map[string]string
 		assertions []traitAssertion
+		pw         string
 	}{
 		{
 			name: "no-args-shows-help",
@@ -24,6 +23,7 @@ func TestAttestCmdFlags(t *testing.T) {
 				assertInOutput("image or OCI directory as the predicate of an attestation"), // excerpt from help description
 				assertFailingReturnCode,
 			},
+			pw: "",
 		},
 		{
 			name: "can encode syft.json as the predicate",
@@ -32,11 +32,30 @@ func TestAttestCmdFlags(t *testing.T) {
 				assertSuccessfulReturnCode,
 				// assertVerifyAttestation(coverageImage), Follow up on this assertion with verify blog or ephemperal registry
 			},
+			pw: "test",
+		},
+		{
+			name: "does not prompt for a password when pw is empty",
+			args: []string{"attest", "-o", "json", coverageImage},
+			assertions: []traitAssertion{
+				assertSuccessfulReturnCode,
+				// assertVerifyAttestation(coverageImage), Follow up on this assertion with verify blog or ephemperal registry
+			},
+			pw: "",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			cleanup := setupPKI(t, test.pw)
+			defer cleanup()
+
+			if test.pw == "" {
+				// we want to make sure the command succeeds
+				// when the password is blank and the env is unset
+				os.Unsetenv("COSIGN_PASSWORD")
+			}
+
 			cmd, stdout, stderr := runSyft(t, test.env, test.args...)
 			for _, traitFn := range test.assertions {
 				traitFn(t, stdout, stderr, cmd.ProcessState.ExitCode())
