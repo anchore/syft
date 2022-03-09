003d28ad484c7cabff9e9230d558e9a47a83b51d
Christopher Angelo Phillips <32073428+spiffcs@users.noreply.github.com>
2022-03-09 13:12:52 -0500

    Add SchemaVersion to version command output (#877)
    * make JsonSchemaVersion available programmatically via syft version command
    
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- internal/version/build.go
+++ internal/version/build.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"runtime"
 	"strings"
+
+	"github.com/anchore/syft/internal"
 )
 
 const valueNotProvided = "[not provided]"
@@ -20,13 +22,14 @@ var platform = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
 
 // Version defines the application version details (generally from build information)
 type Version struct {
-	Version        string `json:"version"`        // application semantic version
-	GitCommit      string `json:"gitCommit"`      // git SHA at build-time
-	GitDescription string `json:"gitDescription"` // output of 'git describe --dirty --always --tags'
-	BuildDate      string `json:"buildDate"`      // date of the build
-	GoVersion      string `json:"goVersion"`      // go runtime version at build-time
-	Compiler       string `json:"compiler"`       // compiler used at build-time
-	Platform       string `json:"platform"`       // GOOS and GOARCH at build-time
+	Version           string `json:"version"`           // application semantic version
+	JSONSchemaVersion string `json:"jsonSchemaVersion"` // application semantic JSON schema version
+	GitCommit         string `json:"gitCommit"`         // git SHA at build-time
+	GitDescription    string `json:"gitDescription"`    // output of 'git describe --dirty --always --tags'
+	BuildDate         string `json:"buildDate"`         // date of the build
+	GoVersion         string `json:"goVersion"`         // go runtime version at build-time
+	Compiler          string `json:"compiler"`          // compiler used at build-time
+	Platform          string `json:"platform"`          // GOOS and GOARCH at build-time
 }
 
 func (v Version) IsProductionBuild() bool {
@@ -39,12 +42,13 @@ func (v Version) IsProductionBuild() bool {
 // FromBuild provides all version details
 func FromBuild() Version {
 	return Version{
-		Version:        version,
-		GitCommit:      gitCommit,
-		GitDescription: gitDescription,
-		BuildDate:      buildDate,
-		GoVersion:      runtime.Version(),
-		Compiler:       runtime.Compiler,
-		Platform:       platform,
+		Version:           version,
+		JSONSchemaVersion: internal.JSONSchemaVersion,
+		GitCommit:         gitCommit,
+		GitDescription:    gitDescription,
+		BuildDate:         buildDate,
+		GoVersion:         runtime.Version(),
+		Compiler:          runtime.Compiler,
+		Platform:          platform,
 	}
 }
