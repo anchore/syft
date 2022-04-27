9aa9db18c474756cb8709de167e94b13d94fce54
Christian Kotzbauer <git@ckotzbauer.de>
2022-04-27 08:50:23 +0200

    fix: add missing dot
    Signed-off-by: Christian Kotzbauer <git@ckotzbauer.de>

--- syft/pkg/cataloger/dotnet/cataloger.go
+++ syft/pkg/cataloger/dotnet/cataloger.go
@@ -7,7 +7,7 @@ import (
 // NewDotnetDepsCataloger returns a new Dotnet cataloger object base on deps json files.
 func NewDotnetDepsCataloger() *common.GenericCataloger {
 	globParsers := map[string]common.ParserFn{
-		"**/*deps.json": parseDotnetDeps,
+		"**/*.deps.json": parseDotnetDeps,
 	}
 
 	return common.NewGenericCataloger(nil, globParsers, "dotnet-deps-cataloger")
