afdec1642359b78445d87af8c2a9186b492b6526
Morten Linderud <morten@linderud.pw>
2022-05-01 13:58:28 +0200

    alpm: Refactor parser and include digests
    We refactor the parser functions for easier testing. This allows us to
    compare the ALPM metadata struct instead of only having the Package
    struct returned.
    
    Implement file database into the standard parser. There is strictly no
    need to have two parser functions. There are only a few different fields
    in each database and they can both just have the entire package struct
    returned.
    
    Internally we never really care about the file database, but we need the
    backup array.
    
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- syft/pkg/alpm_metadata.go
+++ syft/pkg/alpm_metadata.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/anchore/packageurl-go"
+	"github.com/anchore/syft/syft/file"
 	"github.com/anchore/syft/syft/linux"
 	"github.com/scylladb/go-set/strset"
 )
@@ -12,31 +13,33 @@ import (
 const AlpmDBGlob = "/var/lib/pacman/local/**/desc"
 
 type AlpmMetadata struct {
-	BasePackagestring string           `mapstructure:"base" json:"basepackage"`
-	Package           string           `mapstructure:"name" json:"package"`
-	Version           string           `mapstructure:"version" json:"version"`
-	Description       string           `mapstructure:"desc" json:"description"`
-	Architecture      string           `mapstructure:"arch" json:"architecture"`
-	Size              int              `mapstructure:"size" json:"size" cyclonedx:"size"`
-	Packager          string           `mapstructure:"packager" json:"packager"`
-	License           string           `mapstructure:"license" json:"license"`
-	Url               string           `mapstructure:"url" json:"url"`
-	Validation        string           `mapstructure:"validation" json:"validation"`
-	Reason            int              `mapstructure:"reason" json:"reason"`
-	Files             []AlpmFileRecord `json:"files"`
+	BasePackage  string           `mapstructure:"base" json:"basepackage"`
+	Package      string           `mapstructure:"name" json:"package"`
+	Version      string           `mapstructure:"version" json:"version"`
+	Description  string           `mapstructure:"desc" json:"description"`
+	Architecture string           `mapstructure:"arch" json:"architecture"`
+	Size         int              `mapstructure:"size" json:"size" cyclonedx:"size"`
+	Packager     string           `mapstructure:"packager" json:"packager"`
+	License      string           `mapstructure:"license" json:"license"`
+	Url          string           `mapstructure:"url" json:"url"`
+	Validation   string           `mapstructure:"validation" json:"validation"`
+	Reason       int              `mapstructure:"reason" json:"reason"`
+	Files        []AlpmFileRecord `mapstructure:"files" json:"files"`
+	Backup       []AlpmFileRecord `mapstructure:"backup" json:"backup"`
 }
 
 // TODO: Implement mtree support
 type AlpmFileRecord struct {
-	Path         string    `mapstruture:"path" json:"path"`
-	Type         string    `mapstructure:"type" json:"type,omitempty"`
-	Uid          string    `mapstructure:"uid" json:"uid,omitempty"`
-	Gid          string    `mapstructure:"gid" json:"gid,omitempty"`
-	Time         time.Time `mapstructure:"time" json:"time,omitempty"`
-	Size         string    `mapstructure:"size" json:"size,omitempty"`
-	Md5digest    string    `mapstructure:"md5digest" json:"md5digest,omitempty"`
-	Sha256digest string    `mapstructure:"sha256digest" json:"sh256digest,omitempty"`
-	Link         string    `mapstructure:"link" json:"link,omitempty"`
+	Path         string       `mapstruture:"path" json:"path"`
+	Type         string       `mapstructure:"type" json:"type,omitempty"`
+	Uid          string       `mapstructure:"uid" json:"uid,omitempty"`
+	Gid          string       `mapstructure:"gid" json:"gid,omitempty"`
+	Time         time.Time    `mapstructure:"time" json:"time,omitempty"`
+	Size         string       `mapstructure:"size" json:"size,omitempty"`
+	Md5digest    string       `mapstructure:"md5digest" json:"md5digest,omitempty"`
+	Sha256digest string       `mapstructure:"sha256digest" json:"sh256digest,omitempty"`
+	Link         string       `mapstructure:"link" json:"link,omitempty"`
+	Digest       *file.Digest `json:"digest,omitempty"`
 }
 
 // PackageURL returns the PURL for the specific Arch Linux package (see https://github.com/package-url/purl-spec)
