b81b639601cfd8e0cbc7b119aeae96f653938b17
Christopher Phillips <christopher.phillips@anchore.com>
2022-05-04 16:02:43 -0400

    remove all poweruser commands
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- test/cli/power_user_cmd_test.go
+++ test/cli/power_user_cmd_test.go
@@ -32,52 +32,6 @@ func TestPowerUserCmdFlags(t *testing.T) {
 				assertSuccessfulReturnCode,
 			},
 		},
-		{
-			name: "defaut-secrets-results-w-reveal-values",
-			env: map[string]string{
-				"SYFT_SECRETS_REVEAL_VALUES": "true",
-			},
-			args: []string{"power-user", "docker-archive:" + getFixtureImage(t, "image-secrets"), "-vv"},
-			assertions: []traitAssertion{
-				assertInOutput(`"classification": "generic-api-key"`),                            // proof of the secrets cataloger finding something
-				assertInOutput(`"12345A7a901b345678901234567890123456789012345678901234567890"`), // proof of the secrets cataloger finding the api key
-				assertSuccessfulReturnCode,
-			},
-		},
-		{
-			name: "content-cataloger-wired-up",
-			args: []string{"power-user", "docker-archive:" + getFixtureImage(t, "image-secrets"), "-vv"},
-			env: map[string]string{
-				"SYFT_FILE_CONTENTS_GLOBS": "/api-key.txt",
-			},
-			assertions: []traitAssertion{
-				assertInOutput(`"contents": "c29tZV9BcEkta0V5ID0gIjEyMzQ1QTdhOTAxYjM0NTY3ODkwMTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MCIK"`), // proof of the content cataloger
-				assertSuccessfulReturnCode,
-			},
-		},
-		{
-			name: "default-dir-results-w-pkg-coverage",
-			args: []string{"power-user", "dir:test-fixtures/image-pkg-coverage", "-vv"},
-			assertions: []traitAssertion{
-				assertNotInOutput(" command is deprecated"),     // only the root command should be deprecated
-				assertInOutput(`"type": "RegularFile"`),         // proof of file-metadata data
-				assertInOutput(`"algorithm": "sha256"`),         // proof of file-metadata default digest algorithm of sha256
-				assertInOutput(`"metadataType": "ApkMetadata"`), // proof of package artifacts data
-				assertSuccessfulReturnCode,
-			},
-		},
-		{
-			name: "default-secrets-dir-results-w-reveal-values",
-			env: map[string]string{
-				"SYFT_SECRETS_REVEAL_VALUES": "true",
-			},
-			args: []string{"power-user", "dir:test-fixtures/image-secrets", "-vv"},
-			assertions: []traitAssertion{
-				assertInOutput(`"classification": "generic-api-key"`),                            // proof of the secrets cataloger finding something
-				assertInOutput(`"12345A7a901b345678901234567890123456789012345678901234567890"`), // proof of the secrets cataloger finding the api key
-				assertSuccessfulReturnCode,
-			},
-		},
 	}
 
 	for _, test := range tests {
