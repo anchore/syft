f517f763ee63ad3e7225a74a18d92eecd6375e01
Christopher Phillips <christopher.phillips@anchore.com>
2022-05-11 16:49:12 -0400

    failing test with assertions added
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- test/cli/cyclonedx_valid_test.go
+++ test/cli/cyclonedx_valid_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/anchore/stereoscope/pkg/imagetest"
 )
 
+// We have schema validation mechanims in schema/cyclonedx/
+// This test allows us to double check that validation against the cyclonedx-cli tool
 func TestValidCycloneDX(t *testing.T) {
 	imageFixture := func(t *testing.T) string {
 		fixtureImageName := "image-pkg-coverage"
@@ -16,17 +18,23 @@ func TestValidCycloneDX(t *testing.T) {
 		return "docker-archive:" + tarPath
 	}
 
+	// TODO update image to exercise entire cyclonedx schema
 	tests := []struct {
 		name       string
 		subcommand string
 		args       []string
 		fixture    func(*testing.T) string
+		assertions []traitAssertion
 	}{
 		{
 			name:       "validate cyclonedx output",
 			subcommand: "packages",
 			args:       []string{"-o", "cyclonedx-json"},
 			fixture:    imageFixture,
+			assertions: []traitAssertion{
+				assertSuccessfulReturnCode,
+				assertValidCycloneDX,
+			},
 		},
 	}
 
@@ -41,12 +49,56 @@ func TestValidCycloneDX(t *testing.T) {
 			}
 
 			cmd, stdout, stderr := runSyft(t, nil, args...)
-			t.Log("STDOUT:\n", stdout)
-			t.Log("STDERR:\n", stderr)
-			t.Log("COMMAND:", strings.Join(cmd.Args, " "))
+			for _, traitFn := range test.assertions {
+				traitFn(t, stdout, stderr, cmd.ProcessState.ExitCode())
+			}
+			if t.Failed() {
+				t.Log("STDOUT:\n", stdout)
+				t.Log("STDERR:\n", stderr)
+				t.Log("COMMAND:", strings.Join(cmd.Args, " "))
+			}
 
 			validateCycloneDXJSON(t, stdout)
 		})
+	}
+}
+
+func assertValidCycloneDX(tb testing.TB, stdout, stderr string, rc int) {
+	tb.Helper()
+	f, err := os.CreateTemp("", "tmpfile-")
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	// close and remove the temporary file at the end of the program
+	defer f.Close()
+	defer os.Remove(f.Name())
+
+	data := []byte(stdout)
+
+	if _, err := f.Write(data); err != nil {
+		tb.Fatal(err)
+	}
+
+	args := []string{
+		"validate",
+		"--input-format",
+		"json",
+		"--input-version",
+		"v1_4",
+		"--input-file",
+		"/sbom",
+	}
+
+	cmd, stdout, stderr := runCycloneDXInDocker(tb, nil, "cyclonedx/cyclonedx-cli", f, args...)
+	if cmd.ProcessState.ExitCode() != 0 {
+		tb.Errorf("expected no validation failures for cyclonedx-cli but got rc=%d", rc)
+	}
+
+	if tb.Failed() {
+		tb.Log("STDOUT:\n", stdout)
+		tb.Log("STDERR:\n", stderr)
+		tb.Log("COMMAND:", strings.Join(cmd.Args, " "))
 	}
 }
 
@@ -78,7 +130,13 @@ func validateCycloneDXJSON(t *testing.T, stdout string) {
 	}
 
 	cmd, stdout, stderr := runCycloneDXInDocker(t, nil, "cyclonedx/cyclonedx-cli", f, args...)
-	t.Log("STDOUT:\n", stdout)
-	t.Log("STDERR:\n", stderr)
-	t.Log("COMMAND:", strings.Join(cmd.Args, " "))
+	if strings.Contains(stdout, "BOM is not valid") {
+		t.Errorf("expected no validation failures for cyclonedx-cli but found invalid BOM")
+	}
+
+	if t.Failed() {
+		t.Log("STDOUT:\n", stdout)
+		t.Log("STDERR:\n", stderr)
+		t.Log("COMMAND:", strings.Join(cmd.Args, " "))
+	}
 }
