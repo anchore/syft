da58263499d3741dbfecc66a09cfc08ec693e52f
Christopher Phillips <christopher.phillips@anchore.com>
2022-07-06 15:44:58 -0400

    generate new schema with gentoo addition
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- test/cli/packages_cmd_test.go
+++ test/cli/packages_cmd_test.go
@@ -96,7 +96,7 @@ func TestPackagesCmdFlags(t *testing.T) {
 			name: "squashed-scope-flag",
 			args: []string{"packages", "-o", "json", "-s", "squashed", coverageImage},
 			assertions: []traitAssertion{
-				assertPackageCount(33),
+				assertPackageCount(34),
 				assertSuccessfulReturnCode,
 			},
 		},
