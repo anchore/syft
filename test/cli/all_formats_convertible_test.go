e654fab3ac9f8a3dff84cf79610e774a4e7a7829
Christopher Phillips <christopher.phillips@anchore.com>
2022-08-16 13:47:20 -0400

    add failing test
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

---
+++ test/cli/all_formats_convertible_test.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestConvertCmdFlags(t *testing.T) {
+	tests := []struct {
+		name       string
+		args       []string
+		env        map[string]string
+		assertions []traitAssertion
+	}{
+		{
+			name: "no-args-shows-help",
+			args: []string{"convert"},
+			assertions: []traitAssertion{
+				assertInOutput("Convert SBOM files to, and from, SPDX, CycloneDX and Syft's format"),
+				assertInOutput("FAILURE"),
+				assertFailingReturnCode,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			cmd, stdout, stderr := runSyft(t, test.env, test.args...)
+			for _, traitFn := range test.assertions {
+				traitFn(t, stdout, stderr, cmd.ProcessState.ExitCode())
+			}
+			if t.Failed() {
+				t.Log("STDOUT:\n", stdout)
+				t.Log("STDERR:\n", stderr)
+				t.Log("COMMAND:", strings.Join(cmd.Args, " "))
+			}
+		})
+	}
+}
