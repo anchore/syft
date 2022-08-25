e22fafff47c8032b1d3d28dc15f5ca28cce0c570
Christopher Phillips <christopher.phillips@anchore.com>
2022-08-25 14:45:39 -0400

    add all cases to test
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- test/cli/all_formats_convertible_test.go
+++ test/cli/all_formats_convertible_test.go
@@ -6,6 +6,11 @@ import (
 	"testing"
 )
 
+type conversion struct {
+	To   string
+	From string
+}
+
 func TestConvertCmdFlags(t *testing.T) {
 	commonAssertions := []traitAssertion{
 		func(tb testing.TB, stdout, _ string, _ int) {
@@ -18,51 +23,58 @@ func TestConvertCmdFlags(t *testing.T) {
 	}
 
 	tests := []struct {
-		name       string
-		base       string
-		convert    string
-		env        map[string]string
-		assertions []traitAssertion
+		name        string
+		conversions []conversion
+		env         map[string]string
+		assertions  []traitAssertion
 	}{
 		{
-			name:       "syft-format convertable to spdx-json",
-			base:       "syft-json",
-			convert:    "spdx-json",
+			name: "syft-format convertable to spdx-json",
+			conversions: []conversion{
+				{To: "syft-json", From: "spdx-json"},
+				{To: "syft-json", From: "cyclonedx-json"},
+				{To: "spdx-json", From: "syft-json"},
+				{To: "spdx-json", From: "cyclonedx-json"},
+				{To: "cyclonedx-json", From: "syft-json"},
+				{To: "cyclonedx-json", From: "spdx-json"},
+			},
 			assertions: commonAssertions,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			sbomArgs := []string{"dir:./test-fixtures/image-pkg-coverage", "-o", test.base}
-			cmd, stdout, stderr := runSyft(t, test.env, sbomArgs...)
-			if cmd.ProcessState.ExitCode() != 0 {
-				t.Fatalf("failure executing syft creating an sbom")
-				t.Log("STDOUT:\n", stdout)
-				t.Log("STDERR:\n", stderr)
-				t.Log("COMMAND:", strings.Join(cmd.Args, " "))
-				return
-			}
+			for _, c := range test.conversions {
+				sbomArgs := []string{"dir:./test-fixtures/image-pkg-coverage", "-o", c.From}
+				cmd, stdout, stderr := runSyft(t, test.env, sbomArgs...)
+				if cmd.ProcessState.ExitCode() != 0 {
+					t.Fatalf("failure executing syft creating an sbom")
+					t.Log("STDOUT:\n", stdout)
+					t.Log("STDERR:\n", stderr)
+					t.Log("COMMAND:", strings.Join(cmd.Args, " "))
+					return
+				}
 
-			f, err := os.CreateTemp("", "temp_sbom")
-			if err != nil {
-				t.Fatalf("could not create temp sbom file for convert: %s", err)
-			}
-			defer os.Remove(f.Name()) // clean up temp file
+				f, err := os.CreateTemp("", "temp_sbom")
+				if err != nil {
+					t.Fatalf("could not create temp sbom file for convert: %s", err)
+				}
+				defer os.Remove(f.Name()) // clean up temp file
 
-			if _, err := f.Write([]byte(stdout)); err != nil {
-				t.Fatalf("could not write temp sbom for convert: %s", err)
-			}
+				if _, err := f.Write([]byte(stdout)); err != nil {
+					t.Fatalf("could not write temp sbom for convert: %s", err)
+				}
 
-			convertArgs := []string{"convert", f.Name(), "-o", test.convert}
-			cmd, stdout, stderr = runSyft(t, test.env, convertArgs...)
-			for _, traitFn := range test.assertions {
-				traitFn(t, stdout, stderr, cmd.ProcessState.ExitCode())
-			}
-			if t.Failed() {
-				t.Log("STDOUT:\n", stdout)
-				t.Log("STDERR:\n", stderr)
-				t.Log("COMMAND:", strings.Join(cmd.Args, " "))
+				convertArgs := []string{"convert", f.Name(), "-o", c.To}
+				cmd, stdout, stderr = runSyft(t, test.env, convertArgs...)
+				for _, traitFn := range test.assertions {
+					traitFn(t, stdout, stderr, cmd.ProcessState.ExitCode())
+				}
+				if t.Failed() {
+					t.Log("STDOUT:\n", stdout)
+					t.Log("STDERR:\n", stderr)
+					t.Log("COMMAND:", strings.Join(cmd.Args, " "))
+				}
 			}
 		})
 	}
