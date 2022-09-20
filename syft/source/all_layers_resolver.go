438c6657a8354ad98defec39df6b71b5984c1d4c
Keith Zantow <kzantow@gmail.com>
2022-09-20 16:14:24 -0400

    fix: follow symlinks when searching for globs in all-layers scope
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- syft/source/all_layers_resolver.go
+++ syft/source/all_layers_resolver.go
@@ -120,18 +120,28 @@ func (r *allLayersResolver) FilesByPath(paths ...string) ([]Location, error) {
 }
 
 // FilesByGlob returns all file.References that match the given path glob pattern from any layer in the image.
+//
+//nolint:gocognit
 func (r *allLayersResolver) FilesByGlob(patterns ...string) ([]Location, error) {
 	uniqueFileIDs := file.NewFileReferenceSet()
 	uniqueLocations := make([]Location, 0)
 
 	for _, pattern := range patterns {
 		for idx, layerIdx := range r.layers {
-			results, err := r.img.Layers[layerIdx].Tree.FilesByGlob(pattern, filetree.FollowBasenameLinks, filetree.DoNotFollowDeadBasenameLinks)
+			results, err := r.img.Layers[layerIdx].SquashedTree.FilesByGlob(pattern, filetree.FollowBasenameLinks, filetree.DoNotFollowDeadBasenameLinks)
 			if err != nil {
 				return nil, fmt.Errorf("failed to resolve files by glob (%s): %w", pattern, err)
 			}
 
 			for _, result := range results {
+				found, _, err := r.img.Layers[layerIdx].Tree.File(result.RealPath)
+				if err != nil {
+					return nil, fmt.Errorf("failed to resolve file during glob resolution %s: %w", result.RealPath, err)
+				}
+				if !found {
+					// this file is not present in the layer
+					continue
+				}
 				// don't consider directories (special case: there is no path information for /)
 				if result.RealPath == "/" {
 					continue
---
+++ test/integration/all_layers_squashed_comparison_test.go
@@ -0,0 +1,20 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/anchore/syft/syft/source"
+)
+
+func Test_AllLayersIncludesSquashed(t *testing.T) {
+	// This is a verification test for issue #894 (https://github.com/anchore/syft/issues/894)
+	allLayers, _ := catalogFixtureImage(t, "image-suse-all-layers", source.AllLayersScope, nil)
+	squashed, _ := catalogFixtureImage(t, "image-suse-all-layers", source.SquashedScope, nil)
+
+	lenAllLayers := len(allLayers.Artifacts.PackageCatalog.Sorted())
+	lenSquashed := len(squashed.Artifacts.PackageCatalog.Sorted())
+
+	if lenAllLayers < lenSquashed {
+		t.Errorf("squashed has more packages than all-layers: %d > %d", lenSquashed, lenAllLayers)
+	}
+}
