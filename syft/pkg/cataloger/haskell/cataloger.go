9b533f2379f50409908d9477361708692996ec01
Alex Goodman <alex.goodman@anchore.com>
2022-10-25 13:43:47 -0400

    port haskell cataloger to new generic cataloger pattern
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- syft/pkg/cataloger/haskell/cataloger.go
+++ syft/pkg/cataloger/haskell/cataloger.go
@@ -1,15 +1,17 @@
 package haskell
 
 import (
-	"github.com/anchore/syft/syft/pkg/cataloger/common"
+	"github.com/anchore/syft/syft/pkg/cataloger/generic"
 )
 
+// TODO: it seems that the stack.yaml/stack.lock/cabal.project.freeze have different purposes and could have different installation intentions
+// (some describe intent and are meant to be used by a tool to resolve more dependencies while others describe the actual installed state).
+// This hints at splittin these into multiple catalogers, but for now we'll keep them together.
+
 // NewHackageCataloger returns a new Haskell cataloger object.
-func NewHackageCataloger() *common.GenericCataloger {
-	globParsers := map[string]common.ParserFn{
-		"**/stack.yaml":           parseStackYaml,
-		"**/stack.yaml.lock":      parseStackLock,
-		"**/cabal.project.freeze": parseCabalFreeze,
-	}
-	return common.NewGenericCataloger(nil, globParsers, "hackage-cataloger")
+func NewHackageCataloger() *generic.Cataloger {
+	return generic.NewCataloger("haskell-cataloger").
+		WithParserByGlobs(parseStackYaml, "**/stack.yaml").
+		WithParserByGlobs(parseStackLock, "**/stack.yaml.lock").
+		WithParserByGlobs(parseCabalFreeze, "**/cabal.project.freeze")
 }
--- syft/pkg/cataloger/haskell/parse_cabal_freeze_test.go
+++ syft/pkg/cataloger/haskell/parse_cabal_freeze_test.go
@@ -1,152 +1,111 @@
 package haskell
 
 import (
-	"os"
 	"testing"
 
-	"github.com/go-test/deep"
-
+	"github.com/anchore/syft/syft/artifact"
 	"github.com/anchore/syft/syft/pkg"
+	"github.com/anchore/syft/syft/pkg/cataloger/internal/pkgtest"
+	"github.com/anchore/syft/syft/source"
 )
 
 func TestParseCabalFreeze(t *testing.T) {
-	expected := []*pkg.Package{
+	fixture := "test-fixtures/cabal.project.freeze"
+	locationSet := source.NewLocationSet(source.NewLocation(fixture))
+
+	expectedPkgs := []pkg.Package{
 		{
-			Name:         "Cabal",
-			Version:      "3.2.1.0",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "Cabal",
-				Version: "3.2.1.0",
-			},
+			Name:      "Cabal",
+			Version:   "3.2.1.0",
+			PURL:      "pkg:hackage/Cabal@3.2.1.0",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "Diff",
-			Version:      "0.4.1",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "Diff",
-				Version: "0.4.1",
-			},
+			Name:      "Diff",
+			Version:   "0.4.1",
+			PURL:      "pkg:hackage/Diff@0.4.1",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "HTTP",
-			Version:      "4000.3.16",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "HTTP",
-				Version: "4000.3.16",
-			},
+			Name:      "HTTP",
+			Version:   "4000.3.16",
+			PURL:      "pkg:hackage/HTTP@4000.3.16",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "HUnit",
-			Version:      "1.6.2.0",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "HUnit",
-				Version: "1.6.2.0",
-			},
+			Name:      "HUnit",
+			Version:   "1.6.2.0",
+			PURL:      "pkg:hackage/HUnit@1.6.2.0",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "OneTuple",
-			Version:      "0.3.1",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "OneTuple",
-				Version: "0.3.1",
-			},
+			Name:      "OneTuple",
+			Version:   "0.3.1",
+			PURL:      "pkg:hackage/OneTuple@0.3.1",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "Only",
-			Version:      "0.1",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "Only",
-				Version: "0.1",
-			},
+			Name:      "Only",
+			Version:   "0.1",
+			PURL:      "pkg:hackage/Only@0.1",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "PyF",
-			Version:      "0.10.2.0",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "PyF",
-				Version: "0.10.2.0",
-			},
+			Name:      "PyF",
+			Version:   "0.10.2.0",
+			PURL:      "pkg:hackage/PyF@0.10.2.0",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "QuickCheck",
-			Version:      "2.14.2",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "QuickCheck",
-				Version: "2.14.2",
-			},
+			Name:      "QuickCheck",
+			Version:   "2.14.2",
+			PURL:      "pkg:hackage/QuickCheck@2.14.2",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "RSA",
-			Version:      "2.4.1",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "RSA",
-				Version: "2.4.1",
-			},
+			Name:      "RSA",
+			Version:   "2.4.1",
+			PURL:      "pkg:hackage/RSA@2.4.1",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "SHA",
-			Version:      "1.6.4.4",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "SHA",
-				Version: "1.6.4.4",
-			},
+			Name:      "SHA",
+			Version:   "1.6.4.4",
+			PURL:      "pkg:hackage/SHA@1.6.4.4",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 		{
-			Name:         "Spock",
-			Version:      "0.14.0.0",
-			Language:     pkg.Haskell,
-			Type:         pkg.HackagePkg,
-			MetadataType: pkg.HackageMetadataType,
-			Metadata: pkg.HackageMetadata{
-				Name:    "Spock",
-				Version: "0.14.0.0",
-			},
+			Name:      "Spock",
+			Version:   "0.14.0.0",
+			PURL:      "pkg:hackage/Spock@0.14.0.0",
+			Locations: locationSet,
+			Language:  pkg.Haskell,
+			Type:      pkg.HackagePkg,
 		},
 	}
 
-	fixture, err := os.Open("test-fixtures/cabal.project.freeze")
-	if err != nil {
-		t.Fatalf("failed to open fixture: %+v", err)
-	}
+	// TODO: relationships are not under test yet
+	var expectedRelationships []artifact.Relationship
 
-	// TODO: no relationships are under test yet
-	actual, _, err := parseCabalFreeze(fixture.Name(), fixture)
-	if err != nil {
-		t.Error(err)
-	}
-
-	differences := deep.Equal(expected, actual)
-	if differences != nil {
-		t.Errorf("returned package list differed from expectation: %+v", differences)
-	}
+	pkgtest.TestFileParser(t, fixture, parseCabalFreeze, expectedPkgs, expectedRelationships)
 }
