9c57f183b9c406666f67fddb962701b255230c06
Vandana Akhramka <vandana.akhramka@metaswitch.com>
2022-11-03 14:46:17 +0000

    sign off
    Signed-off-by: Vandana Akhramka <vandana.akhramka@metaswitch.com>

--- syft/formats/common/spdxhelpers/to_syft_model.go
+++ syft/formats/common/spdxhelpers/to_syft_model.go
@@ -355,6 +355,7 @@ func extractMetadata(p *spdx.Package2_2, info pkgInfo) (pkg.MetadataType, interf
 			ArchiveDigests: digests,
 			VirtualPath: p.PackageSourceInfo
 		}
+		
 	case pkg.GoModulePkg:
 		var h1Digest string
 		for _, value := range p.PackageChecksums {
