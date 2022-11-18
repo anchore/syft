67888ee855ad37948bfee98e3aa7d592b4f91e95
Christopher Angelo Phillips <32073428+spiffcs@users.noreply.github.com>
2022-11-18 09:45:18 -0500

    1111 clean name bug (#1347)

--- syft/formats/common/spdxhelpers/document_name.go
+++ syft/formats/common/spdxhelpers/document_name.go
@@ -1,34 +1,20 @@
 package spdxhelpers
 
 import (
-	"path"
-	"strings"
-
 	"github.com/anchore/syft/syft/source"
 )
 
 func DocumentName(srcMetadata source.Metadata) string {
 	if srcMetadata.Name != "" {
-		return cleanName(srcMetadata.Name)
+		return srcMetadata.Name
 	}
 
 	switch srcMetadata.Scheme {
 	case source.ImageScheme:
-		return cleanName(srcMetadata.ImageMetadata.UserInput)
+		return srcMetadata.ImageMetadata.UserInput
 	case source.DirectoryScheme, source.FileScheme:
-		return cleanName(srcMetadata.Path)
+		return srcMetadata.Path
 	default:
 		return "unknown"
 	}
-}
-
-func cleanName(name string) string {
-	// remove # according to specification
-	name = strings.ReplaceAll(name, "#", "-")
-
-	// remove : for url construction
-	name = strings.ReplaceAll(name, ":", "-")
-
-	// clean relative pathing
-	return path.Clean(name)
 }
