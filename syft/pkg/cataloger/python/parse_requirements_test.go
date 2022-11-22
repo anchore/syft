a3d67e10ff3d406215d67efeb3cd9a7c60ef3c02
Christopher Phillips <christopher.phillips@anchore.com>
2022-11-22 09:25:12 -0500

    test: add new case to requirements.txt
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- syft/pkg/cataloger/python/parse_requirements_test.go
+++ syft/pkg/cataloger/python/parse_requirements_test.go
@@ -37,6 +37,14 @@ func TestParseRequirementsTxt(t *testing.T) {
 			Language:  pkg.Python,
 			Type:      pkg.PythonPkg,
 		},
+		{
+			Name:      "argh",
+			Version:   "0.26.2",
+			PURL:      "pkg:pypi/argh@0.26.2",
+			Locations: locations,
+			Language:  pkg.Python,
+			Type:      pkg.PythonPkg,
+		},
 	}
 
 	var expectedRelationships []artifact.Relationship
