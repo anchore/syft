19f029919f7538697259d8979f4fc8ef0463e2a8
Keith Zantow <kzantow@gmail.com>
2022-12-21 14:12:14 -0500

    fix: unicode output in cyclonedx-json format
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- syft/formats/cyclonedxjson/encoder.go
+++ syft/formats/cyclonedxjson/encoder.go
@@ -1,6 +1,8 @@
 package cyclonedxjson
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 
 	"github.com/CycloneDX/cyclonedx-go"
@@ -11,9 +13,18 @@ import (
 
 func encoder(output io.Writer, s sbom.SBOM) error {
 	bom := cyclonedxhelpers.ToFormatModel(s)
-	enc := cyclonedx.NewBOMEncoder(output, cyclonedx.BOMFileFormatJSON)
-	enc.SetPretty(true)
+	return Encode(bom, output)
+}
 
-	err := enc.Encode(bom)
-	return err
+// Encode encodes the CycloneDX BOM to JSON, exported so Grype can use this
+func Encode(bom *cyclonedx.BOM, output io.Writer) error {
+	if bom.SpecVersion < cyclonedx.SpecVersion1_2 {
+		return fmt.Errorf("json format is not supported for specification versions lower than %s", cyclonedx.SpecVersion1_2)
+	}
+
+	e := json.NewEncoder(output)
+	e.SetIndent("", " ")
+	e.SetEscapeHTML(false)
+
+	return e.Encode(bom)
 }
