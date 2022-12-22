2a85dc35ede2e97f5113e8b5887dca63f07fed56
Keith Zantow <kzantow@gmail.com>
2022-12-22 18:53:49 -0500

    chore: test redactions
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- syft/formats/spdxjson/encoder_test.go
+++ syft/formats/spdxjson/encoder_test.go
@@ -48,11 +48,11 @@ func TestSPDXRelationshipOrder(t *testing.T) {
 
 func spdxJsonRedactor(s []byte) []byte {
 	// each SBOM reports the time it was generated, which is not useful during snapshot testing
-	s = regexp.MustCompile(`"created":\s+"[^"]",?`).ReplaceAll(s, []byte(""))
+	s = regexp.MustCompile(`"created":\s+"[^"]*",?`).ReplaceAll(s, []byte(""))
 
 	// each SBOM reports a unique documentNamespace when generated, this is not useful for snapshot testing
-	s = regexp.MustCompile(`"documentNamespace":\s+"[^"]",?`).ReplaceAll(s, []byte(""))
+	s = regexp.MustCompile(`"documentNamespace":\s+"[^"]*",?`).ReplaceAll(s, []byte(""))
 
 	// the license list will be updated periodically, the value here should not be directly tested in snapshot tests
-	return regexp.MustCompile(`"licenseListVersion":\s+"[^"]",?`).ReplaceAll(s, []byte(""))
+	return regexp.MustCompile(`"licenseListVersion":\s+"[^"]*",?`).ReplaceAll(s, []byte(""))
 }
