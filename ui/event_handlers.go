78565cb88d783fb0b6e1ec343e42c09adcc879b2
Christopher Phillips <christopher.phillips@anchore.com>
2023-01-09 15:22:54 -0500

    wip
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- ui/event_handlers.go
+++ ui/event_handlers.go
@@ -558,6 +558,15 @@ func ShellOutputHandler(ctx context.Context, fr *frame.Frame, event partybus.Eve
 				// TODO
 			}
 		}
+		b := l.Back()
+		tlogLine := b.Value.(*frame.Line)
+		tEntry := tlogLine.String()
+		for e := l.Front(); e != nil; e = e.Next() {
+			line := e.Value.(*frame.Line)
+			line.Remove()
+		}
+		line, _ := fr.Append()
+		_, _ = line.Write([]byte(fmt.Sprintf("finished cosign execution; %s", tEntry)))
 	}()
 	return nil
 }
