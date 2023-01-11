353d0013905ee50920508c1a7627c125af3e3527
gnmahanth <mahanth@deepfence.io>
2023-01-11 14:36:43 +0000

    remove memory profile code from cmd/syft/main.go
    Signed-off-by: gnmahanth <mahanth@deepfence.io>

--- cmd/syft/main.go
+++ cmd/syft/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
-	"runtime"
-	"runtime/pprof"
 
 	"github.com/anchore/syft/cmd/syft/cli"
 )
@@ -17,14 +14,5 @@ func main() {
 
 	if err := cli.Execute(); err != nil {
 		log.Fatalf("error during command execution: %v", err)
-	}
-	f, err := os.Create("mem.pprof")
-	if err != nil {
-		log.Fatal("could not create memory profile: ", err)
-	}
-	defer f.Close() // error handling omitted for example
-	runtime.GC()    // get up-to-date statistics
-	if err := pprof.WriteHeapProfile(f); err != nil {
-		log.Fatal("could not write memory profile: ", err)
 	}
 }
