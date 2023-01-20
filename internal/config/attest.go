81f8fedf4d524645bc1a809a936c93e6e6a75b80
Christopher Phillips <christopher.phillips@anchore.com>
2023-01-20 14:39:14 -0500

    fix: rouge options surrounding hard PKI
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- internal/config/attest.go
+++ internal/config/attest.go
@@ -3,9 +3,11 @@ package config
 import "github.com/spf13/viper"
 
 type attest struct {
-	key string `yaml:"key" json:"key" mapstructure:"key"`
+	Key      string `yaml:"key" json:"key" mapstructure:"key"`
+	Password string `yaml:"password" json:"password" mapstructure:"password"`
 }
 
 func (cfg attest) loadDefaultValues(v *viper.Viper) {
-	v.SetDefault("attest.key", "key.pub")
+	v.SetDefault("attest.key", "")
+	v.SetDefault("attest.password", "")
 }
