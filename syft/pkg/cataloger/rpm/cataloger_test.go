cbb63e7185fc74d6f163dadccf500911b7a7a889
Alex Goodman <alex.goodman@anchore.com>
2023-01-25 13:57:53 -0500

    add glob-based cataloger tests for rpm cataloger
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

---
+++ syft/pkg/cataloger/rpm/cataloger_test.go
@@ -0,0 +1,60 @@
+package rpm
+
+import (
+	"testing"
+
+	"github.com/anchore/syft/syft/pkg/cataloger/internal/pkgtest"
+)
+
+func Test_DBCataloger_Globs(t *testing.T) {
+	tests := []struct {
+		name     string
+		fixture  string
+		expected []string
+	}{
+		{
+			name:    "obtain DB files",
+			fixture: "test-fixtures/glob-paths",
+			expected: []string{
+				"var/lib/rpm/Packages",
+				"var/lib/rpm/Packages.db",
+				"var/lib/rpm/rpmdb.sqlite",
+				"var/lib/rpmmanifest/container-manifest-2",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			pkgtest.NewCatalogTester().
+				FromDirectory(t, test.fixture).
+				ExpectsContentQueries(test.expected).
+				TestCataloger(t, NewRpmDBCataloger())
+		})
+	}
+}
+
+func Test_RPMFileCataloger_Globs(t *testing.T) {
+	tests := []struct {
+		name     string
+		fixture  string
+		expected []string
+	}{
+		{
+			name:    "obtain rpm files",
+			fixture: "test-fixtures/glob-paths",
+			expected: []string{
+				"dive-0.10.0.rpm",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			pkgtest.NewCatalogTester().
+				FromDirectory(t, test.fixture).
+				ExpectsContentQueries(test.expected).
+				TestCataloger(t, NewFileCataloger())
+		})
+	}
+}
