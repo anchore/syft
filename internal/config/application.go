b88f11d64c1c0a56c54201c9e79a9711d557100f
Christopher Phillips <christopher.phillips@anchore.com>
2023-03-20 10:20:54 -0400

    refactor: update to deprecate behavior pre v1.0.0
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- internal/config/application.go
+++ internal/config/application.go
@@ -225,9 +225,18 @@ func loadConfig(v *viper.Viper, configPath string) error {
 	// start searching for valid configs in order...
 	// 1. look for .<appname>.yaml (in the current directory)
 	confFilePath := "." + internal.ApplicationName
+
+	// TODO: Remove this before v1.0.0
+	// See syft #1634
+	v.AddConfigPath(".")
+	v.SetConfigName(confFilePath)
+
+	// check if config.yaml exists in the current directory
+	// DEPRECATED: this will be removed in v1.0.0
+	if _, err := os.Stat("config.yaml"); err == nil {
+		log.Warn("DEPRECATED: config.yaml as a configuration file is deprecated and will be removed as an option in v1.0.0, please rename to .syft.yaml")
+	}
 	if _, err := os.Stat(confFilePath); err == nil {
-		v.AddConfigPath(".")
-		v.SetConfigName(confFilePath)
 		if err = v.ReadInConfig(); err == nil {
 			v.Set("config", v.ConfigFileUsed())
 			return nil
