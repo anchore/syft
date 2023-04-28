3b581c77731f3e7c06a0b7fbc0aa1ab9dc958bc1
Christopher Phillips <christopher.phillips@anchore.com>
2023-04-28 11:31:39 -0400

    chore: add sorting tests for licenses
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- syft/pkg/license_test.go
+++ syft/pkg/license_test.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -27,4 +28,69 @@ func Test_Hash(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, hash1, hash2)
+}
+
+func Test_Sort(t *testing.T) {
+	tests := []struct {
+		name     string
+		licenses Licenses
+		expected Licenses
+	}{
+		{
+			name:     "empty",
+			licenses: []License{},
+			expected: []License{},
+		},
+		{
+			name: "single",
+			licenses: []License{
+				NewLicenseFromLocation("MIT", source.NewLocation("place!")),
+			},
+			expected: []License{
+				NewLicenseFromLocation("MIT", source.NewLocation("place!")),
+			},
+		},
+		{
+			name: "multiple",
+			licenses: []License{
+				NewLicenseFromLocation("MIT", source.NewLocation("place!")),
+				NewLicenseFromURL("MIT", "https://github.com/anchore/syft/blob/main/LICENSE"),
+				NewLicenseFromLocation("Apache", source.NewLocation("area!")),
+				NewLicenseFromLocation("gpl2+", source.NewLocation("area!")),
+			},
+			expected: Licenses{
+				NewLicenseFromLocation("Apache", source.NewLocation("area!")),
+				NewLicenseFromLocation("MIT", source.NewLocation("place!")),
+				NewLicenseFromURL("MIT", "https://github.com/anchore/syft/blob/main/LICENSE"),
+				NewLicenseFromLocation("gpl2+", source.NewLocation("area!")),
+			},
+		},
+		{
+			name: "multiple with location variants",
+			licenses: []License{
+				NewLicenseFromLocation("MIT", source.NewLocation("place!")),
+				NewLicenseFromLocation("MIT", source.NewLocation("park!")),
+				NewLicense("MIT"),
+				NewLicense("AAL"),
+				NewLicense("Adobe-2006"),
+				NewLicenseFromLocation("Apache", source.NewLocation("area!")),
+			},
+			expected: Licenses{
+				NewLicense("AAL"),
+				NewLicense("Adobe-2006"),
+				NewLicenseFromLocation("Apache", source.NewLocation("area!")),
+				NewLicense("MIT"),
+				NewLicenseFromLocation("MIT", source.NewLocation("park!")),
+				NewLicenseFromLocation("MIT", source.NewLocation("place!")),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			sort.Sort(test.licenses)
+			assert.Equal(t, test.expected, test.licenses)
+		})
+
+	}
 }
