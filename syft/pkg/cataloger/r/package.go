f045bf7317ca2aa2013716aa697297eba3ad5b66
Christopher Phillips <christopher.phillips@anchore.com>
2023-05-11 12:56:25 -0400

    test: add failing tests for r cataloger licenses; update schema
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- syft/pkg/cataloger/r/package.go
+++ syft/pkg/cataloger/r/package.go
@@ -1,6 +1,8 @@
 package r
 
 import (
+	"strings"
+
 	"github.com/anchore/packageurl-go"
 	"github.com/anchore/syft/syft/pkg"
 	"github.com/anchore/syft/syft/source"
@@ -11,11 +13,14 @@ func newPackage(pd parseData, locations ...source.Location) pkg.Package {
 	for _, loc := range locations {
 		locationSet.Add(loc.WithAnnotation(pkg.EvidenceAnnotationKey, pkg.PrimaryEvidenceAnnotation))
 	}
+
+	licenses := parseLicenseData(pd.License)
+
 	result := pkg.Package{
 		Name:         pd.Package,
 		Version:      pd.Version,
 		Locations:    locationSet,
-		Licenses:     []string{pd.License},
+		Licenses:     pkg.NewLicenseSet(licenses...),
 		Language:     pkg.R,
 		Type:         pkg.Rpkg,
 		PURL:         packageURL(pd),
@@ -29,4 +34,63 @@ func newPackage(pd parseData, locations ...source.Location) pkg.Package {
 
 func packageURL(m parseData) string {
 	return packageurl.NewPackageURL("cran", "", m.Package, m.Version, nil, "").ToString()
+}
+
+// https://r-pkgs.org/description.html#the-license-field
+// four forms:
+// 1. "GPL (>= 2)"
+// 2. "GPL-2"
+// 3. "MIT + file LICENSE"
+// 4. "pointer to the full text of the license; file LICENSE"
+// Multiple licences can be specified separated by ‘|’
+// (surrounded by spaces) in which case the user can choose any of the above cases.
+// https://cran.rstudio.com/doc/manuals/r-devel/R-exts.html#Licensing
+func parseLicenseData(license string, locations ...source.Location) []pkg.License {
+	licenses := make([]pkg.License, 0)
+	// check case 1 for surrounding parens
+	if strings.Contains(license, "(") && strings.Contains(license, ")") {
+		licenseVersion := strings.Split(license, " ")
+		if len(licenseVersion) == 2 {
+			license = strings.Join([]string{licenseVersion[0], parseVersion(licenseVersion[1])}, "")
+			licenses = append(licenses, pkg.NewLicenseFromLocations(license, locations...))
+			return licenses
+		}
+	}
+
+	// case 3
+	if strings.Contains(license, "+") && strings.Contains(license, "LICENSE") {
+		splitField := strings.Split(license, " ")
+		if len(splitField) > 0 {
+			licenses = append(licenses, pkg.NewLicenseFromLocations(splitField[0], locations...))
+			return licenses
+		}
+	}
+
+	// TODO: case 4 if we are able to read the location data and find the adjacent file?
+
+	// no specific case found for the above so assume case 2
+	// check if the common name in case 2 is valid SDPX
+	licenses = append(licenses, pkg.NewLicenseFromLocations(license, locations...))
+	return licenses
+}
+
+// attempt to make best guess at SPDX license ID from version operator in case 2
+/*
+‘<’, ‘<=’, ‘>’, ‘>=’, ‘==’, or ‘!=’
+*/
+func parseVersion(version string) string {
+	version = strings.ReplaceAll(version, "(", "")
+	version = strings.ReplaceAll(version, ")", "")
+	operatorVersion := strings.Split(version, " ")
+	if len(operatorVersion) == 2 {
+		version = operatorVersion[1]
+		operator := operatorVersion[0]
+		switch operator {
+		case ">=":
+			return version + "+"
+		case "==":
+			return version
+		}
+	}
+	return version
 }
--- syft/pkg/cataloger/r/package_test.go
+++ syft/pkg/cataloger/r/package_test.go
@@ -1,14 +1,93 @@
 package r
 
-import "testing"
+import (
+	"testing"
 
-func Test_newPackage(t *testing.T) {
+	"github.com/anchore/syft/syft/pkg"
+)
+
+func Test_newPackageLicenses(t *testing.T) {
 	testCases := []struct {
 		name string
-	}{}
+		pd   parseData
+		want []pkg.License
+	}{
+		{
+			"License field with single valid spdx",
+			parseData{
+				Package: "Foo",
+				Version: "1",
+				License: "MIT",
+			},
+			[]pkg.License{
+				pkg.NewLicense("MIT"),
+			},
+		},
+		{
+			"License field with version separator",
+			parseData{
+				Package: "Bar",
+				Version: "2",
+				License: "LGPL (>= 2.0, < 3)",
+			},
+			[]pkg.License{
+				pkg.NewLicense("LGPL-2.0-only"),
+			},
+		},
+		{
+			"License field with file reference",
+			parseData{
+				Package: "Baz",
+				Version: "3",
+				License: "GPL-2 + file LICENSE",
+			},
+			[]pkg.License{
+				pkg.NewLicense("GPL-2.0-only"),
+			},
+		},
+		{
+			"License field which covers no case",
+			parseData{
+				Package: "Baz",
+				Version: "3",
+				License: "Mozilla Public License",
+			},
+			[]pkg.License{
+				pkg.NewLicense("Mozilla Public License"),
+			},
+		},
+		{
+			"License field with multiple cases separated by |",
+			parseData{
+				Package: "Baz",
+				Version: "3",
+				License: "GPL-2 | file LICENCE | LGPL (>= 2.0)",
+			},
+			[]pkg.License{
+				pkg.NewLicense("GPL-2.0-only"),
+				pkg.NewLicense("LGPL-2.0-only"),
+			},
+		},
+	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
+			got := parseLicenseData(tt.pd.License)
+			if len(got) != len(tt.want) {
+				t.Errorf("unexpected number of licenses: got=%d, want=%d", len(got), len(tt.want))
+			}
+
+			for _, wantLicense := range tt.want {
+				found := false
+				for _, gotLicense := range got {
+					if wantLicense.Type == gotLicense.Type && wantLicense.Value == gotLicense.Value {
+						found = true
+					}
+				}
+				if !found {
+					t.Errorf("could not find expected license: %+v", wantLicense)
+				}
+			}
 		})
 	}
 }
