2b9115d1326b2aaadc6c3f086d21f8a0d94035d9
Christopher Phillips <christopher.phillips@anchore.com>
2023-05-12 13:44:28 -0400

    chore: remove stale TODO
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- syft/pkg/license.go
+++ syft/pkg/license.go
@@ -64,8 +64,6 @@ func NewLicense(value string) License {
 		log.Trace("unable to parse license expression: %w", err)
 	}
 
-	// TODO: how do we express other places where a license is declared
-	// EX: we got this from the go module cache at path /x/y/z on disk
 	return License{
 		Value:          value,
 		SPDXExpression: spdxExpression,
