e99d968f5b7434f33ebaee7df7bd6f86d89ef1c1
Alex Goodman <alex.goodman@anchore.com>
2023-05-23 16:54:51 -0400

    update linter script to account for code generation
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- .github/scripts/json-schema-drift-check.sh
+++ .github/scripts/json-schema-drift-check.sh
@@ -1,27 +1,17 @@
 #!/usr/bin/env bash
 set -u
 
-if ! git diff-index --quiet HEAD --; then
-  git diff-index HEAD --
-  git --no-pager diff
-  echo "there are uncommitted changes, please commit them before running this check"
+if [ "$(git status --porcelain | wc -l)" -ne "0" ]; then
+  echo "  🔴 there are uncommitted changes, please commit them before running this check"
   exit 1
 fi
 
-success=true
-
 if ! make generate-json-schema; then
   echo "Generating json schema failed"
-  success=false
+  exit 1
 fi
 
-if ! git diff-index --quiet HEAD --; then
-  git diff-index HEAD --
-  git --no-pager diff
-  echo "JSON schema drift detected!"
-  success=false
-fi
-
-if ! $success; then
+if [ "$(git status --porcelain | wc -l)" -ne "0" ]; then
+  echo "  🔴 there are uncommitted changes, please commit them before running this check"
   exit 1
 fi
