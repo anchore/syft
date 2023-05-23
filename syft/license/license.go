72c87752d40fcc84c6971d2c8f59d7e5cdf3caa2
Christopher Phillips <christopher.phillips@anchore.com>
2023-05-23 11:50:57 -0400

    fix: add panic recovery for license parse
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- syft/license/license.go
+++ syft/license/license.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/github/go-spdx/v2/spdxexp"
 
+	"github.com/anchore/syft/internal/log"
 	"github.com/anchore/syft/internal/spdxlicense"
 )
 
@@ -17,15 +18,25 @@ const (
 )
 
 func ParseExpression(expression string) (string, error) {
+	// https://github.com/anchore/syft/issues/1837
+	// The current spdx library can panic when parsing some expressions
+	// This is a temporary fix to recover and patch until we can investigate and contribute
+	// a fix to the upstream github library
+	defer func() {
+		if r := recover(); r != nil {
+			log.Trace("recovered in parseExpression", r)
+		}
+	}()
+
 	licenseID, exists := spdxlicense.ID(expression)
 	if exists {
 		return licenseID, nil
 	}
-
 	// If it doesn't exist initially in the SPDX list it might be a more complex expression
 	// ignored variable is any invalid expressions
 	// TODO: contribute to spdxexp to expose deprecated license IDs
 	// https://github.com/anchore/syft/issues/1814
+
 	valid, _ := spdxexp.ValidateLicenses([]string{expression})
 	if !valid {
 		return "", fmt.Errorf("failed to validate spdx expression: %s", expression)
