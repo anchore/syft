e9d3fb140a5252c14ecffb83c916640e021be4f4
Christopher Phillips <christopher.phillips@anchore.com>
2023-06-05 12:52:52 -0400

    chore: remove cleanup
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- syft/source/source_test.go
+++ syft/source/source_test.go
@@ -901,14 +901,6 @@ func setupArchiveTest(t testing.TB, sourceDirPath string, layer2 bool) string {
 	archivePrefix, err := os.CreateTemp(t.TempDir(), "syft-archive-TEST-")
 	require.NoError(t, err)
 
-	t.Cleanup(
-		assertNoError(t,
-			func() error {
-				return os.Remove(archivePrefix.Name())
-			},
-		),
-	)
-
 	destinationArchiveFilePath := archivePrefix.Name() + ".tar"
 	t.Logf("archive path: %s", destinationArchiveFilePath)
 	createArchive(t, sourceDirPath, destinationArchiveFilePath, layer2)
