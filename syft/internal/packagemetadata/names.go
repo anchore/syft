863f34c13a7e190d5a6ce9e6bc6020c82b967d5b
Alex Goodman <wagoodman@users.noreply.github.com>
2023-07-26 11:41:53 -0400

    incorporate review feedback for names
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/internal/packagemetadata/names.go
+++ syft/internal/packagemetadata/names.go
@@ -7,37 +7,37 @@ import (
 )
 
 var jsonNameFromType = map[reflect.Type][]string{
-	reflect.TypeOf(pkg.AlpmMetadata{}):               nameList("arch-alpm-catalog-entry", "AlpmMetadata"),
-	reflect.TypeOf(pkg.ApkMetadata{}):                nameList("alpine-apk-catalog-entry", "ApkMetadata"),
+	reflect.TypeOf(pkg.AlpmMetadata{}):               nameList("arch-alpm-db-record", "AlpmMetadata"),
+	reflect.TypeOf(pkg.ApkMetadata{}):                nameList("alpine-apk-db-record", "ApkMetadata"),
 	reflect.TypeOf(pkg.BinaryMetadata{}):             nameList("binary-signature", "BinaryMetadata"),
-	reflect.TypeOf(pkg.CocoapodsMetadata{}):          nameList("cocoapod-lockfile", "CocoapodsMetadataType"),
-	reflect.TypeOf(pkg.ConanLockMetadata{}):          nameList("c-conan-lockfile", "ConanLockMetadataType"),
-	reflect.TypeOf(pkg.ConanMetadata{}):              nameList("c-conan-guidefile", "ConanMetadataType"),
-	reflect.TypeOf(pkg.DartPubMetadata{}):            nameList("dart-pubspec-lockfile", "DartPubMetadata"),
-	reflect.TypeOf(pkg.DotnetDepsMetadata{}):         nameList("dotnet-deps-guidefile", "DotnetDepsMetadata"),
-	reflect.TypeOf(pkg.DpkgMetadata{}):               nameList("debian-dpkg-catalog-entry", "DpkgMetadata"),
-	reflect.TypeOf(pkg.GemMetadata{}):                nameList("ruby-gemspec-guide", "GemMetadata"),                          // TODO: !!!!!!!!!!!!!  note, there is no gemfile metadata!
-	reflect.TypeOf(pkg.GolangBinMetadata{}):          nameList("go-binary-buildinfo", "GolangBinMetadata", "GolangMetadata"), // TODO: !!!!!!!!!!!!!  think on this...
-	reflect.TypeOf(pkg.GolangModMetadata{}):          nameList("go-module-(guidefile?)", "GolangModMetadata"),                // TODO: !!!!!!!!!!!!!  this isn't saying go.mod... as this could include go.sum info too someday??
-	reflect.TypeOf(pkg.HackageMetadata{}):            {"HackageMetadataType"},                                                // TODO: it seems that the stack.yaml/stack.lock/cabal.project.freeze have different purposes and could have different installation intentions    // TODO: !!!!!!!!!!!!!  this should be split based on the cabal and stack files
+	reflect.TypeOf(pkg.CocoapodsMetadata{}):          nameList("cocoa-podfile-lock", "CocoapodsMetadataType"),
+	reflect.TypeOf(pkg.ConanLockMetadata{}):          nameList("c-conan-lock", "ConanLockMetadataType"),
+	reflect.TypeOf(pkg.ConanMetadata{}):              nameList("c-conan", "ConanMetadataType"),
+	reflect.TypeOf(pkg.DartPubMetadata{}):            nameList("dart-pubspec-lock", "DartPubMetadata"),
+	reflect.TypeOf(pkg.DotnetDepsMetadata{}):         nameList("dotnet-deps", "DotnetDepsMetadata"),
+	reflect.TypeOf(pkg.DpkgMetadata{}):               nameList("debian-dpkg-db-record", "DpkgMetadata"),
+	reflect.TypeOf(pkg.GemMetadata{}):                nameList("ruby-gemspec", "GemMetadata"),
+	reflect.TypeOf(pkg.GolangBinMetadata{}):          nameList("go-module-binary-buildinfo", "GolangBinMetadata", "GolangMetadata"),
+	reflect.TypeOf(pkg.GolangModMetadata{}):          nameList("go-module", "GolangModMetadata"),
+	reflect.TypeOf(pkg.HackageMetadata{}):            {"HackageMetadataType"}, // TODO: it seems that the stack.yaml/stack.lock/cabal.project.freeze have different purposes and could have different installation intentions    // TODO: !!!!!!!!!!!!!  this should be split based on the cabal and stack files
 	reflect.TypeOf(pkg.JavaMetadata{}):               nameList("java-archive", "JavaMetadata"),
 	reflect.TypeOf(pkg.KbPackageMetadata{}):          nameList("microsoft-kb-package", "KbPackageMetadata"), // TODO: !!!!!!!!!!!!! not referenced in syft anywhere, but needed for enterprise
 	reflect.TypeOf(pkg.LinuxKernelMetadata{}):        nameList("linux-kernel-archive", "LinuxKernelMetadata"),
 	reflect.TypeOf(pkg.LinuxKernelModuleMetadata{}):  nameList("linux-kernel-module", "LinuxKernelModuleMetadata"),
-	reflect.TypeOf(pkg.MixLockMetadata{}):            nameList("elixir-mix-lockfile", "MixLockMetadataType"),
+	reflect.TypeOf(pkg.MixLockMetadata{}):            nameList("elixir-mix-lock", "MixLockMetadataType"),
 	reflect.TypeOf(pkg.NixStoreMetadata{}):           nameList("nix-store", "NixStoreMetadata"),
-	reflect.TypeOf(pkg.NpmPackageJSONMetadata{}):     nameList("javascript-npm-guidefile", "NpmPackageJsonMetadata"),
-	reflect.TypeOf(pkg.NpmPackageLockJSONMetadata{}): nameList("javascript-npm-lockfile", "NpmPackageLockJsonMetadata"), // TODO: !!!!!!!!!!!!!  should there be versions for these?
-	reflect.TypeOf(pkg.PhpComposerJSONMetadata{}):    nameList("php-composer-lockfile", "PhpComposerJsonMetadata"),      // TODO: note composer json vs lock! // TODO: !!!!!!!!!!!!! also note that I think the installed.json and composer.lock are using the same metadata struct... which is probably wrong
-	reflect.TypeOf(pkg.PortageMetadata{}):            nameList("gentoo-portage-catalog-entry", "PortageMetadata"),
+	reflect.TypeOf(pkg.NpmPackageJSONMetadata{}):     nameList("javascript-npm-package", "NpmPackageJsonMetadata"),
+	reflect.TypeOf(pkg.NpmPackageLockJSONMetadata{}): nameList("javascript-npm-package-lock", "NpmPackageLockJsonMetadata"), // TODO: !!!!!!!!!!!!!  should there be versions for these?
+	reflect.TypeOf(pkg.PhpComposerJSONMetadata{}):    nameList("php-composer-lock", "PhpComposerJsonMetadata"),              // TODO: note composer json vs lock! // TODO: !!!!!!!!!!!!! also note that I think the installed.json and composer.lock are using the same metadata struct... which is probably wrong
+	reflect.TypeOf(pkg.PortageMetadata{}):            nameList("gentoo-portage-db-record", "PortageMetadata"),
 	reflect.TypeOf(pkg.PythonPackageMetadata{}):      nameList("python-egg-or-wheel-...", "PythonPackageMetadata"), // TODO: TODO: !!!!!!!!!!!!! this should be split based on the egg and wheel files (I think)
-	reflect.TypeOf(pkg.PythonPipfileLockMetadata{}):  nameList("python-pipfile-lockfile", "PythonPipfileLockMetadata"),
-	reflect.TypeOf(pkg.PythonRequirementsMetadata{}): nameList("python-requirements-guidefile", "PythonRequirementsMetadata"),
-	reflect.TypeOf(pkg.RebarLockMetadata{}):          nameList("erlang-rebar-lockfile", "RebarLockMetadataType"),
-	reflect.TypeOf(pkg.RDescriptionFileMetadata{}):   nameList("r-description-guidefile", "RDescriptionFileMetadataType"),
-	reflect.TypeOf(pkg.RpmMetadata{}):                nameList("rpm-db-entry", "RpmdbMetadata"), // TODO: !!!!!!!!!!! this should get split into DB and file metadata types
-	//reflect.TypeOf(pkg.RpmMetadata{}):                nameList("rpm-manifest", "RpmMetadata"),   // TODO: !!!!!!!!!!! this should get split into DB and file metadata types
-	reflect.TypeOf(pkg.CargoPackageMetadata{}): nameList("rust-cargo-lockfile", "RustCargoPackageMetadata"),
+	reflect.TypeOf(pkg.PythonPipfileLockMetadata{}):  nameList("python-pipfile-lock", "PythonPipfileLockMetadata"),
+	reflect.TypeOf(pkg.PythonRequirementsMetadata{}): nameList("python-pip-requirements", "PythonRequirementsMetadata"),
+	reflect.TypeOf(pkg.RebarLockMetadata{}):          nameList("erlang-rebar-lock", "RebarLockMetadataType"),
+	reflect.TypeOf(pkg.RDescriptionFileMetadata{}):   nameList("r-description", "RDescriptionFileMetadataType"),
+	reflect.TypeOf(pkg.RpmMetadata{}):                nameList("redhat-rpm-db-record", "RpmdbMetadata"), // TODO: !!!!!!!!!!! this should get split into DB and file metadata types
+	//reflect.TypeOf(pkg.RpmMetadata{}):                nameList("redhat-rpm-archive", "RpmMetadata"),   // TODO: !!!!!!!!!!! this should get split into DB and file metadata types
+	reflect.TypeOf(pkg.CargoPackageMetadata{}): nameList("rust-cargo-lock", "RustCargoPackageMetadata"),
 }
 
 func nameList(id string, others ...string) []string {
