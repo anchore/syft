d76a9d4e2e88c4ed2e188850024135ddc48364f3
Will Murphy <will.murphy@anchore.com>
2023-08-23 17:20:09 -0400

    also trust group ID of POM parent
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

--- syft/pkg/cataloger/java/package_url_test.go
+++ syft/pkg/cataloger/java/package_url_test.go
@@ -94,6 +94,36 @@ func Test_packageURL(t *testing.T) {
 			},
 			expect: "pkg:maven/commons/example-java-app-maven@0.1.0",
 		},
+		{
+			name: "POM project has explicit group ID without . in it",
+			pkg: pkg.Package{
+				Name:         "example-java-app-maven",
+				Version:      "0.1.0",
+				Language:     pkg.Java,
+				Type:         pkg.JavaPkg,
+				MetadataType: pkg.JavaMetadataType,
+				Metadata: pkg.JavaMetadata{
+					VirtualPath: "test-fixtures/java-builds/packages/example-java-app-maven-0.1.0.jar",
+					Manifest: &pkg.JavaManifest{
+						Main: map[string]string{
+							"Manifest-Version": "1.0",
+						},
+					},
+					PomProperties: &pkg.PomProperties{
+						Path:       "META-INF/maven/org.anchore/example-java-app-maven/pom.properties",
+						ArtifactID: "example-java-app-maven",
+						Version:    "0.1.0",
+						Extra:      make(map[string]string),
+					},
+					PomProject: &pkg.PomProject{
+						Parent: &pkg.PomParent{
+							GroupID: "parent",
+						},
+					},
+				},
+			},
+			expect: "pkg:maven/parent/example-java-app-maven@0.1.0",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.expect, func(t *testing.T) {
