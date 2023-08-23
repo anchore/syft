17d4203bbb047d05bbd92856c978e4bfa5fc983e
Alex Goodman <wagoodman@users.noreply.github.com>
2023-08-23 10:06:34 -0400

    Enable reading non-utf-8 encodings for java pom.xml files (#2047)
    * fix reading non utf8 encodings
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * in cases where we cant tell the encoding use the UTF8 replacement char
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * decompose the xml decoding func to get a valid utf8 reader first and test unknown encoding
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/pkg/cataloger/java/parse_pom_xml.go
+++ syft/pkg/cataloger/java/parse_pom_xml.go
@@ -1,6 +1,7 @@
 package java
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"io"
@@ -8,6 +9,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/saintfish/chardet"
 	"github.com/vifraa/gopom"
 	"golang.org/x/net/html/charset"
 
@@ -99,14 +101,47 @@ func newPackageFromPom(pom gopom.Project, dep gopom.Dependency, locations ...fil
 }
 
 func decodePomXML(content io.Reader) (project gopom.Project, err error) {
-	decoder := xml.NewDecoder(content)
-	// prevent against warnings for "xml: encoding "iso-8859-1" declared but Decoder.CharsetReader is nil"
+	inputReader, err := getUtf8Reader(content)
+	if err != nil {
+		return project, fmt.Errorf("unable to read pom.xml: %w", err)
+	}
+
+	decoder := xml.NewDecoder(inputReader)
+	// when an xml file has a character set declaration (e.g. '<?xml version="1.0" encoding="ISO-8859-1"?>') read that and use the correct decoder
 	decoder.CharsetReader = charset.NewReaderLabel
+
 	if err := decoder.Decode(&project); err != nil {
 		return project, fmt.Errorf("unable to unmarshal pom.xml: %w", err)
 	}
 
 	return project, nil
+}
+
+func getUtf8Reader(content io.Reader) (io.Reader, error) {
+	pomContents, err := io.ReadAll(content)
+	if err != nil {
+		return nil, err
+	}
+
+	detector := chardet.NewTextDetector()
+	detection, err := detector.DetectBest(pomContents)
+
+	var inputReader io.Reader
+	if err == nil && detection != nil {
+		if detection.Charset == "UTF-8" {
+			inputReader = bytes.NewReader(pomContents)
+		} else {
+			inputReader, err = charset.NewReaderLabel(detection.Charset, bytes.NewReader(pomContents))
+			if err != nil {
+				return nil, fmt.Errorf("unable to get encoding: %w", err)
+			}
+		}
+	} else {
+		// we could not detect the encoding, but we want a valid file to read. Replace unreadable
+		// characters with the UTF-8 replacement character.
+		inputReader = strings.NewReader(strings.ToValidUTF8(string(pomContents), "�"))
+	}
+	return inputReader, nil
 }
 
 func pomParent(pom gopom.Project, parent *gopom.Parent) (result *pkg.PomParent) {
