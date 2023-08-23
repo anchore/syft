07ac640ac5b53c9f507ccb9189c2364edcb4909e
Stefan Profanter <Pro@users.noreply.github.com>
2023-08-23 19:51:07 +0200

    fix: properly parse conan ref and include user and channel (#2034)
    * fix: properly parse conan ref and include user and channel
    
    Signed-off-by: Stefan Profanter <stefan.profanter@agile-robots.com>
    
    * unexport the conanRef type
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Stefan Profanter <stefan.profanter@agile-robots.com>
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    Co-authored-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/pkg/cataloger/cpp/package.go
+++ syft/pkg/cataloger/cpp/package.go
@@ -8,23 +8,67 @@ import (
 	"github.com/anchore/syft/syft/pkg"
 )
 
-func newConanfilePackage(m pkg.ConanMetadata, locations ...file.Location) *pkg.Package {
-	fields := strings.Split(strings.TrimSpace(m.Ref), "/")
-	if len(fields) < 2 {
-		return nil
+type conanRef struct {
+	Name      string
+	Version   string
+	User      string
+	Channel   string
+	Revision  string
+	Timestamp string
+}
+
+func splitConanRef(ref string) *conanRef {
+	// Conan ref format is:
+	// pkg/0.1@user/channel#rrev%timestamp
+	// This method is based on conan's ref.loads method:
+	// https://github.com/conan-io/conan/blob/release/2.0/conans/model/recipe_ref.py#L93C21-L93C21
+
+	var cref conanRef
+
+	// timestamp
+	tokens := strings.Split(ref, "%")
+	text := tokens[0]
+	if len(tokens) == 2 {
+		cref.Timestamp = tokens[1]
 	}
 
-	pkgName, pkgVersion := fields[0], fields[1]
+	// revision
+	tokens = strings.Split(text, "#")
+	ref = tokens[0]
+	if len(tokens) == 2 {
+		cref.Revision = tokens[1]
+	}
 
-	if pkgName == "" || pkgVersion == "" {
+	// name and version are always given
+	tokens = strings.Split(ref, "@")
+	nameAndVersion := strings.Split(tokens[0], "/")
+	if len(nameAndVersion) < 2 || nameAndVersion[0] == "" || nameAndVersion[1] == "" {
+		return nil
+	}
+	cref.Name = nameAndVersion[0]
+	cref.Version = nameAndVersion[1]
+	// user and channel
+	if len(tokens) == 2 && tokens[1] != "" {
+		tokens = strings.Split(tokens[1], "/")
+		if len(tokens) == 2 {
+			cref.User = tokens[0]
+			cref.Channel = tokens[1]
+		}
+	}
+	return &cref
+}
+
+func newConanfilePackage(m pkg.ConanMetadata, locations ...file.Location) *pkg.Package {
+	ref := splitConanRef(m.Ref)
+	if ref == nil {
 		return nil
 	}
 
 	p := pkg.Package{
-		Name:         pkgName,
-		Version:      pkgVersion,
+		Name:         ref.Name,
+		Version:      ref.Version,
 		Locations:    file.NewLocationSet(locations...),
-		PURL:         packageURL(pkgName, pkgVersion),
+		PURL:         packageURL(ref),
 		Language:     pkg.CPP,
 		Type:         pkg.ConanPkg,
 		MetadataType: pkg.ConanMetadataType,
@@ -37,22 +81,16 @@ func newConanfilePackage(m pkg.ConanMetadata, locations ...file.Location) *pkg.P
 }
 
 func newConanlockPackage(m pkg.ConanLockMetadata, locations ...file.Location) *pkg.Package {
-	fields := strings.Split(strings.Split(m.Ref, "@")[0], "/")
-	if len(fields) < 2 {
-		return nil
-	}
-
-	pkgName, pkgVersion := fields[0], fields[1]
-
-	if pkgName == "" || pkgVersion == "" {
+	ref := splitConanRef(m.Ref)
+	if ref == nil {
 		return nil
 	}
 
 	p := pkg.Package{
-		Name:         pkgName,
-		Version:      pkgVersion,
+		Name:         ref.Name,
+		Version:      ref.Version,
 		Locations:    file.NewLocationSet(locations...),
-		PURL:         packageURL(pkgName, pkgVersion),
+		PURL:         packageURL(ref),
 		Language:     pkg.CPP,
 		Type:         pkg.ConanPkg,
 		MetadataType: pkg.ConanLockMetadataType,
@@ -64,13 +102,20 @@ func newConanlockPackage(m pkg.ConanLockMetadata, locations ...file.Location) *p
 	return &p
 }
 
-func packageURL(name, version string) string {
+func packageURL(ref *conanRef) string {
+	qualifiers := packageurl.Qualifiers{}
+	if ref.Channel != "" {
+		qualifiers = append(qualifiers, packageurl.Qualifier{
+			Key:   "channel",
+			Value: ref.Channel,
+		})
+	}
 	return packageurl.NewPackageURL(
 		packageurl.TypeConan,
-		"",
-		name,
-		version,
-		nil, // TODO: no qualifiers (...yet)
+		ref.User,
+		ref.Name,
+		ref.Version,
+		qualifiers,
 		"",
 	).ToString()
 }
