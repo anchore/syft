f347fb0323a25826a4aace2ce6694fa49d80b125
Will Murphy <will.murphy@anchore.com>
2023-08-30 09:37:55 -0400

    initial failing test
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

---
+++ test/integration/regression_java_virtualpath_test.go
@@ -0,0 +1,34 @@
+package integration
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/anchore/syft/syft/pkg"
+	"github.com/anchore/syft/syft/source"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJavaVirtualPath(t *testing.T) {
+	sbom, _ := catalogFixtureImage(t, "image-java-virtualpath-regression", source.SquashedScope, nil)
+
+	badPURL := "pkg:maven/hudson/hudson@2.2.1"
+	goodPURL := "pkg:maven/org.jvnet.hudson.main/hudson-war@2.2.1"
+	foundCorrectPackage := false
+	badVirtualPath := "/hudson.war:org.jvnet.hudson.main:hudson-war"
+	goodVirtualPath := "/hudson.war"
+	for _, p := range sbom.Artifacts.Packages.Sorted() {
+		if p.Type == pkg.JavaPkg && strings.Contains(p.Name, "hudson") {
+			assert.NotEqual(t, badPURL, p.PURL, "must not find bad purl %q", badPURL)
+			virtPath := ""
+			if meta, ok := p.Metadata.(pkg.JavaMetadata); ok {
+				virtPath = meta.VirtualPath
+				if p.PURL == goodPURL && virtPath == goodVirtualPath {
+					foundCorrectPackage = true
+				}
+			}
+			assert.NotEqual(t, badVirtualPath, virtPath, "must not find bad virtual path %q", badVirtualPath)
+		}
+	}
+	assert.True(t, foundCorrectPackage, "must find correct package, but did not")
+}
