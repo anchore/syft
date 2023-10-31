ac4d180bfb5cc34071aa614a9798b4374d03d7ad
Christopher Phillips <christopher.phillips@anchore.com>
2023-10-31 14:36:29 -0400

    chore: bump number of packages with new dotnet package
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- test/cli/packages_cmd_test.go
+++ test/cli/packages_cmd_test.go
@@ -9,7 +9,7 @@ import (
 const (
 	// this is the number of packages that should be found in the image-pkg-coverage fixture image
 	// when analyzed with the squashed scope.
-	coverageImageSquashedPackageCount = 24
+	coverageImageSquashedPackageCount = 25
 )
 
 func TestPackagesCmdFlags(t *testing.T) {
