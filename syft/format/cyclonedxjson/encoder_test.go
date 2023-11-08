5ae203686b52b8d9db43ee21e13d2a2975dfda28
Alex Goodman <wagoodman@users.noreply.github.com>
2023-11-08 16:55:27 -0600

    remove the snapshot test changes
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/format/cyclonedxjson/encoder_test.go
+++ syft/format/cyclonedxjson/encoder_test.go
@@ -36,7 +36,7 @@ func TestCycloneDxDirectoryEncoder(t *testing.T) {
 }
 
 func TestCycloneDxImageEncoder(t *testing.T) {
-	testImage := "image-simple-with-labels"
+	testImage := "image-simple"
 	testutil.AssertEncoderAgainstGoldenImageSnapshot(t,
 		testutil.ImageSnapshotTestConfig{
 			Image:               testImage,
