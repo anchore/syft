a2a08262c23c9b4bf4a15594410b41e5d2952878
Alex Goodman <wagoodman@users.noreply.github.com>
2024-02-14 10:53:09 -0500

    rename fields to be compliant with json conventions
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/pkg/wordpress.go
+++ syft/pkg/wordpress.go
@@ -2,7 +2,7 @@ package pkg
 
 // WordpressPluginEntry represents all metadata parsed from the wordpress plugin file
 type WordpressPluginEntry struct {
-	PluginInstallDirectory string `mapstructure:"plugin_install_directory" json:"plugin_install_directory"`
+	PluginInstallDirectory string `mapstructure:"pluginInstallDirectory" json:"pluginInstallDirectory"`
 	Author                 string `mapstructure:"author" json:"author,omitempty"`
-	AuthorURI              string `mapstructure:"author_uri" json:"author_uri,omitempty"`
+	AuthorURI              string `mapstructure:"authorUri" json:"authorUri,omitempty"`
 }
