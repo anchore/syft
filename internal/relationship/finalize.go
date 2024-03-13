2e3cdc017ff30764476a00215048b566e266be50
Alex Goodman <wagoodman@users.noreply.github.com>
2024-03-13 18:09:19 -0400

    [wip] prototype binary relationships
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- internal/relationship/finalize.go
+++ internal/relationship/finalize.go
@@ -1,14 +1,19 @@
 package relationship
 
 import (
+	"github.com/anchore/syft/internal/relationship/binary"
 	"github.com/anchore/syft/internal/sbomsync"
 	"github.com/anchore/syft/syft/artifact"
 	"github.com/anchore/syft/syft/cataloging"
+	"github.com/anchore/syft/syft/file"
 	"github.com/anchore/syft/syft/sbom"
 )
 
-func Finalize(builder sbomsync.Builder, cfg cataloging.RelationshipsConfig, src artifact.Identifiable) {
+func Finalize(resolver file.Resolver, builder sbomsync.Builder, cfg cataloging.RelationshipsConfig, src artifact.Identifiable) {
 	accessor := builder.(sbomsync.Accessor)
+
+	// remove ELF packages that are already represented by a non-ELF package
+	// TODO (also, how should we update the TUI to reflect that we removed packages?)
 
 	// add relationships showing packages that are evident by a file which is owned by another package (package-to-package)
 	if cfg.PackageFileOwnershipOverlap {
@@ -20,6 +25,12 @@ func Finalize(builder sbomsync.Builder, cfg cataloging.RelationshipsConfig, src 
 	if cfg.ExcludeBinaryPackagesWithFileOwnershipOverlap {
 		excludeBinariesByFileOwnershipOverlap(accessor)
 	}
+
+	// add the new relationships for executables to the SBOM
+	newBinaryRelationships := binary.NewDependencyRelationships(resolver, accessor)
+	accessor.WriteToSBOM(func(s *sbom.SBOM) {
+		s.Relationships = append(s.Relationships, newBinaryRelationships...)
+	})
 
 	// add source "contains package" relationship (source-to-package)
 	var sourceRelationships []artifact.Relationship
