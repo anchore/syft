e0b1aaf14d1e983b18d89a064d9160f5764a9a68
Brian Ebarb <ebarb.brian@gmail.com>
2024-03-22 08:37:31 -0500

    dependency relationship updates
    Signed-off-by: Brian Ebarb <ebarb.brian@gmail.com>

--- internal/relationship/binary/binary_dependencies.go
+++ internal/relationship/binary/binary_dependencies.go
@@ -13,17 +13,13 @@ import (
 func NewDependencyRelationships(resolver file.Resolver, accessor sbomsync.Accessor) []artifact.Relationship {
 	// TODO: consider library format (e.g. ELF, Mach-O, PE) for the meantime assume all binaries are homogeneous format
 
-	// start with building new file-to-file relationships for executables-to-executables
-	// you need to make certain that they are unique, store in a map[id]map[id]relationship to avoid dupes.
-	// before creating the new file-to-file relationship, check to see if there are packages that represent each
-	// file. If there are, create a package-to-package, file-to-package, or package-to-file relationship as appropriate.
-
+	// start with building new package-to-package relationships for executables-to-executables
+	//each relationship must be unique, store in a map[id]map[id]relationship to avoid duplicates
 	// 1 & 2... build an index of all shared libraries and their owning packages to search against
-	index := newShareLibIndex(resolver, accessor)
+	//index := newShareLibIndex(resolver, accessor)
 
-	// 3. craft package-to-package or package-to-file relationships that represent binary shared library dependencies
-	// note: prefer package-to-package relationships over package-to-file relationships
-
+	// 3. craft package-to-package relationships for each binary that represent shared library dependencies
+	//note: we only care about package-to-package relationships
 	relIndex := newRelationshipIndex()
 	accessor.ReadFromSBOM(func(s *sbom.SBOM) {
 		// read all existing dependencyOf relationships
@@ -34,8 +30,6 @@ func NewDependencyRelationships(resolver file.Resolver, accessor sbomsync.Access
 			relIndex.track(r)
 		}
 	})
-
-	// find all new relationships to add...
 	accessor.ReadFromSBOM(func(s *sbom.SBOM) {
 		for _, parentPkg := range s.Artifacts.Packages.Sorted(pkg.BinaryPkg) {
 			for _, evidentLocation := range parentPkg.Locations.ToSlice() {
@@ -50,48 +44,30 @@ func NewDependencyRelationships(resolver file.Resolver, accessor sbomsync.Access
 				}
 
 				for _, libReference := range exec.ImportedLibraries {
-					// TODO: is this always a basename? technically no, it could be a path...
-					libBasename := path.Base(libReference)
+					//for each library reference, check s.Artifacts.Packages.Sorted(pkg.BinaryPkg) for a bianry package that represents that library
+					//if found, create a relationship between the parent package and the library package
+					// if not found do nothing.
+					//note: we only care about package-to-package relationships
 
-					pkgsThatOwnLib := index.owningLibraryPackage(libBasename)
-					if pkgsThatOwnLib == nil {
-						// create package-to-file relationship...
-						// if there is more than one library for this given library name, then we will include
-						// all of them as dependencies since we don't know the LD_LIBRARY_PATH order
-						// TODO: add configuration for LD_LIBRARY_PATH order?
-						for _, libCoord := range index.owningLibraryLocations(libBasename).ToSlice() {
+					// find the basename of the library
+					libBasename := path.Base(libReference)
+					for _, p := range s.Artifacts.Packages.Sorted(pkg.BinaryPkg) {
+						if p.Name == libBasename {
+							//create a relationship between the parent package and the library package
 							relIndex.add(
 								artifact.Relationship{
-									From: libCoord,
+									From: p,
 									To:   parentPkg,
 									Type: artifact.DependencyOfRelationship,
 								},
 							)
 						}
-
-						// don't create a package-to-package relationship for this library... since we can't
-						continue
 					}
 
-					// create a package-to-package relationship between the binary package and the library package
-					// if there is more than one library for this given library name, then we will include
-					// all of them as dependencies since we don't know the LD_LIBRARY_PATH order
-					for _, pkgThatOwnsLib := range pkgsThatOwnLib.Sorted() {
-						relIndex.add(
-							artifact.Relationship{
-								From: pkgThatOwnsLib,
-								To:   parentPkg,
-								Type: artifact.DependencyOfRelationship,
-							},
-						)
-					}
 				}
 			}
+
 		}
 	})
-
-	// so far this handles the first order dependencies from the binary package. Odds are that the OS package manager
-	// will have already created a package-to-package relationship for the lib packages to other lib packages.
-
 	return relIndex.newRelationships()
 }
