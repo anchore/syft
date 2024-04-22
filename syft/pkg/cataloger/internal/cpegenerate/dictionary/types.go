12ea9912b4403d709aa27c07d8dd25d07f24e06a
Weston Steimel <commits@weston.slmail.me>
2024-04-22 18:43:19 +0100

    feat: index known cpes for PHP extensions (#2777)
    Indexes known CPEs from `pecl.php.net` and `pear.php.net`
    
    Signed-off-by: Weston Steimel <commits@weston.slmail.me>

--- syft/pkg/cataloger/internal/cpegenerate/dictionary/types.go
+++ syft/pkg/cataloger/internal/cpegenerate/dictionary/types.go
@@ -4,6 +4,8 @@ const (
 	EcosystemNPM            = "npm"
 	EcosystemRubyGems       = "rubygems"
 	EcosystemPyPI           = "pypi"
+	EcosystemPHPPear        = "php_pear"
+	EcosystemPHPPecl        = "php_pecl"
 	EcosystemJenkinsPlugins = "jenkins_plugins"
 	EcosystemRustCrates     = "rust_crates"
 )
