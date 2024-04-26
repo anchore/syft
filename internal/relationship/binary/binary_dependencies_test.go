14551fd881c45b1bdfdafc956262987a4e774b89
Christopher Phillips <christopher.phillips@anchore.com>
2024-04-26 10:39:38 -0400

    test: refactor test for more cases
    Signed-off-by: Christopher Phillips <christopher.phillips@anchore.com>

--- internal/relationship/binary/binary_dependencies_test.go
+++ internal/relationship/binary/binary_dependencies_test.go
@@ -13,7 +13,21 @@ import (
 	"github.com/anchore/syft/syft/sbom"
 )
 
+// GLIBC [RPM]
+// GLIBC [lib executable]
+// sytftestfixture [ELF P]
+// sytftestfixture (lib executable)... that imports GLIBC
+
+// 1. ELF P (a) --> exec (a) ---> imports of (a) --> (b) executable (imported executable) (ELF P --> file)
+// 1... but with symlinks!
+// 1. ... but there is no primary evidence
+// 1. ... but there is no executable for the ELF package
+
+// 2. ELF P (a) --> exec (a) ---> imports of (a) --> (b) executable (imported executable) --> resolves to RPM package ( ELF P --> RPM)
+// 3. ELF P (a) is a part of RPM P (b), thus ELF P (a) is deleted from the SBOM... this means that (b) gets all relationships of (a)
+
 func TestNewDependencyRelationships(t *testing.T) {
+	// package discovered in the executable
 	glibCPackage := pkg.Package{
 		Name:    "glibc",
 		Version: "2.28-236.el8_9.12",
@@ -27,6 +41,7 @@ func TestNewDependencyRelationships(t *testing.T) {
 		},
 	}
 
+	// package that is an executable
 	syftTestFixturePackage := pkg.Package{
 		Name:    "syfttestfixture",
 		Version: "0.01",
@@ -47,75 +62,55 @@ func TestNewDependencyRelationships(t *testing.T) {
 		},
 	}
 
+	// executable representation of the above binary pkg
+	syftTestFixtureExecutable := file.Executable{
+		Format:        "elf",
+		HasExports:    true,
+		HasEntrypoint: true,
+		ImportedLibraries: []string{
+			"libc.so.6",
+		},
+	}
+
 	tests := []struct {
-		name     string
-		resolver file.Resolver
-		accessor sbomsync.Accessor
-		want     []artifact.Relationship
+		name            string
+		resolver        file.Resolver
+		coordinateIndex map[file.Coordinates]file.Executable
+		packages        []pkg.Package
+		want            []artifact.Relationship
 	}{
 		{
-			name:     "blank sbom and accessor returns empty relationships",
-			resolver: nil,
-			accessor: func() sbomsync.Accessor {
-				return sbomsync.NewBuilder(&sbom.SBOM{}).(sbomsync.Accessor)
-			}(),
-			want: make([]artifact.Relationship, 0),
+			name:            "blank sbom and accessor returns empty relationships",
+			resolver:        nil,
+			coordinateIndex: map[file.Coordinates]file.Executable{},
+			packages:        []pkg.Package{},
+			want:            make([]artifact.Relationship, 0),
 		},
 		{
-			name: "binary elf cataloger test fixture",
+			name: "given a package that imports glibC, expect a relationship between the two packages when the package is an executable",
 			resolver: file.NewMockResolverForPaths(
 				"/usr/lib64/libc.so.6",
 				"/usr/local/bin/elftests/elfbinwithnestedlib/bin/elfbinwithnestedlib",
 				"/usr/local/bin/elftests/elfbinwithsisterlib/bin/elfwithparallellibbin1",
 			),
-			accessor: func() sbomsync.Accessor {
-				s := sbom.SBOM{
-					Artifacts: sbom.Artifacts{
-						Packages: pkg.NewCollection(),
-					},
-				}
-
-				builder := sbomsync.NewBuilder(&s)
-				builder.AddPackages(
-					glibCPackage,
-					syftTestFixturePackage,
-				)
-
-				syftTestFixtureLib := file.Coordinates{
-					RealPath: "/usr/local/bin/elftests/elfbinwithnestedlib/bin/elfbinwithnestedlib",
-				}
-				syftTestFixtureLibBin1 := file.Coordinates{
-					RealPath: "/usr/local/bin/elftests/elfbinwithsisterlib/bin/elfwithparallellibbin1",
-				}
-				glibcCoord := file.Coordinates{
+			// path -> executable (above mock resolver needs to be able to resolve to paths in this map)
+			coordinateIndex: map[file.Coordinates]file.Executable{
+				{
 					RealPath: "/usr/lib64/libc.so.6",
-				}
-				syftTestFixtureExecutable := file.Executable{
-					Format:        "elf",
-					HasExports:    true,
-					HasEntrypoint: true,
-					ImportedLibraries: []string{
-						"libc.so.6",
-					},
-				}
-
-				accessor := builder.(sbomsync.Accessor)
-				accessor.WriteToSBOM(func(s *sbom.SBOM) {
-					s.Artifacts.Executables = make(map[file.Coordinates]file.Executable)
-
-					// add the libstdc++ executable
-					s.Artifacts.Executables[glibcCoord] = file.Executable{
-						Format:            "elf",
-						HasExports:        true,
-						HasEntrypoint:     true,
-						ImportedLibraries: []string{},
-					}
-					s.Artifacts.Executables[syftTestFixtureLib] = syftTestFixtureExecutable
-					s.Artifacts.Executables[syftTestFixtureLibBin1] = syftTestFixtureExecutable
-				})
-
-				return accessor
-			}(),
+				}: {
+					Format:            "elf",
+					HasExports:        true,
+					HasEntrypoint:     true,
+					ImportedLibraries: []string{},
+				},
+				{
+					RealPath: "/usr/local/bin/elftests/elfbinwithnestedlib/bin/elfbinwithnestedlib",
+				}: syftTestFixtureExecutable,
+				{
+					RealPath: "/usr/local/bin/elftests/elfbinwithsisterlib/bin/elfwithparallellibbin1",
+				}: syftTestFixtureExecutable,
+			},
+			packages: []pkg.Package{glibCPackage, syftTestFixturePackage},
 			want: []artifact.Relationship{
 				{
 					From: glibCPackage,
@@ -127,7 +122,11 @@ func TestNewDependencyRelationships(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			relationships := NewDependencyRelationships(tt.resolver, tt.accessor)
+			accessor := newAccesorClosure(tt.packages, tt.coordinateIndex)
+			// given a resolver that knows about the paths of the packages and executables,
+			// and given an SBOM accessor that knows about the packages and executables,
+			// we should be able to create a set of relationships between the packages and executables
+			relationships := NewDependencyRelationships(tt.resolver, accessor)
 			if d := cmp.Diff(tt.want, relationships, cmpopts.IgnoreUnexported(
 				pkg.Package{},
 				artifact.Relationship{},
@@ -138,4 +137,21 @@ func TestNewDependencyRelationships(t *testing.T) {
 			}
 		})
 	}
+}
+
+func newAccesorClosure(pkgs []pkg.Package, coordinateIndex map[file.Coordinates]file.Executable) sbomsync.Accessor {
+	sb := sbom.SBOM{
+		Artifacts: sbom.Artifacts{
+			Packages: pkg.NewCollection(),
+		},
+	}
+
+	builder := sbomsync.NewBuilder(&sb)
+	builder.AddPackages(pkgs...)
+
+	accessor := builder.(sbomsync.Accessor)
+	accessor.WriteToSBOM(func(s *sbom.SBOM) {
+		s.Artifacts.Executables = coordinateIndex
+	})
+	return accessor
 }
