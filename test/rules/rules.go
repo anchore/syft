ada8f009d25b1e542a757b97e6f8e8604cc82174
Alex Goodman <wagoodman@users.noreply.github.com>
2024-05-07 13:29:46 -0400

    Add relationships for ALPM packages (arch linux) (#2851)
    * add alpm relationships
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * tweak reader linter rule to check for reader impl
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * update JSON schema with alpm dependency information
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- test/rules/rules.go
+++ test/rules/rules.go
@@ -8,8 +8,8 @@ import "github.com/quasilyte/go-ruleguard/dsl"
 func resourceCleanup(m dsl.Matcher) {
 	m.Match(`$res, $err := $resolver.FileContentsByLocation($loc); if $*_ { $*_ }; $next`).
 		Where(m["res"].Type.Implements(`io.Closer`) &&
+			m["res"].Type.Implements(`io.Reader`) &&
 			m["err"].Type.Implements(`error`) &&
-			m["res"].Type.Implements(`io.Closer`) &&
 			!m["next"].Text.Matches(`defer internal.CloseAndLogError`)).
 		Report(`please call "defer internal.CloseAndLogError($res, $loc.RealPath)" right after checking the error returned from $resolver.FileContentsByLocation.`)
 }
