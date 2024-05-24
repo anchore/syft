bf2ab8ef736dcd7fe6096100a47de88a9a88c62a
Alex Goodman <wagoodman@users.noreply.github.com>
2024-05-24 17:59:22 -0400

    provide generic set for basic types
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- internal/set.go
+++ internal/set.go
@@ -1,50 +1,56 @@
 package internal
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
-// StringSet represents a set of string types.
-type StringSet map[string]struct{}
+// Set represents a generic set type.
+type Set[T comparable] map[T]struct{}
 
-// NewStringSet creates a new empty StringSet.
-func NewStringSet(start ...string) StringSet {
-	ret := make(StringSet)
-	for _, s := range start {
-		ret.Add(s)
+// NewSet creates a new empty Set.
+func NewSet[T comparable](start ...T) Set[T] {
+	ret := make(Set[T])
+	for _, v := range start {
+		ret.Add(v)
 	}
 	return ret
 }
 
-// Add a string to the set.
-func (s StringSet) Add(i ...string) {
-	for _, str := range i {
-		s[str] = struct{}{}
+// Add adds elements to the set.
+func (s Set[T]) Add(elements ...T) {
+	for _, e := range elements {
+		s[e] = struct{}{}
 	}
 }
 
-// Remove a string from the set.
-func (s StringSet) Remove(i string) {
-	delete(s, i)
+// Remove removes an element from the set.
+func (s Set[T]) Remove(element T) {
+	delete(s, element)
 }
 
-// Contains indicates if the given string is contained within the set.
-func (s StringSet) Contains(i string) bool {
-	_, ok := s[i]
+// Contains checks if an element is in the set.
+func (s Set[T]) Contains(element T) bool {
+	_, ok := s[element]
 	return ok
 }
 
-// ToSlice returns a sorted slice of strings that are contained within the set.
-func (s StringSet) ToSlice() []string {
-	ret := make([]string, len(s))
+// ToSlice returns a sorted slice of elements in the set.
+func (s Set[T]) ToSlice() []T {
+	ret := make([]T, len(s))
 	idx := 0
 	for v := range s {
 		ret[idx] = v
 		idx++
 	}
-	sort.Strings(ret)
+	sort.Slice(ret, func(i, j int) bool {
+		return fmt.Sprintf("%v", ret[i]) < fmt.Sprintf("%v", ret[j])
+	})
 	return ret
 }
 
-func (s StringSet) Equals(o StringSet) bool {
+// Equals checks if two sets are equal.
+func (s Set[T]) Equals(o Set[T]) bool {
 	if len(s) != len(o) {
 		return false
 	}
@@ -56,6 +62,7 @@ func (s StringSet) Equals(o StringSet) bool {
 	return true
 }
 
-func (s StringSet) Empty() bool {
-	return len(s) < 1
+// Empty checks if the set is empty.
+func (s Set[T]) Empty() bool {
+	return len(s) == 0
 }
