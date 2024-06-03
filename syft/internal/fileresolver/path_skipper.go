1ff96b512792e90396e5d43de472bfefb05c8a82
Alex Goodman <wagoodman@users.noreply.github.com>
2024-06-03 15:46:25 -0400

    refactor and add tests
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/internal/fileresolver/path_skipper.go
+++ syft/internal/fileresolver/path_skipper.go
@@ -1,82 +1,167 @@
 package fileresolver
 
 import (
-	"github.com/anchore/syft/internal/log"
-	"github.com/moby/sys/mountinfo"
 	"io/fs"
 	"os"
 	"sort"
 	"strings"
+
+	"github.com/moby/sys/mountinfo"
+
+	"github.com/anchore/syft/internal/log"
 )
 
-// ignorableMountTypes is a set of mount types that should be ignored.
-var ignorableMountTypes = map[string]struct{}{
-	"proc":     {},
-	"sysfs":    {},
-	"devfs":    {},
-	"devtmpfs": {},
+type pathSkipper struct {
+	// scanTarget is the root path that is being scanned (without any base-path logic applied).
+	scanTarget string
+
+	// ignorableMountTypes is a set of mount types that should be ignored. Optionally a list of paths (the map values)
+	// can be provided that this mount type should be ignored at.  For example in some containers /dev is mounted
+	// as a tmpfs and should be ignored, but /tmp should not be ignored. An empty list of paths means that paths
+	// within the mount type should always be ignored.
+	ignorableMountTypes map[string][]string
+
+	// current mount paths for the current system
+	mounts       []*mountinfo.Info
+	mountsByType map[string][]*mountinfo.Info
 }
 
-// tmpfsSuffixes is a list of paths that we ignore a tmpfs at. For example
-// in some containers /dev is mounted as a tmpfs, but should be ignored
-var tmpfsSuffixes = []string{"/run", "/dev", "/var/run", "/var/lock", "/sys"}
-
-// NewPathSkipper accepts the root and base paths, just as a new directory scanner does.
-// It returns a PathIndexVisitor that is meant to skip directories with files that
-// block forever when read from, never terminate, etc, such as /dev/random, while
-// including as far as possible everything else.
-// It is meant to allow scanning / on a Linux host to succeed, while also not
-// causing any files in a narrow directory scan to be skipped unnecessarily.
-func NewPathSkipper(root string, base string) PathIndexVisitor {
+// skipPathsByMountTypeAndName accepts the root path and returns a PathIndexVisitor that will skip paths based
+// the filesystem type, the mountpoint, and configured blocklist paths for each filesystem type.
+// This will help syft dodge filesystem topologies that have the potential to make the search space much bigger in
+// areas known to not traditionally contain files of interest (installed software).  It is meant to allow scanning
+// "/" on a unix host to succeed, while also not causing any files in a narrow directory scan to be skipped unnecessarily.
+func skipPathsByMountTypeAndName(root string) PathIndexVisitor {
 	infos, err := mountinfo.GetMounts(nil)
 	if err != nil {
 		log.WithFields("error", err).Warnf("unable to get system mounts")
-		// TODO: better / safe to just return nil?
 		return func(_ string, _ string, _ os.FileInfo, _ error) error {
 			return nil
 		}
 	}
-	return newPathSkipper(root, base, infos)
+
+	return newPathSkipperFromMounts(root, infos).pathIndexVisitor
 }
 
-func newPathSkipper(_ string, _ string, mountInfos []*mountinfo.Info) PathIndexVisitor {
+func newPathSkipperFromMounts(root string, infos []*mountinfo.Info) pathSkipper {
+	// we're only interested in ignoring the logical filesystems typically found at these mount points:
+	// - /proc
+	//     - procfs
+	//     - proc
+	// - /sys
+	//     - sysfs
+	// - /dev
+	//     - devfs - BSD/darwin flavored systems and old linux systems
+	//     - devtmpfs - driver core maintained /dev tmpfs
+	//     - udev - userspace implementation that replaced devfs
+	//     - tmpfs - used for /dev in special instances (within a container)
+	ignorableMountTypes := map[string][]string{
+		"proc":     nil,
+		"procfs":   nil,
+		"sysfs":    nil,
+		"devfs":    nil,
+		"devtmpfs": nil,
+		"udev":     nil,
+		// note: order matters here. The most specific path should be listed first (e.g. /sys/something should precede /sys)
+		"tmpfs": {"/run", "/dev", "/var/run", "/var/lock", "/sys"},
+	}
+
 	// The longest path is the most specific path, e.g.
 	// if / is mounted as tmpfs, but /home/syft/permanent is mounted as ext4,
 	// then the mount type for /home/syft/permanent/foo is ext4, and the mount info
 	// stating that /home/syft/permanent is ext4 has the longer mount point.
-	sort.Slice(mountInfos, func(i, j int) bool {
-		return len(mountInfos[i].Mountpoint) > len(mountInfos[j].Mountpoint)
+	sort.Slice(infos, func(i, j int) bool {
+		return len(infos[i].Mountpoint) > len(infos[j].Mountpoint)
 	})
-	// TODO: what's the differencces between base and scan target?
-	return func(scanTarget string, path string, info os.FileInfo, err error) error {
-		// Rule 1: don't entirely ignore the dir we're supposed to be scanning
-		if path == scanTarget {
-			return nil
-		}
 
-		// Rule 2: ignore mount types that typically contain never ending files
-		for _, mi := range mountInfos {
-			if strings.HasPrefix(path, mi.Mountpoint) {
-				if _, ignorable := ignorableMountTypes[mi.FSType]; ignorable {
-					return fs.SkipDir
-				}
-				break
-			}
-		}
+	mountsByType := make(map[string][]*mountinfo.Info)
 
-		// Rule 3: if there's a suspicious looking path, like /dev, mounted as a tmpfs
-		// then skip that. But don't skip tmpfs by default, because untaring an artifact
-		// to /tmp and then scanning it is a common use case.
-		for _, suffix := range tmpfsSuffixes {
-			if strings.HasSuffix(path, suffix) {
-				for _, mi := range mountInfos {
-					if strings.HasPrefix(path, mi.Mountpoint) && mi.FSType == "tmpfs" {
-						return fs.SkipDir
-					}
-				}
-			}
-		}
+	for _, mi := range infos {
+		mountsByType[mi.FSType] = append(mountsByType[mi.FSType], mi)
+	}
 
+	return pathSkipper{
+		scanTarget:          root,
+		ignorableMountTypes: ignorableMountTypes,
+		mounts:              infos,
+		mountsByType:        mountsByType,
+	}
+}
+
+func (ps pathSkipper) pathIndexVisitor(_ string, givenPath string, _ os.FileInfo, _ error) error {
+	// Rule 1: don't entirely ignore the dir we're supposed to be scanning
+	if givenPath == ps.scanTarget {
 		return nil
 	}
+
+	for _, mi := range ps.mounts {
+		conditionalPaths, ignorable := ps.ignorableMountTypes[mi.FSType]
+
+		if len(conditionalPaths) == 0 {
+			// Rule 2: ignore any path within a mount point that is of the given filesystem type unconditionally
+			if !containsPath(givenPath, mi.Mountpoint) {
+				continue
+			}
+
+			if !ignorable {
+				// we've matched on the most specific path at this point, which means we should stop searching
+				// mount points for this path
+				break
+			}
+
+			log.WithFields(
+				"path", givenPath,
+				"mountpoint", mi.Mountpoint,
+				"fs", mi.FSType,
+			).Debug("ignoring path based on mountpoint filesystem type")
+
+			return fs.SkipDir
+		}
+
+		// Rule 3: ignore any path within a mount point that is of the given filesystem type, only if
+		// the path is on a known blocklist of paths for that filesystem type.
+		// For example: /dev can be mounted as a tmpfs, which should always be skipped.
+		for _, conditionalPath := range conditionalPaths {
+			if !containsPath(givenPath, conditionalPath) {
+				continue
+			}
+
+			if !ignorable {
+				// we've matched on the most specific path at this point, which means we should stop searching
+				// mount points for this path
+				break
+			}
+
+			log.WithFields(
+				"path", givenPath,
+				"mountpoint", mi.Mountpoint,
+				"fs", mi.FSType,
+				"condition", conditionalPath,
+			).Debug("ignoring path based on mountpoint filesystem type")
+
+			return fs.SkipDir
+		}
+	}
+
+	return nil
+}
+
+func containsPath(p1, p2 string) bool {
+	p1Clean := simpleClean(p1)
+	p2Clean := simpleClean(p2)
+	if p1Clean == p2Clean {
+		return true
+	}
+	return strings.HasPrefix(p1Clean, p2Clean+"/")
+}
+
+func simpleClean(p string) string {
+	p = strings.TrimSpace(p)
+	if p == "" {
+		return "."
+	}
+	if p == "/" {
+		return "/"
+	}
+	return strings.TrimSuffix(p, "/")
 }
--- syft/internal/fileresolver/path_skipper_test.go
+++ syft/internal/fileresolver/path_skipper_test.go
@@ -1,39 +1,309 @@
 package fileresolver
 
 import (
+	"io/fs"
+	"testing"
+
 	"github.com/moby/sys/mountinfo"
 	"github.com/stretchr/testify/assert"
-	"testing"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_newPathSkipper(t *testing.T) {
-	type setup struct {
-		in0        string
-		in1        string
-		mountInfos []*mountinfo.Info
-	}
-
 	type expect struct {
 		path    string
-		wantErr error
+		wantErr assert.ErrorAssertionFunc
 	}
+	unixSubject := []*mountinfo.Info{
+		{
+			Mountpoint: "/proc",
+			FSType:     "procfs",
+		},
+		{
+			Mountpoint: "/sys",
+			FSType:     "sysfs",
+		},
+		{
+			Mountpoint: "/dev",
+			FSType:     "devfs",
+		},
+		{
+			Mountpoint: "/",
+			FSType:     "/dev/disk3s1s1",
+		},
+		{
+			Mountpoint: "/dev/shm",
+			FSType:     "shm",
+		},
+		{
+			Mountpoint: "/tmp",
+			FSType:     "tmpfs",
+		},
+	}
+
 	tests := []struct {
-		name  string
-		setup setup
-		want  []expect
+		name   string
+		root   string
+		base   string
+		mounts []*mountinfo.Info
+		want   []expect
 	}{
-		// TODO: Add test cases.
+		{
+			name: "happy path",
+			root: "/somewhere",
+			mounts: []*mountinfo.Info{
+				{
+					Mountpoint: "/home/somewhere/else",
+					FSType:     "/dev/disk3s6",
+				},
+				{
+					Mountpoint: "/somewhere",
+					FSType:     "/dev/disk3s7",
+				},
+			},
+			want: []expect{
+				{
+					// within a known mountpoint with valid type (1)
+					path: "/somewhere/dev",
+				},
+				{
+					// is a known mountpoint with valid type
+					path: "/somewhere",
+				},
+				{
+					// within a known mountpoint with valid type (2)
+					path: "/home/somewhere/else/too",
+				},
+				{
+					// outside of any known mountpoint should not be an error
+					path: "/bogus",
+				},
+			},
+		},
+		{
+			name: "ignore paths within a scan target",
+			root: "/somewhere",
+			mounts: []*mountinfo.Info{
+				{
+					Mountpoint: "/somewhere/doesnt/matter/proc",
+					FSType:     "procfs",
+				},
+				{
+					Mountpoint: "/somewhere",
+					FSType:     "/dev/disk3s7",
+				},
+			},
+			want: []expect{
+				{
+					// within a known mountpoint with valid type (1)
+					path: "/somewhere/dev",
+				},
+				{
+					// is a known mountpoint with valid type
+					path: "/somewhere",
+				},
+				{
+					// mountpoint that should be ignored
+					path:    "/somewhere/doesnt/matter/proc",
+					wantErr: assertSkipErr(),
+				},
+				{
+					// within a mountpoint that should be ignored
+					path:    "/somewhere/doesnt/matter/proc",
+					wantErr: assertSkipErr(),
+				},
+			},
+		},
+		{
+			name: "nested mountpoints behave correctly",
+			root: "/somewhere",
+			mounts: []*mountinfo.Info{
+				{
+					Mountpoint: "/somewhere/dev",
+					FSType:     "devfs",
+				},
+				{
+					Mountpoint: "/somewhere/dev/includeme",
+					FSType:     "/dev/disk3s7",
+				},
+			},
+			want: []expect{
+				{
+					// is a known mountpoint with valid type
+					path:    "/somewhere/dev",
+					wantErr: assertSkipErr(),
+				},
+				{
+					// is a known mountpoint with valid type
+					path: "/somewhere/dev/includeme",
+				},
+				{
+					// within a known mountpoint with valid type
+					path: "/somewhere/dev/includeme/too!",
+				},
+			},
+		},
+		{
+			name: "ignore known trixy tmpfs paths",
+			root: "/",
+			mounts: []*mountinfo.Info{
+				{
+					Mountpoint: "/",
+					FSType:     "/dev/disk3s7",
+				},
+				{
+					Mountpoint: "/dev",
+					FSType:     "tmpfs",
+				},
+				{
+					Mountpoint: "/run",
+					FSType:     "tmpfs",
+				},
+				{
+					Mountpoint: "/var/run",
+					FSType:     "tmpfs",
+				},
+				{
+					Mountpoint: "/var/lock",
+					FSType:     "tmpfs",
+				},
+				{
+					Mountpoint: "/sys",
+					FSType:     "tmpfs",
+				},
+				{
+					Mountpoint: "/tmp",
+					FSType:     "tmpfs",
+				},
+			},
+			want: []expect{
+				{
+					path:    "/dev",
+					wantErr: assertSkipErr(),
+				},
+				{
+					path:    "/run",
+					wantErr: assertSkipErr(),
+				},
+				{
+					path:    "/var/run",
+					wantErr: assertSkipErr(),
+				},
+				{
+					path:    "/var/lock",
+					wantErr: assertSkipErr(),
+				},
+				{
+					path:    "/sys",
+					wantErr: assertSkipErr(),
+				},
+				// show that we honor ignoring nested paths
+				{
+					path:    "/sys/nested",
+					wantErr: assertSkipErr(),
+				},
+				// show that paths outside of the known mountpoints are not skipped
+				{
+					path: "/stuff",
+				},
+				// show that we allow other tmpfs paths that are not on the blocklist
+				{
+					path: "/tmp/allowed",
+				},
+				// show sibling paths with same prefix (e.g. /sys vs /system) to that of not allowed paths are not skipped
+				{
+					path: "/system",
+				},
+			},
+		},
+		{
+			name:   "test unix paths",
+			mounts: unixSubject,
+			root:   "/",
+			want: []expect{
+				{
+					// relative path to proc is allowed
+					path: "proc/place",
+				},
+				{
+					// relative path within proc is not allowed
+					path:    "/proc/place",
+					wantErr: assertSkipErr(),
+				},
+				{
+					// path exactly to proc is not allowed
+					path:    "/proc",
+					wantErr: assertSkipErr(),
+				},
+				{
+					// similar to proc
+					path: "/pro/c",
+				},
+				{
+					// similar to proc
+					path: "/pro",
+				},
+				{
+					// dev is not allowed
+					path:    "/dev",
+					wantErr: assertSkipErr(),
+				},
+				{
+					// sys is not allowed
+					path:    "/sys",
+					wantErr: assertSkipErr(),
+				},
+			},
+		},
+		{
+			name:   "test unix paths with base",
+			mounts: unixSubject,
+			root:   "/",
+			base:   "/a/b/c",
+			want: []expect{
+				{
+					// do not consider base when matching paths (non-matching)
+					path: "/a/b/c/dev",
+				},
+				{
+					// do not consider base when matching paths (matching)
+					path:    "/dev",
+					wantErr: assertSkipErr(),
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			visitor := newPathSkipper(tt.setup.in0, tt.setup.in1, tt.setup.mountInfos)
+			if tt.base == "" {
+				tt.base = tt.root
+			}
+
+			require.NotEmpty(t, tt.want)
+			ps := newPathSkipperFromMounts(tt.root, tt.mounts)
+
 			for _, exp := range tt.want {
-				if exp.wantErr == nil {
-					assert.NoError(t, visitor("", exp.path, nil, nil))
-					continue
-				}
-				assert.ErrorIs(t, exp.wantErr, visitor("", exp.path, nil, nil))
+				t.Run(exp.path, func(t *testing.T) {
+
+					got := ps.pathIndexVisitor(tt.base, exp.path, nil, nil)
+					if exp.wantErr == nil {
+						assert.NoError(t, got)
+						return
+					}
+					exp.wantErr(t, got)
+
+				})
 			}
 		})
+	}
+}
+
+func assertSkipErr() assert.ErrorAssertionFunc {
+	return assertErrorIs(fs.SkipDir)
+}
+
+func assertErrorIs(want error) assert.ErrorAssertionFunc {
+	return func(t assert.TestingT, got error, msgAndArgs ...interface{}) bool {
+		return assert.ErrorIs(t, got, want, msgAndArgs...)
 	}
 }
