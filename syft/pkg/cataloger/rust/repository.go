0f8f7247fbb8212f8f01e62aba082717cd91810a
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-06-11 12:59:10 +0200

    Move some of the implementation into seperate files
    Signed-off-by: C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>

---
+++ syft/pkg/cataloger/rust/repository.go
@@ -0,0 +1,194 @@
+package rust
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/anchore/syft/syft/pkg"
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	"github.com/go-git/go-git/v5/storage/memory"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type RepositoryConfig struct {
+	Download     string `json:"dl"`
+	API          string `json:"api"`
+	AuthRequired bool   `json:"auth-required"`
+}
+
+type SourceId struct {
+	kind string
+	url  string
+}
+
+func GetSourceId(r *pkg.RustCargoLockEntry) (*SourceId, error) {
+	var before, after, found = strings.Cut(r.Source, "+")
+	if !found {
+		return nil, fmt.Errorf("did not find \"+\" in source field of dependency: Name: %s, Version: %s, Source: %s", r.Name, r.Version, r.Source)
+	}
+
+	return &SourceId{
+		kind: before,
+		url:  after,
+	}, nil
+}
+
+// see https://github.com/rust-lang/cargo/blob/master/crates/cargo-util-schemas/src/core/source_kind.rs
+const (
+	SourceKindPath           = "path"
+	SourceKindGit            = "git"
+	SourceKindRegistry       = "registry"
+	SourceKindLocalRegistry  = "local-registry"
+	SourceKindSparse         = "sparse"
+	SourceKindLocalDirectory = "directory"
+)
+
+var RegistryRepos = make(map[string]*memory.Storage)
+var RegistryConfig = make(map[string]RepositoryConfig)
+
+// RepositoryConfigName see https://github.com/rust-lang/cargo/blob/b134eff5cedcaa4879f60035d62630400e7fd543/src/cargo/sources/registry/mod.rs#L962
+const RepositoryConfigName = "config.json"
+
+func (i *SourceId) GetConfig() (*RepositoryConfig, error) {
+	if repoConfig, ok := RegistryConfig[i.url]; ok {
+		return &repoConfig, nil
+	}
+	content, err := i.GetPath(RepositoryConfigName)
+	if err != nil {
+		return nil, err
+	}
+	var repoConfig = RepositoryConfig{}
+	err = json.Unmarshal(content, &repoConfig)
+	if err != nil {
+		err = fmt.Errorf("failed to deserialize rust repository configuration: %s", err)
+	}
+	RegistryConfig[i.url] = repoConfig
+	return &repoConfig, err
+}
+
+func (i *SourceId) GetPath(path string) ([]byte, error) {
+	var content []byte
+	switch i.kind {
+	case SourceKindSparse:
+		var resp, err = http.Get(fmt.Sprintf("%s/%s", i.url, path))
+		if err != nil {
+			return content, fmt.Errorf("could not get the path %s/%s from sparse registry: %s", i.url, path, err)
+		}
+		content, err = io.ReadAll(resp.Body)
+		if err != nil {
+			err = fmt.Errorf("failed to get contents of response %s: %s", path, err)
+		}
+		return content, err
+	case SourceKindRegistry:
+		var _, repo, err = getOrInitRepo(i.url)
+		if err != nil {
+			return content, err
+		}
+		var tree *object.Tree = nil
+		tree, err = getTree(repo)
+		if err != nil {
+			return content, err
+		}
+		var file *object.File = nil
+		file, err = tree.File(path)
+		if err != nil {
+			return content, fmt.Errorf("failed to find path %s in tree: %s", path, err)
+		}
+		var reader io.ReadCloser = nil
+		reader, err = file.Reader()
+		if err != nil {
+			err = fmt.Errorf("failed to get reader for file %s: %s", path, err)
+		}
+		content, err = io.ReadAll(reader)
+		if err != nil {
+			err = fmt.Errorf("failed to get contents of file %s: %s", path, err)
+		}
+		return content, err
+	}
+	return content, fmt.Errorf("unsupported Remote")
+}
+
+func getOrInitRepo(url string) (*memory.Storage, *git.Repository, error) {
+	var repo *git.Repository = nil
+	var err error = nil
+
+	var storage, ok = RegistryRepos[url]
+	//Todo: Should we use an on-disk storage?
+	if !ok {
+		storage = memory.NewStorage()
+		RegistryRepos[url] = storage
+		repo, err = git.Init(storage, memfs.New())
+		if err != nil {
+			return storage, nil, fmt.Errorf("unable to initialise repo: %s", err)
+		}
+		err = updateRepo(repo, url)
+		if err != nil {
+			err = fmt.Errorf("unable to fetch registry information: %s", err)
+		}
+	} else {
+		repo, err = git.Open(storage, memfs.New())
+		if err != nil {
+			err = fmt.Errorf("unable to open repository: %s", err)
+		}
+	}
+	return storage, repo, err
+}
+
+func updateRepo(repo *git.Repository, url string) error {
+	//Todo: cargo re-initialises the repo, if the fetch fails. Do we need to copy that?
+	//see https://github.com/rust-lang/cargo/blob/b134eff5cedcaa4879f60035d62630400e7fd543/src/cargo/sources/git/utils.rs#L1150
+	var remote, err = repo.CreateRemoteAnonymous(&config.RemoteConfig{
+		Name:   "anonymous",
+		URLs:   []string{url},
+		Mirror: false,
+		//see https://github.com/rust-lang/cargo/blob/b134eff5cedcaa4879f60035d62630400e7fd543/src/cargo/sources/git/utils.rs#L979
+		Fetch: []config.RefSpec{"+HEAD:refs/remotes/origin/HEAD"},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create anonymous remote for url %s: %s", url, err)
+	}
+	err = remote.Fetch(&git.FetchOptions{
+		RemoteName: "origin",
+		Depth:      1,
+		//Todo: support private repos by allowing auth information to be specified
+		Auth:            nil,
+		Progress:        nil,
+		Tags:            git.NoTags,
+		Force:           false,
+		InsecureSkipTLS: false,
+		CABundle:        nil,
+		ProxyOptions:    transport.ProxyOptions{},
+		Prune:           false,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to fetch registry information from url %s: %s", url, err)
+	}
+	return err
+}
+
+func getTree(repo *git.Repository) (*object.Tree, error) {
+	var ref, err = repo.Reference("refs/remotes/origin/HEAD", true)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get reference to refs/remotes/origin/HEAD: %s", err)
+	}
+
+	var hash = ref.Hash()
+	var commit *object.Commit = nil
+	commit, err = repo.CommitObject(hash)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get commit from repo head: %s", err)
+	}
+
+	var tree *object.Tree = nil
+	tree, err = commit.Tree()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Tree from Commit: %s", err)
+	}
+
+	return tree, err
+}
--- syft/pkg/rust.go
+++ syft/pkg/rust.go
@@ -5,12 +5,7 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"github.com/go-git/go-billy/v5/memfs"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/config"
-	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/go-git/go-git/v5/plumbing/transport"
-	"github.com/go-git/go-git/v5/storage/memory"
+	"github.com/anchore/syft/syft/pkg/cataloger/rust"
 	"github.com/spdx/tools-golang/spdx"
 	"io"
 	"net/http"
@@ -32,70 +27,10 @@ type RustBinaryAuditEntry struct {
 	Source  string `toml:"source" json:"source"`
 }
 
-type RustRepositoryConfig struct {
-	Download     string `json:"dl"`
-	API          string `json:"api"`
-	AuthRequired bool   `json:"auth-required"`
-}
-
-type SourceId struct {
-	kind string
-	url  string
-}
-
-type DependencyInformation struct {
-	Name          string                            `json:"name"`
-	Version       string                            `json:"vers"`
-	Dependencies  []DependencyDependencyInformation `json:"deps"`
-	Checksum      string                            `json:"cksum"`
-	Features      map[string]string                 `json:"features"`
-	Yanked        bool                              `json:"yanked"`
-	Links         string                            `json:"links"`
-	StructVersion int                               `json:"v"`
-	Features2     map[string]string                 `json:"features2"`
-	RustVersion   string                            `json:"rust_version"`
-}
-type DependencyDependencyInformation struct {
-	Name           string   `json:"name"`
-	Requirement    string   `json:"req"`
-	Features       []string `json:"features"`
-	Optional       bool     `json:"optional"`
-	DefaultTargets bool     `json:"default_targets"`
-	Target         string   `json:"target"`
-	Kind           string   `json:"kind"`
-	Registry       string   `json:"registry"`
-	Package        string   `json:"package"`
-}
-
-// see https://github.com/rust-lang/cargo/blob/master/crates/cargo-util-schemas/src/core/source_kind.rs
-const (
-	SourceKindPath           = "path"
-	SourceKindGit            = "git"
-	SourceKindRegistry       = "registry"
-	SourceKindLocalRegistry  = "local-registry"
-	SourceKindSparse         = "sparse"
-	SourceKindLocalDirectory = "directory"
-)
-
-var RegistryRepos = make(map[string]*memory.Storage)
-var RegistryConfig = make(map[string]RustRepositoryConfig)
-
 // GetChecksumType This exists, to made adopting new potential cargo.lock versions easier
 func (r *RustCargoLockEntry) GetChecksumType() spdx.ChecksumAlgorithm {
 	//Cargo currently always uses Sha256: https://github.com/rust-lang/cargo/blob/a9ee3e82b57df019dfc0385f844bc6928150ee63/src/cargo/sources/registry/download.rs#L125
 	return spdx.SHA256
-}
-
-func (r *RustCargoLockEntry) getSourceId() (*SourceId, error) {
-	var before, after, found = strings.Cut(r.Source, "+")
-	if !found {
-		return nil, fmt.Errorf("did not find \"+\" in source field of dependency: Name: %s, Version: %s, Source: %s", r.Name, r.Version, r.Source)
-	}
-
-	return &SourceId{
-		kind: before,
-		url:  after,
-	}, nil
 }
 
 // GetPrefix get {path} for https://doc.rust-lang.org/cargo/reference/registry-index.html
@@ -115,11 +50,11 @@ func (r *RustCargoLockEntry) GetPrefix() string {
 }
 
 func (r *RustCargoLockEntry) GetDownloadLink() (string, error) {
-	var sourceId, err = r.getSourceId()
+	var sourceId, err = rust.GetSourceId(r)
 	if err != nil {
 		return "", err
 	}
-	var repoConfig *RustRepositoryConfig = nil
+	var repoConfig *rust.RepositoryConfig = nil
 	repoConfig, err = sourceId.GetConfig()
 	if err != nil {
 		return "", err
@@ -172,9 +107,9 @@ func (r *RustCargoLockEntry) GetDownloadSha() []byte {
 	var hash = sha256.New().Sum(content)
 	return hash
 }
-func (r *RustCargoLockEntry) GetIndexContent() ([]DependencyInformation, []error) {
-	var deps []DependencyInformation
-	var sourceID, err = r.getSourceId()
+func (r *RustCargoLockEntry) GetIndexContent() ([]rust.DependencyInformation, []error) {
+	var deps []rust.DependencyInformation
+	var sourceID, err = rust.GetSourceId(r)
 	if err != nil {
 		return deps, []error{err}
 	}
@@ -182,7 +117,7 @@ func (r *RustCargoLockEntry) GetIndexContent() ([]DependencyInformation, []error
 	var errors []error
 	content, err = sourceID.GetPath(r.GetIndexPath())
 	for _, v := range bytes.Split(content, []byte("\n")) {
-		var depInfo = DependencyInformation{
+		var depInfo = rust.DependencyInformation{
 			StructVersion: 1,
 		}
 		err = json.Unmarshal(v, &depInfo)
@@ -193,146 +128,4 @@ func (r *RustCargoLockEntry) GetIndexContent() ([]DependencyInformation, []error
 		}
 	}
 	return deps, errors
-}
-
-// RepositoryConfigName see https://github.com/rust-lang/cargo/blob/b134eff5cedcaa4879f60035d62630400e7fd543/src/cargo/sources/registry/mod.rs#L962
-const RepositoryConfigName = "config.json"
-
-func (i *SourceId) GetConfig() (*RustRepositoryConfig, error) {
-	if repoConfig, ok := RegistryConfig[i.url]; ok {
-		return &repoConfig, nil
-	}
-	content, err := i.GetPath(RepositoryConfigName)
-	if err != nil {
-		return nil, err
-	}
-	var repoConfig = RustRepositoryConfig{}
-	err = json.Unmarshal(content, &repoConfig)
-	if err != nil {
-		err = fmt.Errorf("failed to deserialize rust repository configuration: %s", err)
-	}
-	RegistryConfig[i.url] = repoConfig
-	return &repoConfig, err
-}
-
-func (i *SourceId) GetPath(path string) ([]byte, error) {
-	var content []byte
-	switch i.kind {
-	case SourceKindSparse:
-		var resp, err = http.Get(fmt.Sprintf("%s/%s", i.url, path))
-		if err != nil {
-			return content, fmt.Errorf("could not get the path %s/%s from sparse registry: %s", i.url, path, err)
-		}
-		content, err = io.ReadAll(resp.Body)
-		if err != nil {
-			err = fmt.Errorf("failed to get contents of response %s: %s", path, err)
-		}
-		return content, err
-	case SourceKindRegistry:
-		var _, repo, err = getOrInitRepo(i.url)
-		if err != nil {
-			return content, err
-		}
-		var tree *object.Tree = nil
-		tree, err = getTree(repo)
-		if err != nil {
-			return content, err
-		}
-		var file *object.File = nil
-		file, err = tree.File(path)
-		if err != nil {
-			return content, fmt.Errorf("failed to find path %s in tree: %s", path, err)
-		}
-		var reader io.ReadCloser = nil
-		reader, err = file.Reader()
-		if err != nil {
-			err = fmt.Errorf("failed to get reader for file %s: %s", path, err)
-		}
-		content, err = io.ReadAll(reader)
-		if err != nil {
-			err = fmt.Errorf("failed to get contents of file %s: %s", path, err)
-		}
-		return content, err
-	}
-	return content, fmt.Errorf("unsupported Remote")
-}
-
-func getOrInitRepo(url string) (*memory.Storage, *git.Repository, error) {
-	var repo *git.Repository = nil
-	var err error = nil
-
-	var storage, ok = RegistryRepos[url]
-	//Todo: Should we use an on-disk storage?
-	if !ok {
-		storage = memory.NewStorage()
-		RegistryRepos[url] = storage
-		repo, err = git.Init(storage, memfs.New())
-		if err != nil {
-			return storage, nil, fmt.Errorf("unable to initialise repo: %s", err)
-		}
-		err = updateRepo(repo, url)
-		if err != nil {
-			err = fmt.Errorf("unable to fetch registry information: %s", err)
-		}
-	} else {
-		repo, err = git.Open(storage, memfs.New())
-		if err != nil {
-			err = fmt.Errorf("unable to open repository: %s", err)
-		}
-	}
-	return storage, repo, err
-}
-
-func updateRepo(repo *git.Repository, url string) error {
-	//Todo: cargo re-initialises the repo, if the fetch fails. Do we need to copy that?
-	//see https://github.com/rust-lang/cargo/blob/b134eff5cedcaa4879f60035d62630400e7fd543/src/cargo/sources/git/utils.rs#L1150
-	var remote, err = repo.CreateRemoteAnonymous(&config.RemoteConfig{
-		Name:   "anonymous",
-		URLs:   []string{url},
-		Mirror: false,
-		//see https://github.com/rust-lang/cargo/blob/b134eff5cedcaa4879f60035d62630400e7fd543/src/cargo/sources/git/utils.rs#L979
-		Fetch: []config.RefSpec{"+HEAD:refs/remotes/origin/HEAD"},
-	})
-	if err != nil {
-		return fmt.Errorf("failed to create anonymous remote for url %s: %s", url, err)
-	}
-	err = remote.Fetch(&git.FetchOptions{
-		RemoteName: "origin",
-		Depth:      1,
-		//Todo: support private repos by allowing auth information to be specified
-		Auth:            nil,
-		Progress:        nil,
-		Tags:            git.NoTags,
-		Force:           false,
-		InsecureSkipTLS: false,
-		CABundle:        nil,
-		ProxyOptions:    transport.ProxyOptions{},
-		Prune:           false,
-	})
-	if err != nil {
-		return fmt.Errorf("failed to fetch registry information from url %s: %s", url, err)
-	}
-	return err
-}
-
-func getTree(repo *git.Repository) (*object.Tree, error) {
-	var ref, err = repo.Reference("refs/remotes/origin/HEAD", true)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get reference to refs/remotes/origin/HEAD: %s", err)
-	}
-
-	var hash = ref.Hash()
-	var commit *object.Commit = nil
-	commit, err = repo.CommitObject(hash)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get commit from repo head: %s", err)
-	}
-
-	var tree *object.Tree = nil
-	tree, err = commit.Tree()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get Tree from Commit: %s", err)
-	}
-
-	return tree, err
 }
