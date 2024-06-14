6a7a87348f61fbc59c6039a34e6406936b9cc4b9
Christopher Phillips <32073428+spiffcs@users.noreply.github.com>
2024-06-14 11:57:57 -0400

    fix: fix parsing for complex toml types
    Signed-off-by: Christopher Phillips <32073428+spiffcs@users.noreply.github.com>

--- syft/pkg/cataloger/python/parse_poetry_lock.go
+++ syft/pkg/cataloger/python/parse_poetry_lock.go
@@ -3,23 +3,23 @@ package python
 import (
 	"context"
 	"fmt"
-	"sort"
-	"strings"
-
-	"github.com/pelletier/go-toml"
-
+	"github.com/BurntSushi/toml"
 	"github.com/anchore/syft/syft/artifact"
 	"github.com/anchore/syft/syft/file"
 	"github.com/anchore/syft/syft/pkg"
 	"github.com/anchore/syft/syft/pkg/cataloger/generic"
 	"github.com/anchore/syft/syft/pkg/cataloger/internal/dependency"
+	"reflect"
+	"sort"
 )
 
 // integrity check
 var _ generic.Parser = parsePoetryLock
 
 type poetryPackageSource struct {
-	URL string `toml:"url"`
+	URL       string `toml:"url"`
+	Type      string `toml:"type"`
+	Reference string `toml:"reference"`
 }
 
 type poetryPackages struct {
@@ -27,14 +27,14 @@ type poetryPackages struct {
 }
 
 type poetryPackage struct {
-	Name         string                             `toml:"name"`
-	Version      string                             `toml:"version"`
-	Category     string                             `toml:"category"`
-	Description  string                             `toml:"description"`
-	Optional     bool                               `toml:"optional"`
-	Source       poetryPackageSource                `toml:"source"`
-	Dependencies map[string]poetryPackageDependency `toml:"dependencies"`
-	Extras       map[string][]string                `toml:"extras"`
+	Name         string                               `toml:"name"`
+	Version      string                               `toml:"version"`
+	Category     string                               `toml:"category"`
+	Description  string                               `toml:"description"`
+	Optional     bool                                 `toml:"optional"`
+	Source       poetryPackageSource                  `toml:"source"`
+	Dependencies map[string][]poetryPackageDependency `toml:"dependencies"`
+	Extras       map[string][]string                  `toml:"extras"`
 }
 
 type poetryPackageDependency struct {
@@ -44,39 +44,151 @@ type poetryPackageDependency struct {
 	Extras   []string `toml:"extras"`
 }
 
-func (d *poetryPackageDependency) UnmarshalText(data []byte) error {
-	// attempt to parse as a map first
-	var dep map[string]interface{}
-	if err := toml.Unmarshal(data, &dep); err == nil {
-		if extras, ok := dep["extras"]; ok {
-			if extrasList, ok := extras.([]string); ok {
-				d.Extras = extrasList
-			}
-		}
-
-		if markers, ok := dep["markers"]; ok {
-			if markersString, ok := markers.(string); ok {
-				d.Markers = markersString
-			}
-		}
-
-		if version, ok := dep["version"]; ok {
-			if versionString, ok := version.(string); ok {
-				d.Version = versionString
-			}
-		}
-		return nil
+func (p *poetryPackage) UnmarshalTOML(data any) error {
+	d, _ := data.(map[string]any)
+	err := p.decodeTomlFields(d)
+	if err != nil {
+		return err
 	}
+	return err
+}
 
-	if strings.ContainsAny(string(data), "[]{}") {
-		// odds are this is really a malformed toml array or object
-		return fmt.Errorf("unable to parse poetry dependency: version is malformed array/object: %q", string(data))
+func (p *poetryPackage) decodeTomlFields(d map[string]interface{}) error {
+	newPackage := poetryPackage{}
+	structType := reflect.TypeOf(newPackage)
+	structValue := reflect.ValueOf(&newPackage).Elem()
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+		tag := field.Tag.Get("toml")
+		if tag == "" {
+			continue
+		}
+
+		// Check if the field exists in the map
+		value, ok := d[tag]
+		if !ok {
+			continue
+		}
+
+		// Set the field value in the struct
+		structField := structValue.FieldByName(field.Name)
+		if !structField.IsValid() || !structField.CanSet() {
+			continue
+		}
+
+		// Convert the value to the field type
+		switch structField.Kind() {
+		case reflect.String:
+			structField.SetString(value.(string))
+		case reflect.Int:
+			structField.SetInt(int64(value.(int)))
+		case reflect.Bool:
+			structField.SetBool(value.(bool))
+		case reflect.Struct:
+			// If the field is a struct, recursively decode it
+			mapValue := value.(map[string]interface{})
+			decodeStruct(mapValue, structField.Addr().Interface())
+		case reflect.Map:
+			switch tag {
+			case "dependencies":
+				value = parseDependencies(value)
+				structField.Set(reflect.ValueOf(value))
+			case "extras":
+				value = parsePoetryExtras(value)
+				structField.Set(reflect.ValueOf(value))
+			}
+		default:
+		}
 	}
-
-	// assume this is a simple version string
-	d.Version = string(data)
-
+	p.Name = newPackage.Name
+	p.Version = newPackage.Version
+	p.Category = newPackage.Category
+	p.Description = newPackage.Description
+	p.Optional = newPackage.Optional
+	p.Source = newPackage.Source
+	p.Extras = newPackage.Extras
+	p.Dependencies = newPackage.Dependencies
+	p.Extras = newPackage.Extras
 	return nil
+}
+
+func parsePoetryExtras(d any) map[string][]string {
+	extras := map[string][]string{}
+	parsedData := d.(map[string]interface{})
+	for key, value := range parsedData {
+		extras[key] = convertSliceString(value)
+	}
+	return extras
+}
+
+func convertSliceString(is interface{}) []string {
+	// Convert slice of interface{} to slice of string
+	iss := is.([]interface{})
+	var stringSlice []string
+	for _, val := range iss {
+		if str, ok := val.(string); ok {
+			stringSlice = append(stringSlice, str)
+		}
+	}
+	return stringSlice
+}
+
+func parseDependencies(d any) map[string][]poetryPackageDependency {
+	dependencies := map[string][]poetryPackageDependency{}
+	// we know we have dependencies here so let's convert to a map[string]interface{}
+	parsedData := d.(map[string]interface{})
+	for key, value := range parsedData {
+		dependencies[key] = make([]poetryPackageDependency, 0)
+		t := reflect.TypeOf(value)
+		switch t.Kind() {
+		case reflect.String:
+			dependencies[key] = append(dependencies[key], poetryPackageDependency{Version: value.(string)})
+		case reflect.Slice:
+			complexDeps := value.([]interface{})
+			for _, complex := range complexDeps {
+				convertedComplex := complex.(map[string]interface{})
+				// TODO: we need to assert these map values or we will panic
+				newDep := poetryPackageDependency{
+					Version: convertedComplex["version"].(string),
+					Markers: convertedComplex["markers"].(string),
+				}
+				dependencies[key] = append(dependencies[key], newDep)
+			}
+		}
+	}
+	return dependencies
+}
+
+func decodeStruct(data map[string]interface{}, v interface{}) {
+	structType := reflect.TypeOf(v).Elem()
+	structValue := reflect.ValueOf(v).Elem()
+
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+		tag := field.Tag.Get("toml")
+		if tag == "" {
+			continue
+		}
+
+		value, ok := data[tag]
+		if !ok {
+			continue
+		}
+
+		fieldValue := structValue.Field(i)
+		if !fieldValue.IsValid() || !fieldValue.CanSet() {
+			continue
+		}
+
+		switch fieldValue.Kind() {
+		case reflect.String:
+			fieldValue.SetString(value.(string))
+		case reflect.Int:
+			fieldValue.SetInt(int64(value.(int)))
+		default:
+			fmt.Println("Unsupported type")
+		}
+	}
 }
 
 // parsePoetryLock is a parser function for poetry.lock contents, returning all python packages discovered.
@@ -93,15 +205,10 @@ func parsePoetryLock(_ context.Context, _ file.Resolver, _ *generic.Environment,
 }
 
 func poetryLockPackages(reader file.LocationReadCloser) ([]pkg.Package, error) {
-	tree, err := toml.LoadReader(reader)
-	if err != nil {
-		return nil, fmt.Errorf("unable to load poetry.lock for parsing: %w", err)
-	}
-
 	metadata := poetryPackages{}
-	err = tree.Unmarshal(&metadata)
+	_, err := toml.NewDecoder(reader).Decode(&metadata)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse poetry.lock: %w", err)
+		return nil, fmt.Errorf("failed to read poetry lock package: %w", err)
 	}
 
 	var pkgs []pkg.Package
@@ -137,13 +244,15 @@ func extractIndex(p poetryPackage) string {
 
 func extractPoetryDependencies(p poetryPackage) []pkg.PythonPoetryLockDependencyEntry {
 	var deps []pkg.PythonPoetryLockDependencyEntry
-	for name, dep := range p.Dependencies {
-		deps = append(deps, pkg.PythonPoetryLockDependencyEntry{
-			Name:    name,
-			Version: dep.Version,
-			Extras:  dep.Extras,
-			Markers: dep.Markers,
-		})
+	for name, dependencies := range p.Dependencies {
+		for _, d := range dependencies {
+			deps = append(deps, pkg.PythonPoetryLockDependencyEntry{
+				Name:    name,
+				Version: d.Version,
+				Extras:  d.Extras,
+				Markers: d.Markers,
+			})
+		}
 	}
 	sort.Slice(deps, func(i, j int) bool {
 		return deps[i].Name < deps[j].Name
