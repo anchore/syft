1433a2c3aa7fd3e59f1e0732b15595023a1d35e0
Keith Zantow <kzantow@gmail.com>
2024-06-23 01:47:27 -0400

    chore: use go-build with RunTools
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
 .PHONY: *
 
 help:
-	go run -C buildtools . -l
+	@go run -C buildtools . -l
 
 bootstrap:
-	go run -C buildtools . bootstrap
+	@go run -C buildtools . bootstrap
 
 unit:
-	go run -C buildtools . $@
+	@go run -C buildtools . $@
 
 # for some reason test does not work without an explicit target - the dir?
 test:
-	go run -C buildtools . $@
+	@go run -C buildtools . $@
 
 %:
-	go run -C buildtools . $@
+	@go run -C buildtools . $@
--- buildtools/main.go
+++ buildtools/main.go
@@ -1,77 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"path"
-	"path/filepath"
-	"runtime"
-
-	"golang.org/x/mod/modfile"
-
-	_ "github.com/anchore/binny/cmd/binny/cli" // so go mod tidy doesn't remove necessary packages
+	"github.com/kzantow/go-build"
 )
 
 func main() {
-	noerr(buildIfMissing(exe("../.tool/binny"), "github.com/anchore/binny/cmd/binny"))
-	noerr(os.Chdir(".."))
-	noerr(run(exe(".tool/binny"), "install", "-v"))
-	noerr(run(exe(".tool/task"), os.Args[1:]...))
-}
-
-func buildIfMissing(file, pkg string) error {
-	_, err := os.Stat(file)
-	if err == nil {
-		return nil
-	}
-	write("Building: %s", pkg)
-	return run("go", "build", "-o", file, "-ldflags", "-w -s -extldflags '-static' -X main.version="+binnyVersion(), pkg)
-}
-
-//nolint:gosec
-func run(cmd string, args ...string) error {
-	c := exec.Command(cmd, args...)
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-	c.Stdin = os.Stdin
-	return c.Run()
-}
-
-func noerr(e error) {
-	if e != nil {
-		write("ERROR: %v", e)
-		os.Exit(1)
-	}
-}
-
-func noerrGet[T any](t T, e error) T {
-	if e != nil {
-		write("ERROR: %v", e)
-		os.Exit(1)
-	}
-	return t
-}
-
-func write(msg string, args ...any) {
-	_, _ = fmt.Fprintln(os.Stderr, fmt.Sprintf(msg, args...))
-}
-
-func exe(s string) string {
-	out := filepath.Join(path.Split(s))
-	if runtime.GOOS == "windows" {
-		out += ".exe"
-	}
-	return out
-}
-
-func binnyVersion() string {
-	contents := noerrGet(os.ReadFile("go.mod"))
-	f := noerrGet(modfile.Parse("go.mod", contents, nil))
-	for _, r := range f.Require {
-		if r.Mod.Path == "github.com/anchore/binny" {
-			return r.Mod.Version
-		}
-	}
-	return "UNKNOWN"
+	build.RunTools()
 }
