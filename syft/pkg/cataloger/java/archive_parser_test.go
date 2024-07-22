a21d722df98f35bcf3b59e30d7b5ff7fd42ee4d6
dor-hayun <dor.hayun@mend.io>
2024-07-22 12:46:57 +0300

    fix: update 'guessMainPackageNameAndVersionFromPomInfo' and 'artifactIDMatchesFilename'
    - Correct retrieval of package name when main POM file exists
    - Address issue where wrong package name was retrieved for certain jars
    - Example case: 'jansi' jar containing multiple jars like 'jansi-win32'
    - Ensure true is returned when filename matches the artifact ID, prevent random retrieval by checking prefix and suffix
    - Use fallback check with suffix and prefix if no POM properties file matches the exact artifact name
    
    Signed-off-by: dor-hayun <dor.hayun@mend.io>

--- syft/pkg/cataloger/java/archive_parser_test.go
+++ syft/pkg/cataloger/java/archive_parser_test.go
@@ -1156,7 +1156,7 @@ func Test_artifactIDMatchesFilename(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, artifactIDMatchesFilename(tt.artifactID, tt.fileName))
+			assert.Equal(t, tt.want, artifactIDMatchesFilename(tt.artifactID, tt.fileName, nil))
 		})
 	}
 }
