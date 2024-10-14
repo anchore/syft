22d756e342d893719d2f7e7723839baef4552a96
HeyeOpenSource <opensource@heye-international.com>
2024-10-14 16:40:34 +0200

    - Implemented the FieldDescriber interface for the cmd/syft/internal/options dotnetConfig struct.
    Signed-off-by: HeyeOpenSource <opensource@heye-international.com>

--- cmd/syft/internal/options/dotnet.go
+++ cmd/syft/internal/options/dotnet.go
@@ -3,6 +3,7 @@ package options
 import (
 	"strings"
 
+	"github.com/anchore/clio"
 	"github.com/anchore/syft/syft/pkg/cataloger/dotnet"
 )
 
@@ -10,6 +11,18 @@ type dotnetConfig struct {
 	SearchLocalLicenses  bool   `yaml:"search-local-licenses" json:"search-local-licenses" mapstructure:"search-local-licenses"`
 	SearchRemoteLicenses bool   `yaml:"search-remote-licenses" json:"search-remote-licenses" mapstructure:"search-remote-licenses"`
 	Providers            string `yaml:"package-providers,omitempty" json:"package-providers,omitempty" mapstructure:"package-providers"`
+}
+
+var _ interface {
+	clio.FieldDescriber
+} = (*dotnetConfig)(nil)
+
+func (o *dotnetConfig) DescribeFields(descriptions clio.FieldDescriptionSet) {
+	descriptions.Add(&o.SearchLocalLicenses, `search for NuGet package licences in the local cache of the system running Syft, note that this is outside the
+container filesystem and probably outside the root of a local directory scan`)
+	descriptions.Add(&o.SearchRemoteLicenses, `search for NuGet package licences by retrieving the package from a network proxy`)
+	descriptions.Add(&o.Providers, `remote proxies (comma-separated) to use when retrieving go packages from the network,
+if unset this defaults to the NuGet-repositories known to the DotNet environment`)
 }
 
 func defaultDotnetConfig() dotnetConfig {
