4644a0b2ec0bf1750f809f78bf78ef9629660d70
HeyeOpenSource <opensource@heye-international.com>
2024-10-15 08:18:11 +0200

    - Corrected DotNet config field description.
    Signed-off-by: HeyeOpenSource <opensource@heye-international.com>

--- cmd/syft/internal/options/dotnet.go
+++ cmd/syft/internal/options/dotnet.go
@@ -21,7 +21,7 @@ func (o *dotnetConfig) DescribeFields(descriptions clio.FieldDescriptionSet) {
 	descriptions.Add(&o.SearchLocalLicenses, `search for NuGet package licences in the local cache of the system running Syft, note that this is outside the
 container filesystem and probably outside the root of a local directory scan`)
 	descriptions.Add(&o.SearchRemoteLicenses, `search for NuGet package licences by retrieving the package from a network proxy`)
-	descriptions.Add(&o.Providers, `remote proxies (comma-separated) to use when retrieving go packages from the network,
+	descriptions.Add(&o.Providers, `remote proxies (comma-separated) to use when retrieving NuGet packages from the network,
 if unset this defaults to the NuGet-repositories known to the DotNet environment`)
 }
 
