64a433e129d33377df5226abb9e37ba3d54b6cd7
HeyeOpenSource <opensource@heye-international.com>
2024-10-18 08:20:04 +0200

    - Harmonize custom license line endings to UNIX style before registration in order to avoid incompatibilities (and multiple instances of the same license).
    Signed-off-by: HeyeOpenSource <opensource@heye-international.com>

--- internal/licenses/parser.go
+++ internal/licenses/parser.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/google/licensecheck"
 
@@ -40,6 +41,11 @@ func Parse(reader io.Reader, l file.Location) (licenses []pkg.License, err error
 	if cov.Percent < coverageThreshold {
 		// unknown or no licenses here?
 		if len(contents) > 0 {
+			// harmonize line endings to unix compatible first:
+			// 1. \r\n => \n   (Windows   => UNIX)
+			// 2. \r   => \n   (Macintosh => UNIX)
+			contents = []byte(strings.ReplaceAll(strings.ReplaceAll(string(contents), "\r\n", "\n"), "\r", "\n"))
+
 			lic := pkg.NewLicenseFromLocations(unknownLicenseType, l)
 			lic.SPDXExpression = UnknownLicensePrefix + getCustomLicenseContentHash(contents)
 			lic.Contents = string(contents)
