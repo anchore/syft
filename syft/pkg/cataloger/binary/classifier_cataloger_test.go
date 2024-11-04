8a41d772509d37267a65e0b425808e883e4b9dce
Christopher Angelo Phillips <32073428+spiffcs@users.noreply.github.com>
2024-11-04 15:23:27 -0500

    chore: prevent file resolver from bubbling errors in binary cataloger (#3410)
    Signed-off-by: Christopher Phillips <32073428+spiffcs@users.noreply.github.com>
    Signed-off-by: Keith Zantow <kzantow@gmail.com>
    Co-authored-by: Keith Zantow <kzantow@gmail.com>

--- syft/pkg/cataloger/binary/classifier_cataloger_test.go
+++ syft/pkg/cataloger/binary/classifier_cataloger_test.go
@@ -1668,7 +1668,7 @@ func Test_Cataloger_ResilientToErrors(t *testing.T) {
 
 	resolver := &panicyResolver{}
 	_, _, err := c.Catalog(context.Background(), resolver)
-	assert.Error(t, err)
+	assert.Nil(t, err) // non-coordinate-based FindBy* errors are now logged and not returned
 	assert.True(t, resolver.searchCalled)
 }
 
