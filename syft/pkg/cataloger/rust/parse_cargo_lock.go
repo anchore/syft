10205fffecc9f2466cb57ccd8aee94f78ab4a65e
Will Murphy <willmurphyscode@users.noreply.github.com>
2024-12-06 08:12:09 -0500

    feat: parse Cargo.lock git dependency relationships
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

--- syft/pkg/cataloger/rust/parse_cargo_lock.go
+++ syft/pkg/cataloger/rust/parse_cargo_lock.go
@@ -3,15 +3,17 @@ package rust
 import (
 	"context"
 	"fmt"
-	"github.com/anchore/syft/syft/pkg/cataloger/internal/dependency"
+	"strings"
 
 	"github.com/pelletier/go-toml"
 
+	"github.com/anchore/syft/internal/log"
 	"github.com/anchore/syft/internal/unknown"
 	"github.com/anchore/syft/syft/artifact"
 	"github.com/anchore/syft/syft/file"
 	"github.com/anchore/syft/syft/pkg"
 	"github.com/anchore/syft/syft/pkg/cataloger/generic"
+	"github.com/anchore/syft/syft/pkg/cataloger/internal/dependency"
 )
 
 var _ generic.Parser = parseCargoLock
@@ -53,19 +55,36 @@ func parseCargoLock(_ context.Context, _ file.Resolver, _ *generic.Environment, 
 }
 
 func dependencySpecification(p pkg.Package) dependency.Specification {
+	rustMeta, ok := p.Metadata.(pkg.RustCargoLockEntry)
+	if !ok {
+		log.Tracef("cataloger failed to extract rust Cargo.lock metadata for package %+v", p.Name)
+		return dependency.Specification{}
+	}
+
+	// Cargo.lock dependencies are strings that are the name of a package, if that
+	// is unambiguous, or a string like "name version" if the name alone is not
+	// ambiguous, or strings like "name version (source)" if "name version" is ambiguous.
+	// Provide all the strings, since we don't know which string will be used.
+	// In other words, each package "provides" 3 entries, one for each name format,
+	// and each package "requires" whatever it actually requires based on the Cargo.lock.
+	provides := []string{
+		p.Name,
+		fmt.Sprintf("%s %s", p.Name, p.Version),
+	}
+
+	if rustMeta.Source != "" {
+		src := rustMeta.Source
+		if strings.HasPrefix(src, "git") && strings.Contains(src, "#") {
+			src = strings.Split(src, "#")[0]
+		}
+
+		provides = append(provides, fmt.Sprintf("%s %s (%s)", p.Name, p.Version, src))
+	}
+
 	return dependency.Specification{
 		ProvidesRequires: dependency.ProvidesRequires{
-			// Cargo.lock dependencies are strings that are the name of a package, if that
-			// is unambiguous, or a string like "name version" if the name alone is not
-			// ambiguous. Set both keys in the map, since we don't know which key is
-			// going to be used until we're trying to resolve dependencies. If the
-			// first key is overwritten, that means the package name was an ambiguous dependency
-			// and "name version" will be used as the key anyway.
-			Provides: []string{
-				p.Name,
-				fmt.Sprintf("%s %s", p.Name, p.Version),
-			},
-			Requires: p.Metadata.(pkg.RustCargoLockEntry).Dependencies,
+			Provides: provides,
+			Requires: rustMeta.Dependencies,
 		},
 	}
 }
