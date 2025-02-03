41b58ec53b1541046b984ce95fc669dea8326c02
Alex Goodman <wagoodman@users.noreply.github.com>
2025-02-03 13:59:00 -0500

    address PR feedback
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/cataloging/filecataloging/tags.go
+++ syft/cataloging/filecataloging/tags.go
@@ -1,6 +1,6 @@
 package filecataloging
 
 const (
-	// FileTag should be used to identify catalogers that are file-based.
+	// FileTag should be used to identify catalogers that primarily discover information about files (as opposed to packages).
 	FileTag = "file"
 )
