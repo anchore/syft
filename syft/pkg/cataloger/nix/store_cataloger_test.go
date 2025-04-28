648332ce0b77e158677d06d176b9853413c748b2
Alex Goodman <wagoodman@users.noreply.github.com>
2025-04-28 12:47:47 -0400

    repin build image and disable syscall filtering
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/pkg/cataloger/nix/store_cataloger_test.go
+++ syft/pkg/cataloger/nix/store_cataloger_test.go
@@ -35,19 +35,19 @@ func TestStoreCataloger_Image(t *testing.T) {
 			//     └───/nix/store/1x3s2v9wc9m302cspfqcn2iwar0b5w99-jq-1.6-lib [...]
 			fixture: "image-nixos-jq-pkg-store",
 			wantPkgs: []string{
-				"glibc @ 2.37-8 (/nix/store/02mqs1by2vab9yzw0qc4j7463w78p3ps-glibc-2.37-8)",
-				"jq @ 1.6 (/nix/store/1x3s2v9wc9m302cspfqcn2iwar0b5w99-jq-1.6-lib)",
-				"jq @ 1.6 (/nix/store/nzwfgsp28vgxv7n2gl5fxqkca9awh4dz-jq-1.6-bin)",
-				"libidn2 @ 2.3.4 (/nix/store/cw8fpl8r1x9rmaqj55fwbfnnrgw7b40k-libidn2-2.3.4)",
-				"libunistring @ 1.1 (/nix/store/h1ysk4vvw48winwmh38rvnsj0dlsz7c1-libunistring-1.1)",
-				"oniguruma @ 6.9.8 (/nix/store/mzj90j6m3c3a1vv8j9pl920f98i2yz9q-oniguruma-6.9.8-lib)",
-				"xgcc @ 12.3.0 (/nix/store/fmz62d844wf4blb11k21f4m0q6n6hdfp-xgcc-12.3.0-libgcc)",
+				"glibc @ 2.37-8 (/nix/store/aw2fw9ag10wr9pf0qk4nk5sxi0q0bn56-glibc-2.37-8)",
+				"jq @ 1.6 (/nix/store/3xpzpmcqmzsdblkzqa9d9s6l302pnk4g-jq-1.6-lib)",
+				"jq @ 1.6 (/nix/store/aj8lqifsyynq8iknivvxkrsqnblj7qzs-jq-1.6-bin)",
+				"libidn2 @ 2.3.4 (/nix/store/k8ivghpggjrq1n49xp8sj116i4sh8lia-libidn2-2.3.4)",
+				"libunistring @ 1.1 (/nix/store/s2gi8pfjszy6rq3ydx0z1vwbbskw994i-libunistring-1.1)",
+				"oniguruma @ 6.9.8 (/nix/store/dpcyirvyblnflf7cp14dnr1420va93zx-oniguruma-6.9.8-lib)",
+				"xgcc @ 12.3.0 (/nix/store/jbwb8d8l28lg9z0xzl784wyb9vlbwss6-xgcc-12.3.0-libgcc)",
 			},
 			wantRel: []string{
 				// note: parsing all relationships from only derivations results in partial results! (this is why the DB cataloger exists)
-				"libidn2 @ 2.3.4 (/nix/store/cw8fpl8r1x9rmaqj55fwbfnnrgw7b40k-libidn2-2.3.4) [dependency-of] glibc @ 2.37-8 (/nix/store/02mqs1by2vab9yzw0qc4j7463w78p3ps-glibc-2.37-8)",
-				"libunistring @ 1.1 (/nix/store/h1ysk4vvw48winwmh38rvnsj0dlsz7c1-libunistring-1.1) [dependency-of] libidn2 @ 2.3.4 (/nix/store/cw8fpl8r1x9rmaqj55fwbfnnrgw7b40k-libidn2-2.3.4)",
-				"xgcc @ 12.3.0 (/nix/store/fmz62d844wf4blb11k21f4m0q6n6hdfp-xgcc-12.3.0-libgcc) [dependency-of] glibc @ 2.37-8 (/nix/store/02mqs1by2vab9yzw0qc4j7463w78p3ps-glibc-2.37-8)",
+				"libidn2 @ 2.3.4 (/nix/store/k8ivghpggjrq1n49xp8sj116i4sh8lia-libidn2-2.3.4) [dependency-of] glibc @ 2.37-8 (/nix/store/aw2fw9ag10wr9pf0qk4nk5sxi0q0bn56-glibc-2.37-8)",
+				"libunistring @ 1.1 (/nix/store/s2gi8pfjszy6rq3ydx0z1vwbbskw994i-libunistring-1.1) [dependency-of] libidn2 @ 2.3.4 (/nix/store/k8ivghpggjrq1n49xp8sj116i4sh8lia-libidn2-2.3.4)",
+				"xgcc @ 12.3.0 (/nix/store/jbwb8d8l28lg9z0xzl784wyb9vlbwss6-xgcc-12.3.0-libgcc) [dependency-of] glibc @ 2.37-8 (/nix/store/aw2fw9ag10wr9pf0qk4nk5sxi0q0bn56-glibc-2.37-8)",
 			},
 		},
 	}
