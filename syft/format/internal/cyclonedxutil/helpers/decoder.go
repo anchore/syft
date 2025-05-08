b3271ffd44a5081e1fc41a51a2d042a642741ad4
Alex Goodman <wagoodman@users.noreply.github.com>
2025-05-08 09:29:58 -0400

    remove extractSyftID function
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/format/internal/cyclonedxutil/helpers/decoder.go
+++ syft/format/internal/cyclonedxutil/helpers/decoder.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/CycloneDX/cyclonedx-go"
 
-	"github.com/anchore/packageurl-go"
 	"github.com/anchore/syft/syft/artifact"
 	"github.com/anchore/syft/syft/linux"
 	"github.com/anchore/syft/syft/pkg"
@@ -66,15 +65,15 @@ func collectPackages(component *cyclonedx.Component, s *sbom.SBOM, idMap map[str
 	case cyclonedx.ComponentTypeApplication, cyclonedx.ComponentTypeFramework, cyclonedx.ComponentTypeLibrary:
 		p := decodeComponent(component)
 		idMap[component.BOMRef] = p
-		syftID := extractSyftPackageID(component.BOMRef)
-		if syftID != "" {
-			idMap[syftID] = p
-		}
 		if component.BOMRef != "" {
 			// always prefer the IDs from the SBOM over derived IDs
 			p.OverrideID(artifact.ID(component.BOMRef))
 		} else {
 			p.SetID()
+		}
+		syftID := p.ID()
+		if syftID != "" {
+			idMap[string(syftID)] = p
 		}
 		s.Artifacts.Packages.Add(*p)
 	}
@@ -84,19 +83,6 @@ func collectPackages(component *cyclonedx.Component, s *sbom.SBOM, idMap map[str
 			collectPackages(&(*component.Components)[i], s, idMap)
 		}
 	}
-}
-
-func extractSyftPackageID(i string) string {
-	instance, err := packageurl.FromString(i)
-	if err != nil {
-		return ""
-	}
-	for _, q := range instance.Qualifiers {
-		if q.Key == "package-id" {
-			return q.Value
-		}
-	}
-	return ""
 }
 
 func linuxReleaseFromComponents(components []cyclonedx.Component) *linux.Release {
