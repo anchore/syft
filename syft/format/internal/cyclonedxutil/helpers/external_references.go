eebc24c323cd11341cf71c985ec2acb14a7c3f63
Joshua Chen <27291761@qq.com>
2025-05-13 16:30:30 +0800

    feat: add support for encoding hashes in CycloneDX format
    Signed-off-by: Joshua Chen <27291761@qq.com>

--- syft/format/internal/cyclonedxutil/helpers/external_references.go
+++ syft/format/internal/cyclonedxutil/helpers/external_references.go
@@ -54,18 +54,7 @@ func encodeExternalReferences(p pkg.Package) *[]cyclonedx.ExternalReference {
 				})
 			}
 		case pkg.JavaArchive:
-			if len(metadata.ArchiveDigests) > 0 {
-				for _, digest := range metadata.ArchiveDigests {
-					refs = append(refs, cyclonedx.ExternalReference{
-						URL:  "",
-						Type: cyclonedx.ERTypeBuildMeta,
-						Hashes: &[]cyclonedx.Hash{{
-							Algorithm: toCycloneDXAlgorithm(digest.Algorithm),
-							Value:     digest.Value,
-						}},
-					})
-				}
-			}
+			//TODO: add support for java archive external references
 		case pkg.PythonPackage:
 			if metadata.DirectURLOrigin != nil && metadata.DirectURLOrigin.URL != "" {
 				ref := cyclonedx.ExternalReference{
@@ -83,21 +72,6 @@ func encodeExternalReferences(p pkg.Package) *[]cyclonedx.ExternalReference {
 		return &refs
 	}
 	return nil
-}
-
-// supported algorithm in cycloneDX as of 1.4
-// "MD5", "SHA-1", "SHA-256", "SHA-384", "SHA-512",
-// "SHA3-256", "SHA3-384", "SHA3-512", "BLAKE2b-256", "BLAKE2b-384", "BLAKE2b-512", "BLAKE3"
-// syft supported digests: cmd/syft/cli/eventloop/tasks.go
-// MD5, SHA1, SHA256
-func toCycloneDXAlgorithm(algorithm string) cyclonedx.HashAlgorithm {
-	validMap := map[string]cyclonedx.HashAlgorithm{
-		"sha1":   cyclonedx.HashAlgorithm("SHA-1"),
-		"md5":    cyclonedx.HashAlgorithm("MD5"),
-		"sha256": cyclonedx.HashAlgorithm("SHA-256"),
-	}
-
-	return validMap[strings.ToLower(algorithm)]
 }
 
 func decodeExternalReferences(c *cyclonedx.Component, metadata interface{}) {
