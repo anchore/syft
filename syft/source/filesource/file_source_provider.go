8bfef99c43aaedbd203db02272895ab4657d403b
houdini91 <mdstrauss91@gmail.com>
2025-06-09 22:26:56 +0300

    Suggest Image cleanup flow
    Signed-off-by: houdini91 <mdstrauss91@gmail.com>

--- syft/source/filesource/file_source_provider.go
+++ syft/source/filesource/file_source_provider.go
@@ -56,3 +56,8 @@ func (p fileSourceProvider) Provide(_ context.Context) (source.Source, error) {
 		},
 	)
 }
+
+func (p fileSourceProvider) Cleanup(_ context.Context) error {
+	// nothing to do here
+	return nil
+}
