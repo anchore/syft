597e664d7f395089de32870a6397887aae98555b
Simeon Stoykov <simeon.stoykov@quantco.com>
2025-06-11 18:54:51 +0200

    conda sboms are generated
    Signed-off-by: Simeon Stoykov <simeon.stoykov@quantco.com>

--- syft/pkg/cataloger/conda/cataloger.go
+++ syft/pkg/cataloger/conda/cataloger.go
@@ -5,6 +5,8 @@ package conda
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/anchore/syft/syft/artifact"
 	"github.com/anchore/syft/syft/file"
@@ -12,13 +14,38 @@ import (
 	"github.com/anchore/syft/syft/pkg/cataloger/generic"
 )
 
-// NewCondaCataloger returns a new cataloger object for Conda environments.
-func NewCondaCataloger() pkg.Cataloger {
-	return generic.NewCataloger("conda-cataloger").
+// NewCondaMetaCataloger returns a new cataloger object for Conda environments.
+func NewCondaMetaCataloger() pkg.Cataloger {
+	return generic.NewCataloger("conda-meta-cataloger").
 		WithParserByGlobs(parseCondaMeta, "**/conda-meta/*.json")
 }
 
 // parseCondaMeta is a stub parser for conda-meta JSON files.
-func parseCondaMeta(_ context.Context, _ file.Resolver, _ *generic.Environment, _ file.LocationReadCloser) ([]pkg.Package, []artifact.Relationship, error) {
-	return nil, nil, nil
+func parseCondaMeta(ctx context.Context, resolver file.Resolver, _ *generic.Environment, reader file.LocationReadCloser) ([]pkg.Package, []artifact.Relationship, error) {
+	dec := json.NewDecoder(reader)
+	var meta pkg.CondaMetaPackage
+	if err := dec.Decode(&meta); err != nil {
+		return nil, nil, fmt.Errorf("failed to parse conda-meta package file: %w", err)
+	}
+
+	p := pkg.Package{
+		Name:      meta.Name,
+		Version:   meta.Version,
+		PURL:      fmt.Sprintf("pkg:generic/%s@%s", meta.Name, meta.Version),
+		Locations: file.NewLocationSet(reader.Location),
+		Licenses: pkg.NewLicenseSet(
+			pkg.NewLicenseFromLocationsWithContext(ctx, meta.License, reader.Location),
+		),
+		Language: pkg.UnknownLanguage,
+		Type:     pkg.CondaPkg,
+		Metadata: meta,
+	}
+
+	p.SetID()
+
+	pkgs := []pkg.Package{
+		p,
+	}
+
+	return pkgs, nil, nil
 }
