c59f162e938c2676abdc87ce64d56bf2a00d94e9
Alex Goodman <wagoodman@users.noreply.github.com>
2025-07-23 09:26:19 -0400

    add source completion tester
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/testutil/completion_testing.go
+++ syft/testutil/completion_testing.go
@@ -4,14 +4,25 @@ import (
 	"testing"
 
 	"github.com/anchore/syft/syft/internal/packagemetadata"
+	"github.com/anchore/syft/syft/internal/sourcemetadata"
 )
 
 type PackageMetadataCompletionTester struct {
 	*packagemetadata.CompletionTester
 }
 
+type SourceMetadataCompletionTester struct {
+	*sourcemetadata.CompletionTester
+}
+
 func NewPackageMetadataCompletionTester(t testing.TB, ignore ...any) *PackageMetadataCompletionTester {
 	return &PackageMetadataCompletionTester{
 		CompletionTester: packagemetadata.NewCompletionTester(t, ignore...),
+	}
+}
+
+func NewSourceMetadataCompletionTester(t testing.TB, ignore ...any) *SourceMetadataCompletionTester {
+	return &SourceMetadataCompletionTester{
+		CompletionTester: sourcemetadata.NewCompletionTester(t, ignore...),
 	}
 }
