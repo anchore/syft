0d344c19fdf6968d9b4fb50e0c472dc0d1cc30e0
Keith Zantow <kzantow@gmail.com>
2025-08-11 15:37:38 -0400

    chore: add more java binary test cases
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- syft/pkg/cataloger/binary/classifiers_java_test.go
+++ syft/pkg/cataloger/binary/classifiers_java_test.go
@@ -20,15 +20,47 @@ func Test_JavaBinaryImage(t *testing.T) {
 				"openjdk @ 11.0.17+8-LTS (/staged/positive/openjdk-lts/java)",
 			},
 		},
+		{
+			image: "image-java-zulu-8",
+			expected: []string{
+				"zulu @ 1.8.0_462-b08 (/usr/lib/jvm/zulu8-ca-amd64/bin/jdb)",
+			},
+		},
+		{
+			image: "image-java-zulu-21",
+			expected: []string{
+				"zulu @ 21.0.8+9-LTS (/usr/lib/jvm/zulu21-ca-amd64/bin/java)",
+			},
+		},
+		{
+			image: "image-java-ibm-8",
+			expected: []string{
+				"java @ 1.8.0-foreman_2023_10_12_13_27-b00 (/opt/ibm/java/jre/bin/java)",
+			},
+		},
+		{
+			image: "image-java-ibm-jre-8",
+			expected: []string{
+				"java @ 1.8.0-_2025_04_14_02_37-b00 (/opt/ibm/java/jre/bin/java)",
+			},
+		},
+		{
+			image: "image-java-ibm-sdk-8",
+			expected: []string{
+				"java_sdk @ 1.8.0-foreman_2022_01_20_09_33-b00 (/opt/ibm/java/bin/jdb)",
+			},
+		},
 	}
 
 	for _, tt := range tests {
-		c := NewClassifierCataloger(ClassifierCatalogerConfig{
-			Classifiers: defaultJavaClassifiers(),
+		t.Run(tt.image, func(t *testing.T) {
+			c := NewClassifierCataloger(ClassifierCatalogerConfig{
+				Classifiers: defaultJavaClassifiers(),
+			})
+			pkgtest.NewCatalogTester().
+				WithImageResolver(t, tt.image).
+				ExpectsPackageStrings(tt.expected).
+				TestCataloger(t, c)
 		})
-		pkgtest.NewCatalogTester().
-			WithImageResolver(t, tt.image).
-			ExpectsPackageStrings(tt.expected).
-			TestCataloger(t, c)
 	}
 }
