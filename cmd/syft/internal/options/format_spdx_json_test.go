f898170efc37ee7cf83bd63eb969e154f79699c1
Christopher Phillips <32073428+spiffcs@users.noreply.github.com>
2025-08-12 22:29:33 -0400

    test: add some cli testing around new options
    Signed-off-by: Christopher Phillips <32073428+spiffcs@users.noreply.github.com>

--- cmd/syft/internal/options/format_spdx_json_test.go
+++ cmd/syft/internal/options/format_spdx_json_test.go
@@ -13,3 +13,45 @@ func TestFormatSPDXJSON_buildConfig(t *testing.T) {
 	subject := ft.config("Version")
 	assertExpectedValue(t, subject)
 }
+
+func TestFormatSPDXJSON_Validate(t *testing.T) {
+	tests := []struct {
+		name      string
+		createdTime *int64
+		wantErr   bool
+	}{
+		{
+			name:      "nil timestamp is valid",
+			createdTime: nil,
+			wantErr:   false,
+		},
+		{
+			name:      "positive timestamp is valid",
+			createdTime: ptr(int64(1234567890)),
+			wantErr:   false,
+		},
+		{
+			name:      "zero timestamp is valid",
+			createdTime: ptr(int64(0)),
+			wantErr:   false,
+		},
+		{
+			name:      "negative timestamp is invalid",
+			createdTime: ptr(int64(-1)),
+			wantErr:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := FormatSPDXJSON{
+				CreatedTime: tt.createdTime,
+			}
+			err := o.Validate()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
