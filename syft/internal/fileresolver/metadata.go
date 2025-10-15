817123401118baac57d219c00f2ea4655ac634ae
Joseph Shapiro <joeyashapiro@gmail.com>
2025-10-14 20:00:04 -0500

    allow NewMetadataFromPath to open windows file
    Signed-off-by: Joseph Shapiro <joeyashapiro@gmail.com>

--- syft/internal/fileresolver/metadata.go
+++ syft/internal/fileresolver/metadata.go
@@ -2,10 +2,12 @@ package fileresolver
 
 import (
 	"os"
+	"runtime"
 	"syscall"
 
 	"github.com/anchore/stereoscope/pkg/file"
 	"github.com/anchore/syft/internal/log"
+	"github.com/anchore/syft/syft/internal/windows"
 )
 
 func NewMetadataFromPath(path string, info os.FileInfo) file.Metadata {
@@ -15,7 +17,13 @@ func NewMetadataFromPath(path string, info os.FileInfo) file.Metadata {
 	ty := file.TypeFromMode(info.Mode())
 
 	if ty == file.TypeRegular {
-		f, err := os.Open(path)
+		usablePath := path
+		// denormalize the path back to windows so we can open the file
+		if runtime.GOOS == "windows" {
+			usablePath = windows.FromPosix(usablePath)
+		}
+
+		f, err := os.Open(usablePath)
 		if err != nil {
 			// TODO: it may be that the file is inaccessible, however, this is not an error or a warning. In the future we need to track these as known-unknowns
 			f = nil
