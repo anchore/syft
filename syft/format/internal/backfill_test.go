fd740d729b30a22de23f4d553066b7054db9e6d1
Will Murphy <willmurphyscode@users.noreply.github.com>
2025-10-28 08:40:42 -0400

    Respect "rpmmod" PURL qualifier
    Red Hat purls the RPM modularity info in a query param in the PURLs in
    their vulnerability data. It would be nice if Syft respected this
    qualifier so that Grype can use it when a Red Hat purl is passed.
    
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

--- syft/format/internal/backfill_test.go
+++ syft/format/internal/backfill_test.go
@@ -54,6 +54,21 @@ func Test_Backfill(t *testing.T) {
 			},
 		},
 		{
+			name: "rpm with rpmmod",
+			in: pkg.Package{
+				PURL: "pkg:rpm/redhat/httpd@2.4.37-51?arch=x86_64&distro=rhel-8.7&rpmmod=httpd:2.4",
+			},
+			expected: pkg.Package{
+				PURL:    "pkg:rpm/redhat/httpd@2.4.37-51?arch=x86_64&distro=rhel-8.7&rpmmod=httpd:2.4",
+				Type:    pkg.RpmPkg,
+				Name:    "httpd",
+				Version: "2.4.37-51",
+				Metadata: pkg.RpmDBEntry{
+					ModularityLabel: strRef("httpd:2.4"),
+				},
+			},
+		},
+		{
 			name: "bad cpe",
 			in: pkg.Package{
 				PURL: "pkg:npm/testp@3.0.0?cpes=cpe:2.3a:testv:testp:3.0.0:*:*:*:*:*:*:*",
@@ -170,4 +185,8 @@ func Test_nameFromPurl(t *testing.T) {
 			require.Equal(t, tt.expected, got)
 		})
 	}
+}
+
+func strRef(s string) *string {
+	return &s
 }
