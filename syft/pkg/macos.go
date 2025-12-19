6e169b9e7424a90ea1d2feb6cfb30fa129e30d24
Rez Moss <hi@rezmoss.com>
2025-12-19 18:51:57 -0500

    added macOS .app cataloger, fixed #4010
    Signed-off-by: Rez Moss <hi@rezmoss.com>

--- syft/pkg/macos.go
+++ syft/pkg/macos.go
@@ -1,5 +1,7 @@
 package pkg
 
+// MacOSAppEntry represents metadata about a macOS application extracted from Info.plist files.
 type MacOSAppEntry struct {
+	// BundleIdentifier is the unique identifier for the application (e.g. "com.apple.Safari")
 	BundleIdentifier string `json:"bundleIdentifier,omitempty"`
 }
