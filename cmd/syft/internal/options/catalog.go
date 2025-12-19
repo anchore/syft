a1bedad3c4df921befdaad00daac4af4d033924d
Rez Moss <hi@rezmoss.com>
2025-12-18 20:13:13 -0500

    fixed #4430 exclude dev pnpm pkg
    Signed-off-by: Rez Moss <hi@rezmoss.com>

--- cmd/syft/internal/options/catalog.go
+++ cmd/syft/internal/options/catalog.go
@@ -193,7 +193,9 @@ func (cfg Catalog) ToPackagesConfig() pkgcataloging.Config {
 		JavaScript: javascript.DefaultCatalogerConfig().
 			WithIncludeDevDependencies(*multiLevelOption(false, cfg.JavaScript.IncludeDevDependencies)).
 			WithSearchRemoteLicenses(*multiLevelOption(false, enrichmentEnabled(cfg.Enrich, task.JavaScript, task.Node, task.NPM), cfg.JavaScript.SearchRemoteLicenses)).
-			WithNpmBaseURL(cfg.JavaScript.NpmBaseURL),
+			WithNpmBaseURL(cfg.JavaScript.NpmBaseURL).
+			WithPnpmExcludeDevDependencies(*multiLevelOption(false, cfg.JavaScript.PnpmExcludeDevDependencies)),
+
 		LinuxKernel: kernel.LinuxKernelCatalogerConfig{
 			CatalogModules: cfg.LinuxKernel.CatalogModules,
 		},
