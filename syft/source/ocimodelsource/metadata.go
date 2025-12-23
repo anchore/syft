0cc4f03e8a9e7b4ff048557e870a8543141f8d9a
Christopher Phillips <32073428+spiffcs@users.noreply.github.com>
2025-12-23 13:50:55 -0500

    chore: add comment flagging id:"-"
    Signed-off-by: Christopher Phillips <32073428+spiffcs@users.noreply.github.com>

--- syft/source/ocimodelsource/metadata.go
+++ syft/source/ocimodelsource/metadata.go
@@ -3,6 +3,8 @@ package ocimodelsource
 import "github.com/anchore/syft/syft/source"
 
 // OCIModelMetadata represents all static metadata that defines what an OCI model artifact is.
+// The struct tag for Annotations with id:"-" is used so that we do not generate different source ID based
+// on changes to the annotations of a source image. This does not constitute a different identifiable.
 type OCIModelMetadata struct {
 	source.ImageMetadata `json:",inline"`
 	Annotations          map[string]string `json:"annotations,omitempty" id:"-"`
