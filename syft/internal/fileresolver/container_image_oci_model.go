7568d162812425bf609846ce5be11ab1c2707d21
Christopher Phillips <32073428+spiffcs@users.noreply.github.com>
2025-12-23 13:48:08 -0500

    chore: update to unmix layerInfo concerns
    Signed-off-by: Christopher Phillips <32073428+spiffcs@users.noreply.github.com>

--- syft/internal/fileresolver/container_image_oci_model.go
+++ syft/internal/fileresolver/container_image_oci_model.go
@@ -1,4 +1,4 @@
-package ocimodelsource
+package fileresolver
 
 import (
 	"context"
@@ -10,19 +10,25 @@ import (
 	"github.com/anchore/syft/syft/file"
 )
 
-var _ file.Resolver = (*ociModelResolver)(nil)
-var _ file.OciLayerResolver = (*ociModelResolver)(nil)
+var _ file.Resolver = (*OCIModelResolver)(nil)
+var _ file.OciLayerResolver = (*OCIModelResolver)(nil)
 
-// ociModelResolver is a file.Resolver implementation that provides access to
+// LayerInfo holds information about an OCI model layer file stored on disk.
+type LayerInfo struct {
+	TempPath  string // Path to the temp file on disk
+	MediaType string // OCI media type of the layer
+}
+
+// OCIModelResolver is a file.Resolver implementation that provides access to
 // GGUF header data fetched from OCI model artifacts via range-GET requests.
-type ociModelResolver struct {
+type OCIModelResolver struct {
 	tempDir    string                   // temp directory containing all layer files
-	layerFiles map[string]layerInfo     // digest -> layer info (temp path + media type)
+	layerFiles map[string]LayerInfo     // digest -> layer info (temp path + media type)
 	locations  map[string]file.Location // digest -> location
 }
 
-// newOCIModelResolver creates a new resolver with the given temp directory and layer files.
-func newOCIModelResolver(tempDir string, layerFiles map[string]layerInfo) *ociModelResolver {
+// NewOCIModelResolver creates a new resolver with the given temp directory and layer files.
+func NewOCIModelResolver(tempDir string, layerFiles map[string]LayerInfo) *OCIModelResolver {
 	// Create locations for all layer files
 	// Each location has RealPath="/", FileSystemID=digest, AccessPath="/"
 	locations := make(map[string]file.Location, len(layerFiles))
@@ -32,7 +38,7 @@ func newOCIModelResolver(tempDir string, layerFiles map[string]layerInfo) *ociMo
 		locations[digest] = file.NewVirtualLocationFromCoordinates(coords, "/")
 	}
 
-	return &ociModelResolver{
+	return &OCIModelResolver{
 		tempDir:    tempDir,
 		layerFiles: layerFiles,
 		locations:  locations,
@@ -41,7 +47,7 @@ func newOCIModelResolver(tempDir string, layerFiles map[string]layerInfo) *ociMo
 
 // FilesByMediaType returns locations for layers matching the given media type patterns.
 // Patterns support glob-style matching (e.g., "application/vnd.docker.ai*").
-func (r *ociModelResolver) FilesByMediaType(types ...string) ([]file.Location, error) {
+func (r *OCIModelResolver) FilesByMediaType(types ...string) ([]file.Location, error) {
 	var matches []file.Location
 
 	for digest, info := range r.layerFiles {
@@ -66,7 +72,7 @@ func (r *ociModelResolver) FilesByMediaType(types ...string) ([]file.Location, e
 // The location's FileSystemID contains the layer digest, which is used to look up the temp file.
 // This method is used as part of the content selection in the generic cataloger when locations
 // are returned by searching for contents by media type.
-func (r *ociModelResolver) FileContentsByLocation(location file.Location) (io.ReadCloser, error) {
+func (r *OCIModelResolver) FileContentsByLocation(location file.Location) (io.ReadCloser, error) {
 	// Look up the temp file path using the digest stored in FileSystemID
 	digest := location.FileSystemID
 	info, ok := r.layerFiles[digest]
@@ -77,12 +83,12 @@ func (r *ociModelResolver) FileContentsByLocation(location file.Location) (io.Re
 }
 
 // FileMetadataByLocation returns metadata for the file at the given location.
-func (r *ociModelResolver) FileMetadataByLocation(_ file.Location) (m file.Metadata, err error) {
+func (r *OCIModelResolver) FileMetadataByLocation(_ file.Location) (m file.Metadata, err error) {
 	return m, nil
 }
 
 // HasPath checks if the given path exists in the resolver.
-func (r *ociModelResolver) HasPath(path string) bool {
+func (r *OCIModelResolver) HasPath(path string) bool {
 	// The virtual path is "/" for all files
 	if path == "/" && len(r.layerFiles) > 0 {
 		return true
@@ -91,31 +97,31 @@ func (r *ociModelResolver) HasPath(path string) bool {
 }
 
 // FilesByPath returns locations for files matching the given paths.
-func (r *ociModelResolver) FilesByPath(_ ...string) ([]file.Location, error) {
+func (r *OCIModelResolver) FilesByPath(_ ...string) ([]file.Location, error) {
 	return nil, nil
 }
 
 // FilesByGlob returns locations for files matching the given glob patterns.
-func (r *ociModelResolver) FilesByGlob(_ ...string) ([]file.Location, error) {
+func (r *OCIModelResolver) FilesByGlob(_ ...string) ([]file.Location, error) {
 	return nil, nil
 }
 
 // FilesByMIMEType returns locations for files with the given MIME types.
 // This is not implemented for OCI model artifacts as we don't have MIME type detection.
-func (r *ociModelResolver) FilesByMIMEType(_ ...string) ([]file.Location, error) {
+func (r *OCIModelResolver) FilesByMIMEType(_ ...string) ([]file.Location, error) {
 	// Not implemented - OCI model artifacts don't have MIME type detection
 	return nil, nil
 }
 
 // RelativeFileByPath returns a file at the given path relative to the reference location.
 // This is not applicable for OCI model artifacts.
-func (r *ociModelResolver) RelativeFileByPath(_ file.Location, _ string) *file.Location {
+func (r *OCIModelResolver) RelativeFileByPath(_ file.Location, _ string) *file.Location {
 	// Not implemented - no layer hierarchy in OCI model artifacts
 	return nil
 }
 
 // AllLocations returns all file locations in the resolver.
-func (r *ociModelResolver) AllLocations(ctx context.Context) <-chan file.Location {
+func (r *OCIModelResolver) AllLocations(ctx context.Context) <-chan file.Location {
 	ch := make(chan file.Location)
 
 	go func() {
@@ -131,12 +137,4 @@ func (r *ociModelResolver) AllLocations(ctx context.Context) <-chan file.Locatio
 	}()
 
 	return ch
-}
-
-// cleanup removes the temp directory and all files within it.
-func (r *ociModelResolver) cleanup() error {
-	if r.tempDir == "" {
-		return nil
-	}
-	return os.RemoveAll(r.tempDir)
 }
