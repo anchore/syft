ad2cff7039b306d72151eca163e6d649bfcb4c81
Rez Moss <hi@rezmoss.com>
2026-01-05 12:08:10 -0500

    added deno cataloger , #fixed 4417 , update schema
    Signed-off-by: Rez Moss <hi@rezmoss.com>

--- syft/pkg/deno.go
+++ syft/pkg/deno.go
@@ -1,11 +1,19 @@
 package pkg
 
+// DenoLockEntry is a struct that rep a single entry found in the "packages" section of a Deno deno.lock file
 type DenoLockEntry struct {
-	Integrity    string   `mapstructure:"integrity" json:"integrity"`
+	// Integrity is the crpto hash of the package content for verification
+	Integrity string `mapstructure:"integrity" json:"integrity"`
+
+	// Dependencies is the list of package specifiers that this package depends on
 	Dependencies []string `mapstructure:"dependencies" json:"dependencies"`
 }
 
+// DenoRemoteLockEntry is a struct that rep a single entry found in the "remote" section of a Deno deno.lock file
 type DenoRemoteLockEntry struct {
-	URL       string `mapstructure:"url" json:"url"`
+	// URL is the remote URL from which the module fetcef
+	URL string `mapstructure:"url" json:"url"`
+
+	// Integrity is the crpto hash of the package content for verification
 	Integrity string `mapstructure:"integrity" json:"integrity"`
 }
