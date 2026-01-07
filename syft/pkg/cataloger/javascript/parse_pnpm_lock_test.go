3a3a86eb017bc3ccd290e38cfed2fa2ae8396830
Rez Moss <hi@rezmoss.com>
2026-01-07 10:39:16 -0500

    fixed #4430 exclude dev pnpm pkg (#4487)
    * fixed #4430 exclude dev pnpm pkg
    
    Signed-off-by: Rez Moss <hi@rezmoss.com>
    
    * use existing dev deps option
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * fixed #4430 exclude dev pnpm pkg, add test
    
    Signed-off-by: Rez Moss <hi@rezmoss.com>
    
    * fixed #4430 exclude dev pnpm pkg, add test
    
    Signed-off-by: Rez Moss <hi@rezmoss.com>
    
    ---------
    
    Signed-off-by: Rez Moss <hi@rezmoss.com>
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    Co-authored-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- syft/pkg/cataloger/javascript/parse_pnpm_lock_test.go
+++ syft/pkg/cataloger/javascript/parse_pnpm_lock_test.go
@@ -62,7 +62,47 @@ func TestParsePnpmLock(t *testing.T) {
 		},
 	}
 
-	adapter := newGenericPnpmLockAdapter(CatalogerConfig{})
+	adapter := newGenericPnpmLockAdapter(CatalogerConfig{IncludeDevDependencies: true})
+	pkgtest.TestFileParser(t, fixture, adapter.parsePnpmLock, expectedPkgs, expectedRelationships)
+}
+
+func TestParsePnpmLock_ExcludeDevDependencies(t *testing.T) {
+	var expectedRelationships []artifact.Relationship
+	fixture := "test-fixtures/pnpm/pnpm-lock.yaml"
+
+	locationSet := file.NewLocationSet(file.NewLocation(fixture))
+
+	expectedPkgs := []pkg.Package{
+		{
+			Name:      "nanoid",
+			Version:   "3.3.4",
+			PURL:      "pkg:npm/nanoid@3.3.4",
+			Locations: locationSet,
+			Language:  pkg.JavaScript,
+			Type:      pkg.NpmPkg,
+			Metadata:  pkg.PnpmLockEntry{Resolution: pkg.PnpmLockResolution{}},
+		},
+		{
+			Name:      "picocolors",
+			Version:   "1.0.0",
+			PURL:      "pkg:npm/picocolors@1.0.0",
+			Locations: locationSet,
+			Language:  pkg.JavaScript,
+			Type:      pkg.NpmPkg,
+			Metadata:  pkg.PnpmLockEntry{Resolution: pkg.PnpmLockResolution{}},
+		},
+		{
+			Name:      "source-map-js",
+			Version:   "1.0.2",
+			PURL:      "pkg:npm/source-map-js@1.0.2",
+			Locations: locationSet,
+			Language:  pkg.JavaScript,
+			Type:      pkg.NpmPkg,
+			Metadata:  pkg.PnpmLockEntry{Resolution: pkg.PnpmLockResolution{}},
+		},
+	}
+
+	adapter := newGenericPnpmLockAdapter(CatalogerConfig{IncludeDevDependencies: false})
 	pkgtest.TestFileParser(t, fixture, adapter.parsePnpmLock, expectedPkgs, expectedRelationships)
 }
 
