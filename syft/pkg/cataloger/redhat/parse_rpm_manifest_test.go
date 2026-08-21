93cf89363b2612d3bfec7a65601c68f90a21ffbb
Sueun Cho <sueun.dev@gmail.com>
2026-08-22 03:47:23 +0900

    fix(rpm): keep the epoch when parsing RPM manifest packages (#5201)
    ---------
    
    Signed-off-by: Sueun Cho <sueun.dev@gmail.com>
    Signed-off-by: Christopher Phillips <32073428+spiffcs@users.noreply.github.com>
    Co-authored-by: Christopher Phillips <32073428+spiffcs@users.noreply.github.com>

--- syft/pkg/cataloger/redhat/parse_rpm_manifest_test.go
+++ syft/pkg/cataloger/redhat/parse_rpm_manifest_test.go
@@ -1,7 +1,11 @@
 package redhat
 
 import (
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/anchore/syft/syft/file"
 	"github.com/anchore/syft/syft/pkg"
@@ -80,6 +84,23 @@ func TestParseRpmManifest(t *testing.T) {
 				Vendor:    "Microsoft Corporation",
 			},
 		},
+		{
+			Name:      "vim",
+			Version:   "2:9.0-1.cm2",
+			PURL:      "pkg:rpm/vim@9.0-1.cm2?arch=x86_64&epoch=2&upstream=vim-9.0-1.cm2.src.rpm",
+			Locations: file.NewLocationSet(location),
+			Type:      pkg.RpmPkg,
+			Metadata: pkg.RpmDBEntry{
+				Name:      "vim",
+				Epoch:     intRef(2),
+				Arch:      "x86_64",
+				Release:   "1.cm2",
+				Version:   "9.0",
+				SourceRpm: "vim-9.0-1.cm2.src.rpm",
+				Size:      45000,
+				Vendor:    "Microsoft Corporation",
+			},
+		},
 	}
 
 	pkgtest.NewCatalogTester().
@@ -87,4 +108,75 @@ func TestParseRpmManifest(t *testing.T) {
 		Expects(expected, nil).
 		TestParser(t, parseRpmManifest)
 
+}
+
+func TestNewMetadataFromManifestLine(t *testing.T) {
+	// fields are: NAME, VERSION-RELEASE, INSTALLTIME, BUILDTIME, VENDOR, EPOCH, SIZE, ARCH, EPOCHNUM, SOURCERPM
+	line := func(epoch, size, epochNum string) string {
+		return strings.Join([]string{
+			"vim", "9.0-1.cm2", "1653816591", "1653753130", "Microsoft Corporation",
+			epoch, size, "x86_64", epochNum, "vim-9.0-1.cm2.src.rpm",
+		}, "\t")
+	}
+
+	tests := []struct {
+		name      string
+		entry     string
+		wantEpoch *int
+		wantSize  int
+		wantErr   require.ErrorAssertionFunc
+	}{
+		{
+			name:      "no epoch",
+			entry:     line("(none)", "45000", "0"),
+			wantEpoch: nil,
+			wantSize:  45000,
+		},
+		{
+			name:      "explicit zero epoch is distinct from no epoch",
+			entry:     line("0", "45000", "0"),
+			wantEpoch: intRef(0),
+			wantSize:  45000,
+		},
+		{
+			name:      "unparsable epochnum yields no epoch",
+			entry:     line("2", "45000", ""),
+			wantEpoch: nil,
+			wantSize:  45000,
+		},
+		{
+			name:      "unparsable size yields zero size",
+			entry:     line("2", "bogus", "2"),
+			wantEpoch: intRef(2),
+			wantSize:  0,
+		},
+		{
+			name:    "too few fields",
+			entry:   "vim\t9.0-1.cm2",
+			wantErr: require.Error,
+		},
+		{
+			name:    "version field missing the release",
+			entry:   strings.Replace(line("(none)", "45000", "0"), "9.0-1.cm2", "9.0", 1),
+			wantErr: require.Error,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantErr == nil {
+				tt.wantErr = require.NoError
+			}
+
+			metadata, err := newMetadataFromManifestLine(tt.entry)
+			tt.wantErr(t, err)
+			if err != nil {
+				return
+			}
+
+			require.NotNil(t, metadata)
+			assert.Equal(t, tt.wantEpoch, metadata.Epoch)
+			assert.Equal(t, tt.wantSize, metadata.Size)
+		})
+	}
 }
